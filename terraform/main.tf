0f7bfddc818b89ab0a4cdfbc23f5ee5e9bff418c
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-02-26 11:58:14 +0000

    Removes Terraform aws_key_pair resource
    Kops creates an SSH keypair, so we don’t need the Terraform equivalent

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,3 @@ provider "aws" {
   version = "~> 1.1"
   region = "eu-west-1"
 }
-
-resource "aws_key_pair" "sandbox" {
-  key_name   = "kubernetes-sandbox"
-  public_key = "${file("../ssh/kubernetes_sandbox_id_rsa.pub")}"
-}
