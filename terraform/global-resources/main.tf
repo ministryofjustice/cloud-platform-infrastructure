7b636215d9053b9cad5c90d107a1e7fd989b5a36
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-03-26 17:43:26 +0100

    Separate ‘global’ Terraform resources
    Moves non-platform-specific resources into a separate directory and Terraform remote state S3 bucket. This will allow us to reuse the main platform Terraform resources in multiple environments/workspaces, without duplicating global resources such as parent DNS zones and Terraform state S3 buckets.

---
+++ terraform/global-resources/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "moj-cp-k8s-investigation-global-terraform"
+    region = "eu-west-1"
+    key = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.9.0"
+  region = "eu-west-1"
+}
