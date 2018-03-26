ccaa7f48b87e55074c35f041f5a1f52025ca746e
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-03-26 17:50:04 +0100

    Adds “platform” Terraform directory
    Makes use of Terraform “workspace” feature, to allow us to apply the same resources to create multiple environments - production, staging etc.
    
    As platform Terraform resources are now defined as modules, the environment declaration now simply configures terraform, imports modules, and passes in per-workspace values where required.

---
+++ terraform/cloud-platform/main.tf
@@ -0,0 +1,48 @@
+# Setup
+terraform {
+  backend "s3" {
+    bucket = "moj-cp-k8s-investigation-platform-terraform"
+    region = "eu-west-1"
+    key = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.9.0"
+  region = "eu-west-1"
+}
+
+data "terraform_remote_state" "global" {
+    backend = "s3"
+    config {
+        bucket = "moj-cp-k8s-investigation-global-terraform"
+        region = "eu-west-1"
+        key = "terraform.tfstate"
+    }
+}
+
+locals {
+    cluster_name = "${terraform.workspace}"
+    cluster_base_domain_name = "${local.cluster_name}.k8s.integration.dsd.io"
+}
+
+# Modules
+module "cluster_dns" {
+    source = "../modules/cluster_dns"
+
+    cluster_base_domain_name = "${local.cluster_base_domain_name}"
+    parent_zone_id = "${data.terraform_remote_state.global.k8s_zone_id}"
+}
+
+module "cluster_ssl" {
+    source = "../modules/cluster_ssl"
+
+    cluster_base_domain_name = "${local.cluster_base_domain_name}"
+    dns_zone_id = "${module.cluster_dns.cluster_dns_zone_id}"
+}
+
+module "kops_state" {
+    source = "../modules/kops_state"
+
+    cluster_name = "${local.cluster_name}"
+}
