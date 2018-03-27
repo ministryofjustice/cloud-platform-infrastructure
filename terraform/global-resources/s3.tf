ad81af08af2ff4a25e9f5e7bc97668b0a2ff0876
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-03-27 14:33:00 +0100

    Moved Kops state bucket into Terraform global-resources, as we only need one for all clusters

--- terraform/global-resources/s3.tf
+++ terraform/global-resources/s3.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "platform_terraform" {
     enabled = true
   }
 }
+
+resource "aws_s3_bucket" "kops_state_store" {
+  bucket = "moj-cp-k8s-investigation-kops"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
--- terraform/modules/kops_state/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "kops_state_store" {
-  bucket = "moj-cp-kops-${var.cluster_name}"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-}
