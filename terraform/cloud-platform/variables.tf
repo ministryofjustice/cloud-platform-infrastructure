21dd6c323dd035ebeb19665650112fdd1c9d0356
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-04-03 17:34:54 +0100

    Adds aws_federation Terraform module to cloud-platforms resources

--- terraform/cloud-platform/variables.tf
+++ terraform/cloud-platform/variables.tf
@@ -1,3 +1,8 @@
 variable "region" {
     default = "eu-west-1"
 }
+
+variable "aws_federation_saml_x509_cert" {}
+variable "aws_federation_saml_idp_domain" {}
+variable "aws_federation_saml_login_url" {} 
+variable "aws_federation_saml_logout_url" {}
