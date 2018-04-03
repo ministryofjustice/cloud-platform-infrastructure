0d72ebfd11c3e7d8394da8bd626dfa057ac22b75
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-04-03 11:32:31 +0100

    terraform config for the cluster-creation pipeline creation

---
+++ cluster-pipeline/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "cluster-creation-pipeline"
+}
+
+variable "git_repo" {
+  default = "kubernetes-investigations"
+}
+
+variable "git_branch" {
+  default = "feature/cluster-creation-pipeline"
+}
+
+variable "git_token" {
+  default = "6b9f603904a73ce45bfe8f3bab09a52ccf4b3d0e"
+}
