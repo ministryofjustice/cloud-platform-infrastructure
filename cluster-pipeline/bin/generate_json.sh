a3ffac5b18f86c65f5e264b7424451c24e703915
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-04-03 15:16:32 +0100

    tf template generation for vpc

--- cluster-pipeline/bin/generate_json.sh
+++ cluster-pipeline/bin/generate_json.sh
@@ -8,11 +8,13 @@ if [ $# -ne 3 ]
 then
   echo "usage: $0 <FABRIC_NAME> <DOMAIN_NAME> <FABRIC_REGION>"
 else
+  echo "Creating config.json..."
+
   echo "
   {
       \"domain_name\": \""$DOMAIN_NAME"\",
       \"fabric_availability_zones\": [],
       \"fabric_name\": \""$FABRIC_NAME"\",
       \"fabric_region\": \""$FABRIC_REGION"\"
-  }" > config.json
+  }" > config.json && echo "Done."
 fi
