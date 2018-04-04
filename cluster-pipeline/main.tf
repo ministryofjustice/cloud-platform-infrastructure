5e6b8bef3f925e028e6f011352f226d64a102427
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-04-04 10:55:59 +0100

    debug tf templates

--- cluster-pipeline/main.tf
+++ cluster-pipeline/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 // ---------------------------------------------------------------------------------------------------------------------
 
 module "main_network" {
-  source                  = "github.com/datawire/reference-infrastructure-fabric//modules/vpc"
+  source                  = "modules/vpc"
   cidr_block              = "${var.vpc_cidr}"
   name                    = "${var.fabric_name}"
   internal_subnets        = ["${var.internal_subnets}"]
--- cluster-pipeline/modules/vpc/outputs.tf
+++ cluster-pipeline/modules/vpc/outputs.tf
@@ -2,5 +2,5 @@
 
 output "id"                 { value = "${aws_vpc.main.id}" }
 output "cidr_block"         { value = "${aws_vpc.main.cidr_block}" }
-output "external_subets"    { value = "${aws_subnet.external.*}" }
+#output "external_subets"    { value = "${aws_subnet.external.*}" }
 output "availability_zones" { value = "${var.availability_zones}" }
