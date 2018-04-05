b86442eb8a254243febce05e350d7e94a05229e4
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-04-05 14:43:33 +0100

    added tf templates for hosted zone creation

--- cluster-pipeline/main.tf
+++ cluster-pipeline/main.tf
@@ -10,9 +10,24 @@ provider "aws" {
 
 module "main_network" {
   source                  = "modules/vpc"
+
   cidr_block              = "${var.vpc_cidr}"
   name                    = "${var.fabric_name}"
   internal_subnets        = ["${var.internal_subnets}"]
   external_subnets        = ["${var.external_subnets}"]
   availability_zones      = ["${var.fabric_availability_zones}"]
+}
+
+module "cluster_dns" {
+  source = "../terraform/modules/cluster_dns"
+
+  cluster_base_domain_name = "${var.fabric_region}"
+  parent_zone_id = "Z1QX7XOKFA0VGE"
+}
+
+module "cluster_ssl" {
+  source = "../terraform/modules/cluster_ssl"
+
+  cluster_base_domain_name = "${var.fabric_region}"
+  dns_zone_id = "${module.cluster_dns.cluster_dns_zone_id}"
 }
