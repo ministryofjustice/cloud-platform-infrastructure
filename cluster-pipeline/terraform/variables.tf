fcb5d16f50c1f9502141a11e69a830552238ddae
ale-novo <alejandro.novotny@digital.justice.gov.uk>
2018-04-11 16:40:52 +0100

    encrypted token

--- cluster-pipeline/terraform/variables.tf
+++ cluster-pipeline/terraform/variables.tf
@@ -1,15 +1,4 @@
-variable "project_name" {
-  default = "cluster-creation"
-}
-
-variable "git_repo" {
-  default = "kubernetes-investigations"
-}
-
-variable "git_branch" {
-  default = "feature/cluster-creation-pipeline"
-}
-
-variable "git_token" {
-  default = "6b9f603904a73ce45bfe8f3bab09a52ccf4b3d0e"
-}
+variable "project_name" {}
+variable "git_repo" {}
+variable "git_branch" {}
+variable "git_token" {}
