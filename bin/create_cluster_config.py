543f5ee85fdf090577fd50dd63edfafea698c3b2
Kerin Cosford <cosford@gmail.com>
2018-04-27 11:55:25 +0100

    Correct python formatting
    Python should follow the styleguide laid out in PEP8 https://www.python.org/dev/peps/pep-0008/

--- bin/create_cluster_config.py
+++ bin/create_cluster_config.py
@@ -1,29 +1,42 @@
- #!/usr/bin/python
+#!/usr/bin/python
 
-import sys, subprocess, json, argparse, yaml, pprint, os.path
+import sys
+import subprocess
+import json
+import argparse
+import yaml
+import pprint
+import os.path
 from ast import literal_eval
 
 # Init lists
 templates = []
-clusters=[]
-instances=[]
-bastions=[]
-nodes=[]
-masters=[]
-output=[]
+clusters = []
+instances = []
+bastions = []
+nodes = []
+masters = []
+output = []
 
-# Json output configuration                                                                                                                                          
+# Json output configuration
+
+
 class literal(str):
     pass
+
+
 def literal_presenter(dumper, data):
     return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
 
 # Function to fetch a value from the terraform dictionary
+
+
 def tf(key):
-  if terraform[key]['value']:
-    return terraform[key]['value']
-  else:
-    sys.exit(key + ' not found, exit.')
+    if terraform[key]['value']:
+        return terraform[key]['value']
+    else:
+        sys.exit(key + ' not found, exit.')
+
 
 # Dumper config
 yaml.SafeDumper.add_representer(literal, literal_presenter)
@@ -32,25 +45,28 @@ yaml.SafeDumper.ignore_aliases = lambda *args: True
 # Parser config
 p = pprint.PrettyPrinter(indent=4)
 parser = argparse.ArgumentParser(description='Create Kubernetes clusters.')
-parser.add_argument('clusterconfig', help='the base yaml file to create the cluster from')
+parser.add_argument(
+    'clusterconfig',
+    help='the base yaml file to create the cluster from')
 
 args = parser.parse_args()
 #print( 'loading file ' + args.clusterconfig)
 
 # Load the base kops yaml file located in the repo
-if os.path.isfile(args.clusterconfig): 
-  stream = open(args.clusterconfig)
-  for template in yaml.load_all(stream):
-    templates.append(template)
-  stream.close()
+if os.path.isfile(args.clusterconfig):
+    stream = open(args.clusterconfig)
+    for template in yaml.load_all(stream):
+        templates.append(template)
+    stream.close()
 else:
-  sys.exit( args.clusterconfig + ' file not found, exit.')
+    sys.exit(args.clusterconfig + ' file not found, exit.')
 
 # Load the terraform outputs from the terraform command
 try:
-  terraform = json.loads(str(subprocess.check_output(['terraform', 'output', '-json']).decode('utf-8')))
+    terraform = json.loads(str(subprocess.check_output(
+        ['terraform', 'output', '-json']).decode('utf-8')))
 except subprocess.CalledProcessError as e:
-  print('error executing terraform command, exit.')
+    print('error executing terraform command, exit.')
 
 # Populate variables from terraform output
 cluster_name = tf('cluster_domain_name')
@@ -65,73 +81,103 @@ external_subnets = tf('external_subnets')
 
 # Organize in lists by kind and role
 for template in templates:
-  if template['kind'] == 'Cluster':
-    clusters.append(template)
-  if template['kind'] == 'InstanceGroup':
-    instances.append(template)
-    if template['spec']['role'] == 'Bastion':
-      bastions.append(template)
-    if template['spec']['role'] == 'Node':
-      nodes.append(template)
-    if template['spec']['role'] == 'Master':
-      masters.append(template)
+    if template['kind'] == 'Cluster':
+        clusters.append(template)
+    if template['kind'] == 'InstanceGroup':
+        instances.append(template)
+        if template['spec']['role'] == 'Bastion':
+            bastions.append(template)
+        if template['spec']['role'] == 'Node':
+            nodes.append(template)
+        if template['spec']['role'] == 'Master':
+            masters.append(template)
 
 # Update all Cluster kind
 for template in clusters:
-  template['metadata'].update({'name': cluster_name })
-  policies = template['spec']['additionalPolicies']['node']                                                                                             
-  template['spec'].update({'additionalPolicies': { 'node': literal(policies) } } )
-  template['spec'].update({'configBase': kops_state_store })
-  template['spec'].update({'dnsZone': dns_zone })
+    template['metadata'].update({'name': cluster_name})
+    policies = template['spec']['additionalPolicies']['node']
+    template['spec'].update(
+        {'additionalPolicies': {'node': literal(policies)}})
+    template['spec'].update({'configBase': kops_state_store})
+    template['spec'].update({'dnsZone': dns_zone})
 
-  etcdclusters = []
-  etcdmembers = []
-  for az in availability_zones:
-    etcdmembers.append({'instanceGroup': 'master-' + az, 'name': az[-1:] })
-  for name in ['main', 'events']:
-    etcdclusters.append({'name': name, 'etcdMembers': etcdmembers })
-  template['spec'].update({'etcdClusters': etcdclusters })
+    etcdclusters = []
+    etcdmembers = []
+    for az in availability_zones:
+        etcdmembers.append({'instanceGroup': 'master-' + az, 'name': az[-1:]})
+    for name in ['main', 'events']:
+        etcdclusters.append({'name': name, 'etcdMembers': etcdmembers})
+    template['spec'].update({'etcdClusters': etcdclusters})
 
-  template['spec'].update({'masterPublicName': master_public_name })
-  template['spec'].update({'networkCIDR': network_cidr })
-  template['spec'].update({'topology': { 'bastion': { 'bastionPublicName': topology }, 'dns': { 'type': 'Public' }, 'masters': 'private', 'nodes': 'private' } } )
+    template['spec'].update({'masterPublicName': master_public_name})
+    template['spec'].update({'networkCIDR': network_cidr})
+    template['spec'].update({
+        'topology': {
+            'bastion': {
+                'bastionPublicName': topology
+            },
+            'dns': {
+                'type': 'Public'
+            },
+            'masters': 'private',
+            'nodes': 'private'
+        }
+    })
 
-  subnets = []
-  if len(internal_subnets) == len(external_subnets) == len(availability_zones):
-    for i in range(len(external_subnets)):
-      subnets.append( { 'cidr': external_subnets[i], 'name': availability_zones[i], 'type': 'Private', 'zone': availability_zones[i] } )
-    for i in range(len(internal_subnets)):
-      subnets.append( { 'cidr': internal_subnets[i], 'name': 'utility-' + availability_zones[i], 'type': 'Utility', 'zone': availability_zones[i] } )
-  template['spec'].update({ 'subnets': subnets })
-  output.append(template)
+    subnets = []
+    if len(internal_subnets) == len(
+            external_subnets) == len(availability_zones):
+        for i in range(len(external_subnets)):
+            subnets.append(
+                {
+                    'cidr': external_subnets[i],
+                    'name': availability_zones[i],
+                    'type': 'Private',
+                    'zone': availability_zones[i]})
+        for i in range(len(internal_subnets)):
+            subnets.append(
+                {
+                    'cidr': internal_subnets[i],
+                    'name': 'utility-' + availability_zones[i],
+                    'type': 'Utility',
+                    'zone': availability_zones[i]})
+    template['spec'].update({'subnets': subnets})
+    output.append(template)
 
 # Update all instanceGroup kind
 for template in instances:
-  template['metadata'].update({'labels': { 'kops.k8s.io/cluster': cluster_name } } ) 
+    template['metadata'].update(
+        {'labels': {'kops.k8s.io/cluster': cluster_name}})
 
 # Update masters
 if len(masters) == len(availability_zones):
-  for i in range(len(masters)):
-    masters[i]['spec'].update({'subnets': [ availability_zones[i] ] } )
-    masters[i]['spec'].update({'nodeLabels': { 'kops.k8s.io/instancegroup': 'master-' + availability_zones[i] } } )
-    output.append(masters[i])
+    for i in range(len(masters)):
+        masters[i]['spec'].update({'subnets': [availability_zones[i]]})
+        masters[i]['spec'].update({
+            'nodeLabels': {
+                'kops.k8s.io/instancegroup': 'master-' + availability_zones[i]
+            }
+        })
+        output.append(masters[i])
 
 # Update nodes
 for template in nodes:
-  template['spec'].update({'subnets': availability_zones })
-  template['spec'].update({'nodeLabels': { 'kops.k8s.io/instancegroup' : 'nodes' } } )
-  output.append(template)
+    template['spec'].update({'subnets': availability_zones})
+    template['spec'].update(
+        {'nodeLabels': {'kops.k8s.io/instancegroup': 'nodes'}})
+    output.append(template)
 
 # Update bastions
 for template in bastions:
-  azs = []
-  for az in availability_zones:
-    azs.append('utility-' + az)
-  template['spec'].update({'subnets': azs })
-  template['spec'].update({'nodeLabels': { 'kops.k8s.io/instancegroup' : 'bastions' } } )
-  output.append(template)
+    azs = []
+    for az in availability_zones:
+        azs.append('utility-' + az)
+    template['spec'].update({'subnets': azs})
+    template['spec'].update(
+        {'nodeLabels': {'kops.k8s.io/instancegroup': 'bastions'}})
+    output.append(template)
 
-# Print outputs 
+# Print outputs
 for item in output:
-  print('---')
-  print(yaml.safe_dump(item,  default_flow_style = False ))
+    print('---')
+    print(yaml.safe_dump(item, default_flow_style=False))
--- bin/create_clusters.py
+++ bin/create_clusters.py
@@ -1,56 +1,64 @@
- #!/usr/bin/python
+#!/usr/bin/python
 
-import subprocess, json, shlex
+import subprocess
+import json
+import shlex
 
 statestore = 'moj-cp-k8s-investigation-kops'
 domain = '.k8s.integration.dsd.io'
 
+
 def run(command):
-  process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
-  while True:
-    output = process.stdout.readline()
-    if output == '' and process.poll() is not None:
-      break
-    if output:
-      print output.strip()
-  rc = process.poll()
-  return rc
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print output.strip()
+    rc = process.poll()
+    return rc
+
 
 # terraform init
 try:
-  print(subprocess.check_output(['terraform', 'init']))
+    print(subprocess.check_output(['terraform', 'init']))
 except subprocess.CalledProcessError as e:
-  print('error initing terraform, exit.')
+    print('error initing terraform, exit.')
 
 # get terraform workspace list
 try:
-  workspaces = subprocess.check_output(['terraform', 'workspace', 'list']).decode('utf-8').split()
-  workspaces.remove('*')
+    workspaces = subprocess.check_output(
+        ['terraform', 'workspace', 'list']).decode('utf-8').split()
+    workspaces.remove('*')
 except subprocess.CalledProcessError as e:
-  print('error listing workspaces, exit.')
+    print('error listing workspaces, exit.')
 
 try:
-  yaml = [ s.strip('.yaml') for s in subprocess.check_output(['ls', '../../kops/']).decode('utf-8').split() ]
+    yaml = [s.strip('.yaml') for s in subprocess.check_output(
+        ['ls', '../../kops/']).decode('utf-8').split()]
 except subprocess.CalledProcessError as e:
-  print('error listing cluster yaml files, exit.')
+    print('error listing cluster yaml files, exit.')
 
 # query the kops state store to list the active clusters
 try:
-  clusters = json.loads(str(subprocess.check_output(['kops', 'get', 'clusters', '--state=s3://' + statestore, '--output=json' ]).decode('utf-8')))
+    clusters = json.loads(str(subprocess.check_output(
+        ['kops', 'get', 'clusters', '--state=s3://' + statestore,
+         '--output=json']).decode('utf-8')))
 except subprocess.CalledProcessError as e:
-  print('error listing clusters, exit.')
+    print('error listing clusters, exit.')
 
 # generate a list of cluster names only
-clusterlist =[]
+clusterlist = []
 for item in clusters:
-  clusterlist.append(item['metadata']['name'].replace(domain,''))
+    clusterlist.append(item['metadata']['name'].replace(domain, ''))
 
 # create the corresponding clusters
 for item in yaml:
-  if item in workspaces and item in clusterlist:
-    print('cluster ' + item + ' already exists, skipping')
-  else:
-    print('creating cluster ' + item)
-    run('../../bin/create_cluster.sh ' + item)
+    if item in workspaces and item in clusterlist:
+        print('cluster ' + item + ' already exists, skipping')
+    else:
+        print('creating cluster ' + item)
+        run('../../bin/create_cluster.sh ' + item)
 
 print('Done.')
