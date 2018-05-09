56c3bc757b823a249eebacef1dc6cc991792a7e6
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-05-09 15:32:51 +0100

    Moved AWS federation vars to global-resources

--- terraform/cloud-platform/variables.tf
+++ terraform/cloud-platform/variables.tf
@@ -1,8 +1,3 @@
 variable "region" {
     default = "eu-west-1"
 }
-
-variable "aws_federation_saml_x509_cert" {}
-variable "aws_federation_saml_idp_domain" {}
-variable "aws_federation_saml_login_url" {} 
-variable "aws_federation_saml_logout_url" {}
