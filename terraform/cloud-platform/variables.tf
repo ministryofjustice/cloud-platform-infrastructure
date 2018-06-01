1883bd569d1a46c3bfcb742c95620136cb7e7a02
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-06-01 12:50:00 +0100

    adjust subnet CIDR

--- terraform/cloud-platform/variables.tf
+++ terraform/cloud-platform/variables.tf
@@ -6,13 +6,13 @@ variable "vpc_cidr" {
 variable "internal_subnets" {
   type        = "list"
   description = "list of subnet CIDR blocks that are not publicly acceessibly"
-  default     = ["172.20.0.0/22", "172.20.4.0/22", "172.20.8.0/22"]
+  default     = ["172.20.32.0/19", "172.20.64.0/19", "172.20.96.0/19"]
 }
 
 variable "external_subnets" {
   type        = "list"
   description = "list of subnet CIDR blocks that are publicly acceessibly"
-  default     = ["172.20.32.0/19", "172.20.64.0/19", "172.20.96.0/19"]
+  default     = ["172.20.0.0/22", "172.20.4.0/22", "172.20.8.0/22"]
 }
 
 variable "availability_zones" {
