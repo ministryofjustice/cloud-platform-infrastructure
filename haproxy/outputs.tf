75447d3e95ec42497393736f8ac665a5569ca032
Nayo Akinyele <nayoa@hotmail.co.uk>
2018-10-23 13:40:49 +0100

    terraform fmt

--- haproxy/outputs.tf
+++ haproxy/outputs.tf
@@ -9,6 +9,7 @@ output "Public Key Pem" {
 output "Private Key Pem" {
   value = "${tls_private_key.hapee_private_key.private_key_pem}"
 }
+
 output "Key Pair Fingerprint" {
- value = "${aws_key_pair.hapee_key_pair.fingerprint}"
+  value = "${aws_key_pair.hapee_key_pair.fingerprint}"
 }
