62d68f6eab571e66be9355b630500d10faf744f3
Nayo Akinyele <nayoa@hotmail.co.uk>
2018-10-23 13:38:54 +0100

    Add SSH key and Key Pair for EC2. Output relevant information

--- haproxy/outputs.tf
+++ haproxy/outputs.tf
@@ -1,3 +1,14 @@
 output "ELB DNS address" {
   value = "${aws_elb.hapee_elb.dns_name}"
 }
+
+output "Public Key Pem" {
+  value = "${tls_private_key.hapee_private_key.public_key_pem}"
+}
+
+output "Private Key Pem" {
+  value = "${tls_private_key.hapee_private_key.private_key_pem}"
+}
+output "Key Pair Fingerprint" {
+ value = "${aws_key_pair.hapee_key_pair.fingerprint}"
+}
