575ecf9b289ca461cde4025bf7e225e10ca2caed
ollieanwyll <oliver.anwyll@digital.justice.gov.uk>
2018-11-05 11:09:18 +0000

    Add serviceAccount

--- terraform/cloud-platform-components/fluentd.tf
+++ terraform/cloud-platform-components/fluentd.tf
@@ -1,1 +1,11 @@
-# PLACEHOLD
+resource "kubernetes_service_account" "fluentd_service_account" {
+  metadata {
+    name = "fluentd-es"
+    namespace = "logging"
+    labels {
+        k8s-app = "fluentd-es"
+        kubernetes.io/cluster-service = "true"
+        addonmanager.kubernetes.io/mode = "Reconcile"
+    }
+  }
+}
