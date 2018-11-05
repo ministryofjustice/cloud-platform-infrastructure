f15a9765ec3a1cad62a2118bf1474ebd9552dcea
ollieanwyll <oliver.anwyll@digital.justice.gov.uk>
2018-11-05 16:08:22 +0000

    Terraform fmt

--- terraform/cloud-platform-components/fluentd.tf
+++ terraform/cloud-platform-components/fluentd.tf
@@ -1,29 +1,29 @@
 resource "helm_release" "fluentd_es" {
-    name      = "fluentd-es"
-    chart     = "resources/fluentd-es"
+  name  = "fluentd-es"
+  chart = "resources/fluentd-es"
 
-    set {
-        name  = "namespace"
-        value = "logging"
-    }
+  set {
+    name  = "namespace"
+    value = "logging"
+  }
 
-    set {
-        name = "version"
-        value = "v2.2.0"
-    }
+  set {
+    name  = "version"
+    value = "v2.2.0"
+  }
 
-    set {
-        name = "FLUENT_ELASTICSEARCH_HOST"
-        value = "https://search-cloud-platform-live-7qrzc26xexgxtkt5qz72gt6cxa.eu-west-1.es.amazonaws.com"
-    }
+  set {
+    name  = "FLUENT_ELASTICSEARCH_HOST"
+    value = "https://search-cloud-platform-live-7qrzc26xexgxtkt5qz72gt6cxa.eu-west-1.es.amazonaws.com"
+  }
 
-    set {
-        name = "FLUENT_ELASTICSEARCH_AUDIT_HOST"
-        value = "search-cloud-platform-audit-effm3qdiau42obkarrpvdxioxm.eu-west-1.es.amazonaws.com"
-    }
+  set {
+    name  = "FLUENT_ELASTICSEARCH_AUDIT_HOST"
+    value = "search-cloud-platform-audit-effm3qdiau42obkarrpvdxioxm.eu-west-1.es.amazonaws.com"
+  }
 
-    set {
-        name = "FLUENT_KUBERNETES_CLUSTER_NAME"
-        value = "${terraform.workspace}"
-    }
+  set {
+    name  = "FLUENT_KUBERNETES_CLUSTER_NAME"
+    value = "${terraform.workspace}"
+  }
 }
