e013669522c1e29c5397559ced51c8d46ce7c5df
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-11-08 13:53:41 +0000

    Remove the cert hostname from haproxy

--- haproxy/cert.tf
+++ haproxy/cert.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "${var.haproxy_host}.${var.haproxy_domain}"
+  domain_name       = "${var.haproxy_domain}"
   validation_method = "DNS"
 }
 
