5767560715423633d2076275992f396d16073dda
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-11-08 14:23:18 +0000

    Add S3 bucket state store ref and ran tf fmt

--- haproxy/main.tf
+++ haproxy/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "cloud-platform-haproxy-terraform"
+    region = "eu-west-1"
+    key    = "terraform.tfstate"
+  }
+}
+
 provider "aws" {
   region  = "${var.aws_region}"
   version = ">=1.34"
