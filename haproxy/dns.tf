930dffa162a5468409e3102942d25b0f7c1f14c1
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-11-08 12:18:32 +0000

    Amend dns entries for haproxy

--- haproxy/dns.tf
+++ haproxy/dns.tf
@@ -1,18 +1,6 @@
-resource "aws_route53_zone" "haproxy" {
-  name = "${var.haproxy_domain}"
-}
-
-resource "aws_route53_record" "parent" {
-  zone_id = "${var.parent_zone}"
-  type    = "NS"
-  name    = "${var.haproxy_domain}"
-  records = ["${aws_route53_zone.haproxy.name_servers}"]
-  ttl     = "300"
-}
-
 resource "aws_route53_record" "www" {
-  zone_id = "${aws_route53_zone.haproxy.zone_id}"
-  name    = "${var.haproxy_host}.${var.haproxy_domain}"
+  zone_id = "${var.parent_zone}"
+  name    = "${var.haproxy_domain}"
   type    = "CNAME"
   records = ["${aws_lb.haproxy_alb.dns_name}"]
   ttl     = "300"
