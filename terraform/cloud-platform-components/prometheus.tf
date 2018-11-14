8035c74a1257fcfd9494ce41d962fd5025926d6e
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-11-14 17:27:19 +0000

    Add shell prometheus terraform to automate monitoring
    Ticket https://github.com/ministryofjustice/cloud-platform/issues/468 requires the automation of Helm installs for cluster bootstrapping.

---
+++ terraform/cloud-platform-components/prometheus.tf
@@ -0,0 +1,74 @@
+resource "helm_repository" "coreos" {
+  name = "coreos"
+  url  = "https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/"
+}
+
+resource "kubernetes_storage_class" "prometheus" {
+  metadata {
+    name = "prometheus-storage"
+  }
+
+  storage_provisioner = "kubernetes.io/aws-ebs"
+
+  parameters {
+    type = "gp2"
+  }
+}
+
+resource "helm_release" "prometheus_operator" {
+  name          = "prometheus-operator"
+  chart         = "coreos/prometheus-operator"
+  namespace     = "monitoring-terraform"
+  recreate_pods = "true"
+
+  depends_on = [
+    "null_resource.deploy",
+    "helm_repository.coreos",
+  ]
+}
+
+resource "helm_release" "kube_prometheus" {
+  name          = "kube-prometheus"
+  chart         = "coreos/kube-prometheus"
+  namespace     = "monitoring-terraform"
+  recreate_pods = "true"
+
+  values = [
+    "${file("../../helm-charts/prometheus-operator/kube-prometheus/values.yaml")}",
+  ]
+
+  set {
+    name  = "grafana.extraVars[0].value"
+    value = "https://grafana.apps.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+
+  set {
+    name  = "grafana.ingress.host"
+    value = "https://grafana.apps.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+
+  set {
+    name  = "alertmanager.externalUrl"
+    value = "https://alertmanager.apps.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+
+  set {
+    name  = "prometheus.externalUrl"
+    value = "https://prometheus.apps.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+
+  set {
+    name  = "alertmanager.config.receivers[1].pagerduty_configs[0].service_key"
+    value = "${var.pager_duty_config}"
+  }
+
+  set {
+    name  = "alertmanager.config.receivers[2].slack_configs[0].api_url"
+    value = "${var.slack_config}"
+  }
+
+  depends_on = [
+    "null_resource.deploy",
+    "helm_repository.coreos",
+  ]
+}
