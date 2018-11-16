eb783210d9b28d88d015626b83a22eec4398ffc2
Sablu Miah <sablu.miah@digital.justice.gov.uk>
2018-11-16 15:26:53 +0000

    Add MetricsServer.tf

---
+++ terraform/cloud-platform-components/metris-server.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "metrics_server" {
+  name      = "metrics-server"
+  chart     = "stable/metrics-server"
+  namespace = "kube-system"
+  keyring   = ""
+  version   = "2.0.4"
+  
+  values = [<<EOF
+rbac:
+  create: true
+
+serviceaccount:
+  create: true
+
+apiservice:
+  create: true
+
+image:
+  repository: gcr.io/google_containers/metrics-server-amd64
+  tag: v0.3.1
+  pullpolicy: IfNotPresent
+
+args:
+  value[0]: "--kubelet-insecure-tls"
+  value[1]: "--logtostderr"
+
+EOF
+  ]
+
+}
