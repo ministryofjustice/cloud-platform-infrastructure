40e1c716514ada2ebb0b0ca811a0fe5bb38d9ee5
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-11-20 15:43:06 +0000

    Change relative path to use path.module

--- terraform/cloud-platform-components/psp.tf
+++ terraform/cloud-platform-components/psp.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "pod_security_policy" {
   provisioner "local-exec" {
-    command = "kubectl create -f ./resources/psp/pod-security-policy.yaml"
+    command = "kubectl create -f ${path.module}/resources/psp/pod-security-policy.yaml"
   }
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "kubectl delete -f ./resources/psp/pod-security-policy.yaml"
+    command = "kubectl delete -f ${path.module}/resources/psp/pod-security-policy.yaml"
   }
 }
