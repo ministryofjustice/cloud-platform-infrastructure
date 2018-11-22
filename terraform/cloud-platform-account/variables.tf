c47ea4891000bdf1344aa9e7ae1a5bb966fb4699
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-11-22 22:39:51 +0000

    Add cloudtrail tf and relevant variables file
    This will allow us to create a cloudtrail account in multiple accounts

---
+++ terraform/cloud-platform-account/variables.tf
@@ -0,0 +1,18 @@
+variable "team_name" {
+  description = "The team creating the resource"
+  default     = "cloud-platform"
+}
+
+variable "business_unit" {
+  description = " Area of the MOJ responsible for the service"
+  default     = "mojdigital"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form <team-name> (<team-email>)"
+  default     = "platform@digital.justice.gov.uk"
+}
+
+variable "cloudtrail_bucket_name" {
+  default = "cloud-platform-cloudtrail"
+}
