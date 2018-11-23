92f310e788111eb598d3f7d032599c16f6aef86a
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-11-23 16:23:57 +0000

    Merge elasticsearch state into global-state

--- terraform/global-resources/Kibana/README.md
+++
@@ -1,2 +0,0 @@
-This will deploy three AWS-managed ES domains (test, live and audit), with Kibana on a public IP and and access policy allowing only the k8s clusters (and the OIDc proxy that will be running on them)
-To redeploy, just `terraform plan && terraform apply`
--- terraform/global-resources/elasticsearch.tf
+++ terraform/global-resources/elasticsearch.tf
@@ -1,76 +1,22 @@
-terraform {
-  backend "s3" {
-    bucket = "cloud-platform-kibana"
-    region = "eu-west-1"
-    key    = "terraform.tfstate"
+locals {
+  live_domain = "cloud-platform-live"
+
+  allowed_live_ips = {
+    "54.229.250.233/32" = "test-1-a"
+    "54.229.139.68/32"  = "test-1-b"
+    "34.246.149.106/32" = "test-1-c"
+    "52.17.133.167/32"  = "live-0-a"
+    "34.247.134.240/32" = "live-0-b"
+    "34.251.93.81/32"   = "live-0-c"
   }
-}
 
-provider "aws" {
-  region = "eu-west-1"
-}
+  audit_domain = "cloud-platform-audit"
 
-data "terraform_remote_state" "global" {
-  backend = "s3"
-
-  config {
-    bucket = "cloud-platform-kibana"
-    region = "eu-west-1"
-    key    = "terraform.tfstate"
-  }
+  allowed_audit_ips = "${local.allowed_live_ips}"
 }
 
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
-
-resource "aws_elasticsearch_domain" "test" {
-  domain_name           = "${local.test_domain}"
-  elasticsearch_version = "6.2"
-
-  cluster_config {
-    instance_type  = "m4.large.elasticsearch"
-    instance_count = "3"
-  }
-
-  ebs_options {
-    ebs_enabled = "true"
-    volume_type = "gp2"
-    volume_size = "150"
-  }
-
-  advanced_options {
-    "rest.action.multi.allow_explicit_index" = "true"
-  }
-
-  access_policies = <<CONFIG
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "es:*",
-      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.test_domain}/*",
-      "Condition": {
-        "IpAddress": {
-          "aws:SourceIp": ${jsonencode(keys(local.allowed_test_ips))}
-        }
-      }
-    }
-  ]
-}
-CONFIG
-
-  snapshot_options {
-    automated_snapshot_start_hour = 23
-  }
-
-  tags {
-    Domain = "${local.test_domain}"
-  }
-}
 
 resource "aws_elasticsearch_domain" "live" {
   domain_name           = "${local.live_domain}"
@@ -127,7 +73,7 @@ resource "aws_elasticsearch_domain" "audit" {
 
   cluster_config {
     instance_type  = "m4.large.elasticsearch"
-    instance_count = "2"
+    instance_count = "3"
   }
 
   ebs_options {
