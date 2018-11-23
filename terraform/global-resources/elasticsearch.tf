6358cf18182fa359f0af937fa8a28227056d208a
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-11-23 16:48:32 +0000

    Rewrite IAM policies in HCL

--- terraform/global-resources/elasticsearch.tf
+++ terraform/global-resources/elasticsearch.tf
@@ -18,6 +18,32 @@ locals {
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
+data "aws_iam_policy_document" "live" {
+  statement {
+    actions = [
+      "es:*",
+    ]
+
+    resources = [
+      "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.live_domain}/*",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+
+      values = [
+        "${keys(local.allowed_live_ips)}",
+      ]
+    }
+  }
+}
+
 resource "aws_elasticsearch_domain" "live" {
   domain_name           = "${local.live_domain}"
   elasticsearch_version = "6.2"
@@ -37,26 +63,7 @@ resource "aws_elasticsearch_domain" "live" {
     "rest.action.multi.allow_explicit_index" = "true"
   }
 
-  access_policies = <<CONFIG
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "es:*",
-      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.live_domain}/*",
-      "Condition": {
-        "IpAddress": {
-          "aws:SourceIp": ${jsonencode(keys(local.allowed_live_ips))}
-        }
-      }
-    }
-  ]
-}
-CONFIG
+  access_policies = "${data.aws_iam_policy_document.live.json}"
 
   snapshot_options {
     automated_snapshot_start_hour = 23
@@ -64,6 +71,44 @@ CONFIG
 
   tags {
     Domain = "${local.live_domain}"
+  }
+}
+
+data "aws_iam_policy_document" "audit" {
+  statement {
+    actions = [
+      "es:AddTags",
+      "es:ESHttpHead",
+      "es:DescribeElasticsearchDomain",
+      "es:ESHttpPost",
+      "es:ESHttpGet",
+      "es:ESHttpPut",
+      "es:DescribeElasticsearchDomainConfig",
+      "es:ListTags",
+      "es:DescribeElasticsearchDomains",
+      "es:ListDomainNames",
+      "es:ListElasticsearchInstanceTypes",
+      "es:DescribeElasticsearchInstanceTypeLimits",
+      "es:ListElasticsearchVersions",
+    ]
+
+    resources = [
+      "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.audit_domain}/*",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+
+      values = [
+        "${keys(local.allowed_audit_ips)}",
+      ]
+    }
   }
 }
 
@@ -86,39 +131,7 @@ resource "aws_elasticsearch_domain" "audit" {
     "rest.action.multi.allow_explicit_index" = "true"
   }
 
-  access_policies = <<CONFIG
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action":[ "es:AddTags",
-                "es:ESHttpHead",
-                "es:DescribeElasticsearchDomain",
-                "es:ESHttpPost",
-                "es:ESHttpGet",
-                "es:ESHttpPut",
-                "es:DescribeElasticsearchDomainConfig",
-                "es:ListTags",
-                "es:DescribeElasticsearchDomains",
-                "es:ListDomainNames",
-                "es:ListElasticsearchInstanceTypes",
-                "es:DescribeElasticsearchInstanceTypeLimits",
-                "es:ListElasticsearchVersions"
-      ],
-      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.audit_domain}/*",
-      "Condition": {
-        "IpAddress": {
-          "aws:SourceIp": ${jsonencode(keys(local.allowed_audit_ips))}
-        }
-      }
-    }
-  ]
-}
-CONFIG
+  access_policies = "${data.aws_iam_policy_document.audit.json}"
 
   snapshot_options {
     automated_snapshot_start_hour = 23
