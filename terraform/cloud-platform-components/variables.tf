f50acb86f9cd52838b8d4b2b672d3b09259b1590
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-02-05 19:01:20 +0000

    Add Grafana secret keys for interpolation

--- terraform/cloud-platform-components/variables.tf
+++ terraform/cloud-platform-components/variables.tf
@@ -5,3 +5,15 @@ variable "pagerduty_config" {
 variable "slack_config" {
   description = "Add Slack webhook API URL and channel for integration with slack."
 }
+
+variable "github_client_id" {
+  description = "ClientID of GitHub Oauth application"
+}
+
+variable "github_client_secret" {
+  description = "Client secret of GitHub Oauth application"
+}
+
+variable "github_secret_key" {
+  description = "Client secret of GitHub Oauth application"
+}
