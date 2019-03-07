aab7bde2180b4a1d22e60b4d210e831b09df91dd
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-03-07 20:40:24 +0000

    Add tls rule to kuberos
    Allowing cert manager to assign the default cert.

--- terraform/cloud-platform-components/kuberos.tf
+++ terraform/cloud-platform-components/kuberos.tf
@@ -23,6 +23,45 @@ oidc:
 replicaCount: 2
 EOF
   ]
+  set {
+    name  = "ingress.host"
+    value = "login.apps.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+  
+  set {
+    name  = "ingress.tls.secretName.host"
+    value = "login.apps.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+
+  set {
+    name  = "cluster.name"
+    value = "${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+
+  set {
+    name  = "cluster.address"
+    value = "https://api.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  }
+
+  set {
+    name  = "oidc.issuerUrl"
+    value = "${data.terraform_remote_state.cluster.oidc_issuer_url}"
+  }
+
+  set {
+    name  = "oidc.clientId"
+    value = "${data.terraform_remote_state.cluster.oidc_kubernetes_client_id}"
+  }
+
+  set {
+    name  = "oidc.clientSecret"
+    value = "${data.terraform_remote_state.cluster.oidc_kubernetes_client_secret}"
+  }
+
+  set {
+    name  = "replicaCount"
+    value = "2"
+  }
 
   depends_on = ["null_resource.deploy"]
 
