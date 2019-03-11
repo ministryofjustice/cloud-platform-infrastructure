09ee887a1c31b6b501e3600663fc86d286d77628
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2019-03-11 13:34:36 +0000

    fmt

--- terraform/cloud-platform/main.tf
+++ terraform/cloud-platform/main.tf
@@ -98,13 +98,15 @@ resource "aws_key_pair" "cluster" {
 }
 
 resource "auth0_client" "kubernetes" {
-  name                 = "${local.cluster_name}:kubernetes"
-  description          = "Cloud Platform kubernetes"
-  app_type             = "regular_web"
-  callbacks            = [
+  name        = "${local.cluster_name}:kubernetes"
+  description = "Cloud Platform kubernetes"
+  app_type    = "regular_web"
+
+  callbacks = [
     "https://login.apps.${local.cluster_base_domain_name}/ui",
-    "https://kibana.apps.${local.cluster_base_domain_name}/oauth2/callback"
+    "https://kibana.apps.${local.cluster_base_domain_name}/oauth2/callback",
   ]
+
   custom_login_page_on = true
   is_first_party       = true
   oidc_conformant      = true
@@ -126,7 +128,7 @@ resource "auth0_client" "components" {
     "https://alertmanager.apps.${local.cluster_base_domain_name}/oauth2/callback",
     "https://prometheus.apps.${local.cluster_base_domain_name}/redirect_uri",
     "https://alertmanager.apps.${local.cluster_base_domain_name}/redirect_uri",
-    "https://concourse.apps.${local.cluster_base_domain_name}/sky/issuer/callback"
+    "https://concourse.apps.${local.cluster_base_domain_name}/sky/issuer/callback",
   ]
 
   custom_login_page_on = true
