a27415dd3b079a71e7c55b2a75e22d3ff6ca2e94
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2019-03-15 12:26:53 +0000

    bastion typo

--- terraform/cloud-platform/resources/bastion/configure_bastion.sh
+++ terraform/cloud-platform/resources/bastion/configure_bastion.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-aws ec2 --region "${aws_region}" associate-address --instance-id "$$(curl -s http://169.254.169.254/latest/meta-data/instance-id)" --allocation-id "${eip_id}"
+aws ec2 --region "${aws_region}" associate-address --instance-id "$(curl -s http://169.254.169.254/latest/meta-data/instance-id)" --allocation-id "${eip_id}"
 
 systemctl daemon-reload
 systemctl enable authorized-keys-manager.service
