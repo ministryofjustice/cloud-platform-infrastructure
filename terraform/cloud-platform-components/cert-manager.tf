ae0adff571c4e6b28b57628d5e07e8f2df180645
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-04-12 10:30:22 +0100

    Updated default-provider to route53-cloud-platform
    This is to enable cert-manger to create Certificate and secret when Ingress is applied with annotation: certmanager.k8s.io/cluster-issuer: letsencrypt-production

--- terraform/cloud-platform-components/cert-manager.tf
+++ terraform/cloud-platform-components/cert-manager.tf
@@ -84,7 +84,7 @@ ingressShim:
   defaultIssuerName: letsencrypt-production
   defaultIssuerKind: ClusterIssuer
   defaultACMEChallengeType: dns01
-  defaultACMEDNS01ChallengeProvider: route53
+  defaultACMEDNS01ChallengeProvider: route53-cloud-platform
 
 securityContext:
   enabled: false
