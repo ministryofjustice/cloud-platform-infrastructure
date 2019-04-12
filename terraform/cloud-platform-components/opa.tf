31f4e794cc12fd11a4004c7db16f7424da191dc2
Vijay Veeranki <46675900+vijay-veeranki-moj@users.noreply.github.com>
2019-04-12 17:05:45 +0100

    Revert "Revert "Install Open-policy-agent and create a ingress-conflicts policy.""

---
+++ terraform/cloud-platform-components/opa.tf
@@ -0,0 +1,32 @@
+data "template_file" "values" {
+  template = "${file("${path.module}/templates/opa/values.yaml.tpl")}"
+}
+
+resource "helm_release" "open-policy-agent" {
+  name       = "opa"
+  namespace  = "opa"
+  repository = "stable"
+  chart      = "opa"
+  version    = "1.3.2"
+
+  values = [
+    "${data.template_file.values.rendered}",
+  ]
+
+  lifecycle {
+    ignore_changes = ["keyring"]
+  }
+}
+
+resource "null_resource" "open-policy-agent_policies" {
+  depends_on = ["helm_release.open-policy-agent"]
+
+  provisioner "local-exec" {
+    command = "kubectl apply -n opa -f ${path.module}/resources/opa/"
+  }
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete -n opa --ignore-not-found -f ${path.module}/resources/opa/"
+  }
+}
