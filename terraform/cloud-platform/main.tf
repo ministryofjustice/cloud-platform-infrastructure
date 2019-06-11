35354c043952d3fe59f5329d7eb3f30142f6885b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-06-11 15:47:59 +0100

    Run terraform 0.12upgrade command + remove auth0
    We had to remove auth0 here as the 3rd party provider doesn't work on tf 0.12

--- terraform/cloud-platform/main.tf
+++ terraform/cloud-platform/main.tf
@@ -15,22 +15,16 @@ provider "aws" {
 }
 
 #
-# See instructions here: https://github.com/ministryofjustice/kubernetes-investigations/tree/master/auth0
-#              and here: https://github.com/yieldr/terraform-provider-auth0
 #
 # The empty configuration assumes that you have the appropriate environment
 # variables exported as explained in the upstream repo and is similar to the way
 # the AWS providr credentials are handled.
 #
-provider "auth0" {
-  version = ">= 0.1.18"
-  domain  = "${local.auth0_tenant_domain}"
-}
 
 data "terraform_remote_state" "global" {
   backend = "s3"
 
-  config {
+  config = {
     bucket  = "cloud-platform-terraform-state"
     region  = "eu-west-1"
     key     = "global-resources/terraform.tfstate"
@@ -39,10 +33,8 @@ data "terraform_remote_state" "global" {
 }
 
 locals {
-  cluster_name             = "${terraform.workspace}"
+  cluster_name             = terraform.workspace
   cluster_base_domain_name = "${local.cluster_name}.cloud-platform.service.justice.gov.uk"
-  auth0_tenant_domain      = "justice-cloud-platform.eu.auth0.com"
-  oidc_issuer_url          = "https://${local.auth0_tenant_domain}/"
 
   live_workspace = "live-1"
   live_domain    = "cloud-platform.service.justice.gov.uk"
@@ -51,23 +43,23 @@ locals {
 # Modules
 module "cluster_dns" {
   source                   = "../modules/cluster_dns"
-  cluster_base_domain_name = "${local.cluster_base_domain_name}"
-  parent_zone_id           = "${data.terraform_remote_state.global.cp_zone_id}"
+  cluster_base_domain_name = local.cluster_base_domain_name
+  parent_zone_id           = data.terraform_remote_state.global.outputs.cp_zone_id
 }
 
 module "cluster_ssl" {
   source                   = "../modules/cluster_ssl"
-  cluster_base_domain_name = "${local.cluster_base_domain_name}"
-  dns_zone_id              = "${module.cluster_dns.cluster_dns_zone_id}"
+  cluster_base_domain_name = local.cluster_base_domain_name
+  dns_zone_id              = module.cluster_dns.cluster_dns_zone_id
 }
 
 module "cluster_vpc" {
   source               = "terraform-aws-modules/vpc/aws"
-  name                 = "${local.cluster_name}"
-  cidr                 = "${var.vpc_cidr}"
-  azs                  = "${var.availability_zones}"
-  private_subnets      = "${var.internal_subnets}"
-  public_subnets       = "${var.external_subnets}"
+  name                 = local.cluster_name
+  cidr                 = var.vpc_cidr
+  azs                  = var.availability_zones
+  private_subnets      = var.internal_subnets
+  public_subnets       = var.external_subnets
   enable_nat_gateway   = true
   enable_vpn_gateway   = false
   enable_dns_hostnames = true
@@ -86,8 +78,8 @@ module "cluster_vpc" {
 
   tags = {
     Terraform = "true"
-    Cluster   = "${local.cluster_name}"
-    Domain    = "${local.cluster_base_domain_name}"
+    Cluster   = local.cluster_name
+    Domain    = local.cluster_base_domain_name
   }
 }
 
@@ -97,51 +89,7 @@ resource "tls_private_key" "cluster" {
 }
 
 resource "aws_key_pair" "cluster" {
-  key_name   = "${local.cluster_base_domain_name}"
-  public_key = "${tls_private_key.cluster.public_key_openssh}"
+  key_name   = local.cluster_base_domain_name
+  public_key = tls_private_key.cluster.public_key_openssh
 }
 
-resource "auth0_client" "kubernetes" {
-  name        = "${local.cluster_name}:kubernetes"
-  description = "Cloud Platform kubernetes"
-  app_type    = "regular_web"
-
-  callbacks = [
-    "${terraform.workspace == local.live_workspace ? format("https://login.%s/ui", local.live_domain) : "https://login.apps.${local.cluster_base_domain_name}/ui"}",
-  ]
-
-  custom_login_page_on = true
-  is_first_party       = true
-  oidc_conformant      = true
-  sso                  = true
-
-  jwt_configuration = {
-    alg                 = "RS256"
-    lifetime_in_seconds = "2592000"
-  }
-}
-
-resource "auth0_client" "components" {
-  name        = "${local.cluster_name}:components"
-  description = "Cloud Platform components"
-  app_type    = "regular_web"
-
-  callbacks = [
-    "${format("https://prometheus.%s/oauth2/callback", terraform.workspace == local.live_workspace ? local.live_domain : "apps.${local.cluster_base_domain_name}")}",
-    "${format("https://alertmanager.%s/oauth2/callback", terraform.workspace == local.live_workspace ? local.live_domain : "apps.${local.cluster_base_domain_name}")}",
-    "${format("https://concourse.%s/sky/issuer/callback", terraform.workspace == local.live_workspace ? local.live_domain : "apps.${local.cluster_base_domain_name}")}",
-    "${format("https://kibana.%s/oauth2/callback", terraform.workspace == local.live_workspace ? local.live_domain : "apps.${local.cluster_base_domain_name}")}",
-    "${format("https://kibana-audit.%s/oauth2/callback", terraform.workspace == local.live_workspace ? local.live_domain : "apps.${local.cluster_base_domain_name}")}",
-    "${format("https://grafana.%s/login/generic_oauth", terraform.workspace == local.live_workspace ? local.live_domain : "apps.${local.cluster_base_domain_name}")}",
-  ]
-
-  custom_login_page_on = true
-  is_first_party       = true
-  oidc_conformant      = true
-  sso                  = true
-
-  jwt_configuration = {
-    alg                 = "RS256"
-    lifetime_in_seconds = "36000"
-  }
-}
