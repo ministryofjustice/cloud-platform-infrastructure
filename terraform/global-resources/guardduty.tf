e3cecd77b1de37722faee0591c4daca17815d6a8
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-06-21 15:06:00 +0100

    Add missing S3 SSE config

--- terraform/global-resources/guardduty.tf
+++ terraform/global-resources/guardduty.tf
@@ -22,6 +22,14 @@ resource "aws_s3_bucket" "security" {
     prevent_destroy = true
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = "${merge(map("Name","Security Bucket"), var.tags)}"
 }
 
