e05c4ca8a6358db54c36053e60185cd20bc5e9d0
Mourad T <mourad.trabelsi@digital.justice.gov.uk>
2019-07-02 16:57:38 +0100

    add DUMMY_ELASTICSEARCH var, add ES helm release, replace test ES endpoint

--- terraform/cloud-platform-components/fluentd.tf
+++ terraform/cloud-platform-components/fluentd.tf
@@ -5,7 +5,7 @@ resource "helm_release" "fluentd_es" {
 
   set {
     name  = "fluent_elasticsearch_host"
-    value = "${replace(terraform.workspace, "live", "") != terraform.workspace ? "search-cloud-platform-live-dibidbfud3uww3lpxnhj2jdws4.eu-west-2.es.amazonaws.com" : "search-cloud-platform-test-zradqd7twglkaydvgwhpuypzy4.eu-west-2.es.amazonaws.com"}"
+    value = "${replace(terraform.workspace, "live", "") != terraform.workspace ? "search-cloud-platform-live-dibidbfud3uww3lpxnhj2jdws4.eu-west-2.es.amazonaws.com" : "${helm.helm_release.dummy_elasticsearch.name}-elasticsearch-client.logging.svc"}"
   }
 
   set {
@@ -23,4 +23,17 @@ resource "helm_release" "fluentd_es" {
   lifecycle {
     ignore_changes = ["keyring"]
   }
+}
+
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+resource "helm_release" "dummy_elasticsearch" {
+  name       = "dummy"
+  count      = "${var.DUMMY_ELASTICSEARCH}"
+  repository = "${data.helm_repository.stable.metadata.0.name}"
+  chart      = "elasticsearch"
+  namespace  = "logging"
 }
--- terraform/cloud-platform-components/variables.tf
+++ terraform/cloud-platform-components/variables.tf
@@ -6,3 +6,8 @@ variable "alertmanager_slack_receivers" {
   description = "A list of configuration values for Slack receivers"
   type        = "list"
 }
+
+variable "DUMMY_ELASTICSEARCH" {
+  description = "In case the cluster doesn't need to connect to an actual ES cluster, enable this var"
+  default     = 0
+}
