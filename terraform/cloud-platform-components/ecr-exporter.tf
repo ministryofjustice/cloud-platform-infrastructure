7ce70b91fb535b452dff847033f5d56d7afe3ca5
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-07-03 15:36:07 +0100

    Add ecr-exporter as a component for clusters

---
+++ terraform/cloud-platform-components/ecr-exporter.tf
@@ -0,0 +1,63 @@
+data "aws_iam_policy_document" "ecr_exporter_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${data.aws_iam_role.nodes.arn}"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecr_exporter" {
+  name               = "ecr-exporter.${data.terraform_remote_state.cluster.cluster_domain_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.ecr_exporter_assume.json}"
+}
+
+data "aws_iam_policy_document" "ecr_exporter" {
+  statement {
+    actions = [
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "ecr_exporter" {
+  name   = "ecr-exporter"
+  role   = "${aws_iam_role.ecr_exporter.id}"
+  policy = "${data.aws_iam_policy_document.ecr_exporter.json}"
+}
+
+resource "helm_release" "ecr_exporter" {
+  name      = "ecr-exporter"
+  count     = "${terraform.workspace == local.live_workspace ? 1 : 0}"
+  namespace = "monitoring"
+  chart     = "../../helm-charts/prometheus-ecr-exporter"
+
+  set {
+    name  = "serviceMonitor.enabled"
+    value = true
+  }
+
+  set {
+    name  = "aws.role"
+    value = "${aws_iam_role.ecr_exporter.name}"
+  }
+
+  set {
+    name  = "aws.region"
+    value = "eu-west-2"
+  }
+
+  depends_on = [
+    "null_resource.deploy",
+    "helm_release.prometheus_operator",
+  ]
+
+  lifecycle {
+    ignore_changes = ["keyring"]
+  }
+}
