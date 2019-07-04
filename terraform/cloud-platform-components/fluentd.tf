1a8fac5fbed585d6a04a416b09242db2384a54d0
Mourad T <mourad.trabelsi@digital.justice.gov.uk>
2019-07-04 10:18:31 +0100

    remove ES chart, comment out test cluster endpoint

--- terraform/cloud-platform-components/fluentd.tf
+++ terraform/cloud-platform-components/fluentd.tf
@@ -5,7 +5,9 @@ resource "helm_release" "fluentd_es" {
 
   set {
     name  = "fluent_elasticsearch_host"
-    value = "${replace(terraform.workspace, "live", "") != terraform.workspace ? "search-cloud-platform-live-dibidbfud3uww3lpxnhj2jdws4.eu-west-2.es.amazonaws.com" : "dummy-elasticsearch-client.logging.svc"}"
+    value = "${replace(terraform.workspace, "live", "") != terraform.workspace ? "search-cloud-platform-live-dibidbfud3uww3lpxnhj2jdws4.eu-west-2.es.amazonaws.com" : "placeholder-elasticsearch"}"
+    # if you need to connect to the test elasticsearch cluster, replace "placeholder-elasticsearch" with "search-cloud-platform-test-zradqd7twglkaydvgwhpuypzy4.eu-west-2.es.amazonaws.com"
+    # Your cluster will need to be whitelisted.
   }
 
   set {
@@ -18,22 +20,9 @@ resource "helm_release" "fluentd_es" {
     value = "${terraform.workspace}"
   }
 
-  depends_on = ["null_resource.deploy", "null_resource.priority_classes", "helm_release.dummy_elasticsearch"]
+  depends_on = ["null_resource.deploy", "null_resource.priority_classes"]
 
   lifecycle {
     ignore_changes = ["keyring"]
   }
-}
-
-data "helm_repository" "stable" {
-  name = "stable"
-  url  = "https://kubernetes-charts.storage.googleapis.com"
-}
-
-resource "helm_release" "dummy_elasticsearch" {
-  name       = "dummy"
-  count      = "${var.DUMMY_ELASTICSEARCH}"
-  repository = "${data.helm_repository.stable.metadata.0.name}"
-  chart      = "elasticsearch"
-  namespace  = "logging"
 }
