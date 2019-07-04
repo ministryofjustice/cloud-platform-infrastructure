0a3b28f77c5e30fd4bc6bdc2614f4c7a9aa0c73c
Mourad T <mourad.trabelsi@digital.justice.gov.uk>
2019-07-04 10:17:57 +0100

    remove sleep

--- terraform/cloud-platform-components/helm.tf
+++ terraform/cloud-platform-components/helm.tf
@@ -31,7 +31,7 @@ resource "null_resource" "deploy" {
   ]
 
   provisioner "local-exec" {
-    command = "helm init --wait --service-account tiller; sleep 60"
+    command = "helm init --wait --service-account tiller"
   }
 
   provisioner "local-exec" {
