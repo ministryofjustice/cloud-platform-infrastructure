dbfc71ec6bafff1c3f395b79012e3a37263cd0d6
Poornima Krishnasamy <51324851+poornima-moj@users.noreply.github.com>
2019-08-05 15:29:07 +0100

    IAM role and cronjob for cluster backup script (#333)
    * Created IAM role, policy and added cronjob

--- terraform/cloud-platform-components/cluster-backup-checker.tf
+++ terraform/cloud-platform-components/cluster-backup-checker.tf
@@ -1,5 +1,12 @@
 # Cluster backup snapshots checker
-# KIAM role creation
+# KIAM role and policy creation
+# monitoring namespace must have an annotation with a regular expression
+# expressing which roles are permitted to be assumed within that namespace.
+# Correct annotation are added to the Pod to indicate which role should be assumed.
+# Without correct Pod annotation Kiam cannot provide access to the Pod to execute required actions.
+# Cronjob to schedule the job every day @17:00 UTC to run the script mentioned in the image
+# 
+# Image - Ruby script added to Cloud platform ECR Repository
 data "aws_iam_policy_document" "cluster_backup_checker_assume" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -19,9 +26,7 @@ resource "aws_iam_role" "cluster_backup_checker" {
 data "aws_iam_policy_document" "cluster_backup_checker" {
   statement {
     actions = [
-      "ec2:CreateSnapshot",
-      "ec2:DeleteSnapshot",
-      "ec2:DescribeVolumes",
+      "sts:AssumeRole",
       "ec2:DescribeSnapshots",
     ]
 
@@ -33,4 +38,61 @@ resource "aws_iam_role_policy" "cluster_backup_checker" {
   name   = "cluster-checker"
   role   = "${aws_iam_role.cluster_backup_checker.id}"
   policy = "${data.aws_iam_policy_document.cluster_backup_checker.json}"
+}
+
+resource "kubernetes_cron_job" "cluster_backup_checker_cronjob" {
+  metadata {
+    name      = "cluster-backup-checker-cronjob"
+    namespace = "monitoring"
+  }
+
+  spec {
+    schedule = "0 17 * * *"
+
+    job_template {
+      metadata = {}
+
+      spec {
+        template {
+          metadata = {
+            annotations {
+              "iam.amazonaws.com/role" = "${aws_iam_role.cluster_backup_checker.name}"
+            }
+          }
+
+          spec {
+            container {
+              image = "${var.aws_master_account_id}.dkr.ecr.eu-west-2.amazonaws.com/cloud-platform/cluster-backup-checker:1.1"
+              name  = "snapshot-checker"
+
+              env {
+                name  = "SLACK_WEBHOOK"
+                value = "${var.cloud_platform_slack_webhook}"
+              }
+
+              env {
+                name  = "KUBERNETES_CLUSTER"
+                value = "${format("%s.%s", local.live_workspace, local.live_domain)}"
+              }
+
+              env {
+                name  = "AWS_REGION"
+                value = "eu-west-2"
+              }
+
+              env {
+                name  = "ROLE_NAME"
+                value = "${aws_iam_role.cluster_backup_checker.name}"
+              }
+
+              env {
+                name  = "ACCOUNT_ID"
+                value = "${var.aws_master_account_id}"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
--- terraform/cloud-platform-components/variables.tf
+++ terraform/cloud-platform-components/variables.tf
@@ -6,3 +6,9 @@ variable "alertmanager_slack_receivers" {
   description = "A list of configuration values for Slack receivers"
   type        = "list"
 }
+
+variable "aws_master_account_id" {}
+
+variable "cloud_platform_slack_webhook" {
+  description = "Slack webhook to pass it to  script to send alerts"
+}
