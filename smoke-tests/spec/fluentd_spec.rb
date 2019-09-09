b505ab8d4879abfb13b1965749168adaf4a4770f
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2019-09-09 14:16:12 +0100

    fluentd smoketest

---
+++ smoke-tests/spec/fluentd_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+# We want a fluentd pod running on each node, including masters
+describe "Log shipping" do
+
+  # gets the node IPs, then the 
+  it "runs fluentd" do
+    nodes = `kubectl get nodes -o json -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' --sort-by='.status.addresses[?(@.type=="InternalIP")].address'`.chomp
+    pods = `kubectl -n logging get pods -o json -o jsonpath='{..items[*].status.hostIP}' --field-selector status.phase='Running' --selector app=='fluentd-es' --sort-by='.status.hostIP'`.chomp
+    expect(nodes).to eq(pods)
+  end
+
+end
