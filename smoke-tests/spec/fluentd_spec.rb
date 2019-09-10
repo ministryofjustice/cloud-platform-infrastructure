76f5902ceada185e76a2e8dd2328d3fbb6aa9e87
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2019-09-10 09:56:56 +0100

    create helper funcs

--- smoke-tests/spec/fluentd_spec.rb
+++ smoke-tests/spec/fluentd_spec.rb
@@ -5,9 +5,9 @@ describe "Log shipping" do
 
   # gets all node IPs, then the fluentd pods, compares the lists
   it "runs fluentd" do
-    nodes = `kubectl get nodes -o json -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' --sort-by='.status.addresses[?(@.type=="InternalIP")].address'`.chomp
-    pods = `kubectl -n logging get pods -o json -o jsonpath='{..items[*].status.hostIP}' --field-selector status.phase='Running' --selector app=='fluentd-es' --sort-by='.status.hostIP'`.chomp
-    expect(nodes).to eq(pods)
+    cluster_nodes = get_cluster_ips()
+    app_nodes = get_app_nodes("logging", "fluentd-es")
+    expect(cluster_nodes).to eq(app_nodes)
   end
 
 end
