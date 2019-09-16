eab756a128446f4da9c3351c8b77c3c1d761d014
David Salgado <david.salgado@digital.justice.gov.uk>
2019-09-16 12:01:36 +0100

    Rename variables and adjust contexts
    * url1 -> good_url
    * url2 -> bad_url
    
    Move setup/teardown to before/after blocks
    
    Extract context setup so that tests are as
    minimally different as possible, to make them
    easy to read.

--- smoke-tests/spec/modsec_spec.rb
+++ smoke-tests/spec/modsec_spec.rb
@@ -3,51 +3,63 @@ require "spec_helper"
 describe "Testing modsec" do
   let(:namespace) { "smoketest-modsec-#{readable_timestamp}" }
   let(:host) { "#{namespace}.apps.#{current_cluster}" }
-  let(:url1) { "https://#{host}" }
-  let(:url2) { "https://#{host}?exec=/bin/bash" }
+  let(:good_url) { "https://#{host}" }
+  let(:bad_url) { "https://#{host}?exec=/bin/bash" }
   let(:ingress_name) { "modsec-smoketest-app-ing" }
 
-  context "deploy ingress with modsec" do
-    before do
-      create_namespace(namespace)
+  before do
+    create_namespace(namespace)
 
-      apply_template_file(
-        namespace: namespace,
-        file: "spec/fixtures/modsec-smoketest.yaml.erb",
-        binding: binding,
-      )
-      wait_for(namespace, "ingress", ingress_name)
-      sleep 10
+    apply_template_file(
+      namespace: namespace,
+      file: "spec/fixtures/modsec-smoketest.yaml.erb",
+      binding: binding,
+    )
+    wait_for(namespace, "ingress", ingress_name)
+    sleep 10
+  end
+
+  after do
+    delete_namespace(namespace)
+  end
+
+  context "when modsec deployed" do  # this is the default behaviour
+    context "when the url is benign" do
+      let(:url) { good_url }
+
+      specify "request succeeds" do
+        expect(URI.open(url).status).to eq(["200", "OK"])
+      end
     end
 
-  context "when modsec deployed" do
-    it "URL benign and request successful" do
-      result = URI.open(url1)
-      expect(result.status).to eq(["200", "OK"])
-      delete_namespace(namespace)
-    end
+    context "when the url is malicious" do
+      let(:url) { bad_url }
 
-    it "URL malicious and request blocked" do
-      expect { URI.open(url2) }.to raise_error(OpenURI::HTTPError, "403 Forbidden")
-      delete_namespace(namespace)
+      specify "request is blocked" do
+        expect { URI.open(url) }.to raise_error(OpenURI::HTTPError, "403 Forbidden")
+      end
     end
   end
-  
+
   context "when modsec disabled" do
-    it "URL benign and request successful" do
+    before do
       set_modsec_ing_annotation_false(namespace, ingress_name)
       sleep 5
-      result = URI.open(url1)
-      expect(result.status).to eq(["200", "OK"])
-      delete_namespace(namespace)
     end
 
-    it "URL malicious and request successful" do
-      set_modsec_ing_annotation_false(namespace, ingress_name)
-      sleep 5
-      result = URI.open(url2)
-      expect(result.status).to eq(["200", "OK"])
-      delete_namespace(namespace)
+    context "when the url is benign" do
+      let(:url) { good_url }
+
+      specify "request succeeds" do
+        expect(URI.open(url).status).to eq(["200", "OK"])
+      end
+    end
+
+    context "when the url is malicious" do
+      let(:url) { bad_url }
+
+      specify "request succeeds" do
+        expect(URI.open(url).status).to eq(["200", "OK"])
       end
     end
   end
