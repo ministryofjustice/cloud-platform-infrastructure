3abc01c64d6a1f1ef1ebd2193477a430e8b1f441
David Salgado <david.salgado@digital.justice.gov.uk>
2019-09-16 17:00:45 +0100

    WiP: refactor and clarify the rules
    The rules on custom error pages are very confusing
    This change tries to write the tests in such a
    way as to make it clear where errors are served
    under different circumstances.
    
    NB: The test on line 88 is intermittently failing,
    even with a 120 second sleep after apply the 
    annotations to the namespace

--- smoke-tests/spec/error_page_spec.rb
+++ smoke-tests/spec/error_page_spec.rb
@@ -1,21 +1,47 @@
 require "spec_helper"
 
+def expect_cluster_error_page(url, message)
+  expect {
+    URI.open(url)
+  }.to raise_error { |error|
+    expect(error).to be_a(OpenURI::HTTPError)
+    expect(error.message).to eq(message)
+    expect(error.io.string).to include("Error, service unavailable - GOV.UK")
+  }
+end
+
+def expect_ingress_error_page(url, message)
+  expect {
+    URI.open(url)
+  }.to raise_error { |error|
+    expect(error).to be_a(OpenURI::HTTPError)
+    expect(error.message).to eq(message)
+    expect(error.io.string).to_not include("Error, service unavailable - GOV.UK")
+  }
+end
+
+def expect_backend_error_page(url, message, body)
+  expect {
+    URI.open(url)
+  }.to raise_error { |error|
+    expect(error).to be_a(OpenURI::HTTPError)
+    expect(error.message).to eq(message)
+    expect(error.io.string).to eq(body)
+  }
+end
+
 describe "http request error responses" do
   let(:unmatched_url) { "https://foobar.apps.#{current_cluster}" }
 
   context "cluster default backend" do
-    it "serves a custom page for 404 errors" do
-      expect {
-        URI.open(unmatched_url)
-      }.to raise_error { |error|
-        expect(error).to be_a(OpenURI::HTTPError)
-        expect(error.message).to eq("404 Not Found")
-        expect(error.io.string).to include("Error, service unavailable - GOV.UK")
-      }
+    context "when the error is 404" do   # cluster default backend *always* serves 404 errors
+      it "serves the cluster's error page" do
+        expect_cluster_error_page(unmatched_url, "404 Not Found")
+      end
     end
   end
 
-  context "in a namespace" do
+  context "in a namespace with no http listeners" do
     let(:namespace) { "integrationtest-#{readable_timestamp}" }
     let(:host) { "#{namespace}.apps.#{current_cluster}" }
     let(:namespace_url) { "https://#{host}" }
@@ -28,121 +54,81 @@ describe "http request error responses" do
         file: "spec/fixtures/helloworld-deployment.yaml.erb",
         binding: binding
       )
+
+      # if nothing in the namespace is listening for requests, we get a 503 error
+      scale_replicas(namespace, "intergration-test-helloworld", "0")
+      sleep 10
     end
 
     after do
       delete_namespace(namespace)
     end
 
-    context "handling 503 error" do
+    # The list of error codes the cluster default backend will handle is defined in:
+    # https://github.com/ministryofjustice/cloud-platform-infrastructure/blob/master/terraform/cloud-platform-components/nginx-ingress-acme.tf
+    # Currently (16/09/19) it is "413,502,503,504". We can't change this, because this test will run against live-1
+    context "when the error code is in the cluster error list" do
+      it "serves the cluster's custom error page" do
+        expect_cluster_error_page(namespace_url, "503 Service Unavailable")
+      end
+    end
+
+    # This is a crazy hack by the kubernetes team. See here for more details:
+    # https://github.com/kubernetes/ingress-nginx/pull/3344#issue-227791109
+    context "when namespace defines list of errors including anything not in cluster list" do
+      let(:ing_annotations) { [ %{nginx.ingress.kubernetes.io/custom-http-errors="415"} ] }
+
       before do
-        # if nothing in the namespace is listening for requests, we get a 503 error
-        scale_replicas(namespace, "intergration-test-helloworld", "0")
+        annotate_ingress(namespace, "integration-test-app-ing", ing_annotations)
+        sleep 120 # sometimes 10 is enough here, but sometimes it's not
+      end
+
+      # All error pages will be served from the namespace ingress, regardless of status code
+      it "serves error page from nginx ingress" do
+        expect_ingress_error_page(namespace_url, "503 Service Temporarily Unavailable")
+      end
+    end
+
+    context "when namespace has its own default backend" do
+      let(:ing_annotations) { [ "nginx.ingress.kubernetes.io/default-backend=nginx-errors" ] }
+
+      before do
+        apply_yaml_file(
+          namespace: namespace,
+          file: "spec/fixtures/default-backend.yaml.erb"
+        )
+        annotate_ingress(namespace, "integration-test-app-ing", ing_annotations)
         sleep 10
       end
 
-      it "serves the cluster's custom 503 page" do
-        expect {
-          URI.open(namespace_url)
-        }.to raise_error { |error|
-          expect(error).to be_a(OpenURI::HTTPError)
-          expect(error.message).to eq("503 Service Unavailable")
-          expect(error.io.string).to include("Error, service unavailable - GOV.UK")
-        }
+      it "serves the cluster's custom error page" do
+        expect_cluster_error_page(namespace_url, "503 Service Unavailable")
       end
-    end
 
-  end
-end
+      context "when namespace is annotated with a list of http errors, including anything not in cluster list" do
+        let(:ing_annotations) { [
+          "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
+          %{nginx.ingress.kubernetes.io/custom-http-errors="415"},
+        ] }
 
-describe "Namespace with no custom default-backend but with ingress annotations, serve nginx error page" do
-  let(:namespace) { "no-backend-#{readable_timestamp}" }
-  let(:host) { "#{namespace}.apps.#{current_cluster}" }
-  let(:url) { "https://#{host}" }
-  let(:ing_annotations) {
-    [
-     %{nginx.ingress.kubernetes.io/custom-http-errors="404,415,504"}
-    ]
-  }
+        # All error pages will be served from the namespace default backend, regardless of status code
+        it "serves error page from namespace default backend" do
+          expect_backend_error_page(namespace_url, "503 Service Unavailable", "5xx html") # the page body "5xx html" is defined in the backend docker image
+        end
+      end
 
-  context "when app is deployed with ingress annotations" do
-    before do
-      create_namespace(namespace)
+      context "when the namespace is annotated with an error code from the cluster error list" do
+        let(:ing_annotations) { [
+          "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
+          %{nginx.ingress.kubernetes.io/custom-http-errors="504"},  # 504 is a proxy for 503. 504 is raised, but 503 is handled
+        ] }
 
-      apply_template_file(
-        namespace: namespace,
-        file: "spec/fixtures/helloworld-deployment.yaml.erb",
-        binding: binding
-      )
+        it "serves 503 error page from namespace default backend" do
+          expect_backend_error_page(namespace_url, "503 Service Unavailable", "5xx html") # the page body "5xx html" is defined in the backend docker image
+        end
 
-      annotate_ingress(namespace, "integration-test-app-ing", ing_annotations)
-
-      scale_replicas(namespace, "intergration-test-helloworld", "0")
-
-      sleep 10 # Without this, the test fails
-    end
-
-    after do
-      delete_namespace(namespace)
-    end
-
-    it "serves nginx error" do
-      expect {
-        URI.open(url)
-      }.to raise_error { |error|
-        expect(error).to be_a(OpenURI::HTTPError)
-        expect(error.message).to eq("503 Service Temporarily Unavailable")
-        expect(error.io.string).to include("503 Service Temporarily Unavailable")
-      }
-    end
-  end
-end
-
-describe "Namespace with custom default-backend and ingress annotations, serve custom default-backend error page" do
-  let(:namespace) { "custom-backend-#{readable_timestamp}" }
-  let(:host) { "#{namespace}.apps.#{current_cluster}" }
-  let(:url) { "https://#{host}" }
-  let(:ing_annotations) {
-    [
-     "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
-     %{nginx.ingress.kubernetes.io/custom-http-errors="404,415,504"}
-    ]
-  }
-
-  context "when app and defaut-backend is deployed with ingress annotations" do
-    before do
-      create_namespace(namespace)
-
-      apply_template_file(
-        namespace: namespace,
-        file: "spec/fixtures/helloworld-deployment.yaml.erb",
-        binding: binding
-      )
-
-      annotate_ingress(namespace, "integration-test-app-ing", ing_annotations)
-
-      apply_yaml_file(
-        namespace: namespace,
-        file: "spec/fixtures/default-backend.yaml.erb"
-      )
-
-      scale_replicas(namespace, "intergration-test-helloworld", "0")
-
-      sleep 60 # Without this, the test fails
-    end
-
-    after do
-      delete_namespace(namespace)
-    end
-
-    it "serves custom default-backend" do
-      expect {
-        URI.open(url)
-      }.to raise_error { |error|
-        expect(error).to be_a(OpenURI::HTTPError)
-        expect(error.message).to eq("503 Service Unavailable")
-        expect(error.io.string).to eq("5xx html")
-      }
+        it "serves other errors from the cluster default backend"
+      end
     end
   end
 end
