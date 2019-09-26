7e453aba5f797e7b4e7ceb577b4e2bfee00ae529
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2019-09-26 09:26:50 +0100

    null_resource to add labels to kube-system (#414)
    * null_resource to add labels to kube-system

--- terraform/cloud-platform-components/opa.tf
+++ terraform/cloud-platform-components/opa.tf
@@ -28,6 +28,7 @@ resource "helm_release" "open-policy-agent" {
   version    = "1.8.0"
 
   depends_on = [
+    "null_resource.kube_system_ns_label",
     "kubernetes_namespace.opa",
     "null_resource.deploy",
   ]
@@ -38,6 +39,13 @@ resource "helm_release" "open-policy-agent" {
 
   lifecycle {
     ignore_changes = ["keyring"]
+  }
+}
+
+# By adding this label OPA will ignore kube-system for all policy decisions. 
+resource "null_resource" "kube_system_ns_label" {
+  provisioner "local-exec" {
+    command = "kubectl label ns kube-system 'openpolicyagent.org/webhook=ignore'"
   }
 }
 
