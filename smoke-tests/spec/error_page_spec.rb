7fad07fd5477008727a708a4556c859aa3e0ad6e
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-10-23 21:34:54 +0100

    Updated error_page tests to simplify the code.

--- smoke-tests/spec/error_page_spec.rb
+++ smoke-tests/spec/error_page_spec.rb
@@ -52,8 +52,13 @@ describe "http request error responses" do
   end
 
   context "in a namespace with the HTTP handler" do 
-    let(:namespace) { "integrationtest-#{readable_timestamp}" }
+    let(:namespace) { "integrationtest-errorpage-#{readable_timestamp}" }
+    let(:namespace_url) { "https://#{host}/err?code=#{error_to_raise}" }
     let(:host) { "#{namespace}.apps.#{current_cluster}" }
+    let(:ing_annotations) { [
+      "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
+      %{nginx.ingress.kubernetes.io/custom-http-errors="#{error_to_annotate}"},  
+    ] }
 
     before do
       create_namespace(namespace)
@@ -74,17 +79,17 @@ describe "http request error responses" do
     # https://github.com/ministryofjustice/cloud-platform-infrastructure/blob/master/terraform/cloud-platform-components/nginx-ingress-acme.tf
     # Currently (22/10/19) it is "413,502,503,504". We can't change this, because this test will run against live-1
     context "when the error code is in the cluster error list" do
-      let(:namespace_url) { "https://#{host}/err?code=503" }
+      let(:error_to_raise) { "503" }
 
       it "serves the cluster's custom error page" do
         expect_cluster_error_page(namespace_url, "503 Service Unavailable")
       end
     end
 
-    context "when the error code is not in the cluster error list" do # result: no error page at all
-      let(:namespace_url) { "https://#{host}/err?code=501" }
+    context "when the error code is not in the cluster error list" do
+      let(:error_to_raise) { "501" }
 
-      it "serves the cluster's custom error page" do
+      it "serves no error page" do
         expect_ingress_error_page(namespace_url, "501 Not Implemented")
       end
     end
@@ -99,26 +104,29 @@ describe "http request error responses" do
         sleep 5 # sometimes 5 is enough here, but sometimes it's not
       end
 
-      context "when the error code in the cluster error list" do
-        let(:namespace_url) { "https://#{host}/err?code=503" }
+      context "when the error is in the cluster error list and application not serving error page" do
+        let(:error_to_raise) { "503" }
 
-        it "serves error page from nginx ingress, when application not serving error page" do
+        before do
           scale_replicas(namespace, "integration-test-errorpage", "0") # Scaled down to 0, so application is not listening for requests.
           sleep 5
+        end
+
+        it "serves error page from nginx ingress" do
           expect_ingress_error_page(namespace_url, "503 Service Temporarily Unavailable")
         end
       end
 
-      context "when application serving error and the error code is in the cluster error list" do
-        let(:namespace_url) { "https://#{host}/err?code=503" }
+      context "when the error is in the cluster error list and application serving error" do
+        let(:error_to_raise) { "502" }
 
         it "serves error page from application" do
-          expect_application_error_page(namespace_url, "503 Service Unavailable", "Code=503")
+          expect_application_error_page(namespace_url, "502 Bad Gateway", "Code=502")
         end
       end
 
       context "when the error is not in the cluster error list" do
-        let(:namespace_url) { "https://#{host}/err?code=401" }
+        let(:error_to_raise) { "401" }
 
         it "serves no error page" do
           expect_ingress_error_page(namespace_url, "401 Unauthorized")
@@ -135,7 +143,7 @@ describe "http request error responses" do
       end
 
       context "when the error is in the namespace error list" do # i.e. it's 503
-        let(:namespace_url) { "https://#{host}/err?code=503" }
+        let(:error_to_raise) { "503" }
 
         it "serves cluster error page for error in the list" do
           expect_cluster_error_page(namespace_url, "503 Service Unavailable")
@@ -143,7 +151,7 @@ describe "http request error responses" do
       end
 
       context "when the error is not in the namespace error list" do
-        let(:namespace_url) { "https://#{host}/err?code=409" }
+        let(:error_to_raise) { "409" }
 
         it "serves no error page for error not in the list" do
           expect_ingress_error_page(namespace_url, "409 Conflict")
@@ -152,62 +160,56 @@ describe "http request error responses" do
     end
 
     context "when namespace has its own default backend" do
-      let(:ing_annotations) { [ "nginx.ingress.kubernetes.io/default-backend=nginx-errors" ] }
-      let(:namespace_url) { "https://#{host}/err?code=503" }
 
       before do
         annotate_ingress(namespace, "integration-test-app-ing", ing_annotations)
         sleep 5
       end
 
-      it "serves the cluster's custom error page" do
-        expect_cluster_error_page(namespace_url, "503 Service Unavailable")
+      context "when ingress is annotated with default-backend service but not with the list of http errors" do
+        let(:ing_annotations) { [ "nginx.ingress.kubernetes.io/default-backend=nginx-errors" ] }
+        let(:error_to_raise) { "503" }
+
+        it "serves the cluster's custom error page" do
+          expect_cluster_error_page(namespace_url, "503 Service Unavailable")
+        end
       end
 
-      context "when namespace is annotated with a list of http errors, including anything not in cluster list" do
-        let(:ing_annotations) { [
-          "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
-          %{nginx.ingress.kubernetes.io/custom-http-errors="415"},
-        ] }
-        let(:namespace_url) { "https://#{host}/err?code=503" }
+      context "when ingress is annotated with an error code not from the cluster error list and application not serving error pages" do
+        let(:error_to_annotate) { "415" }
+        let(:error_to_raise) { "503" }
 
-        it "serves error page from namespace default backend, when application is not serving errors." do
+        before do
           scale_replicas(namespace, "integration-test-errorpage", "0") # Scaled down to 0, so application is not listening for requests.
           sleep 5
+        end
+
+        it "serves error page from namespace default backend." do
           expect_backend_error_page(namespace_url, "503 Service Unavailable", "5xx html") # the page body "5xx html" is defined in the backend docker image
         end
       end
 
-      context "when application serving error page and namespace is annotated with a list of http errors, including anything not in cluster list" do
-        let(:ing_annotations) { [
-          "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
-          %{nginx.ingress.kubernetes.io/custom-http-errors="415"},
-        ] }
-        let(:namespace_url) { "https://#{host}/err?code=503" }
+      context "when ingress is annotated with an error code not from the cluster error list and application is serving error pages" do
+        let(:error_to_annotate) { "415" }
+        let(:error_to_raise) { "503" }
 
         it "serves error page from application" do
           expect_application_error_page(namespace_url, "503 Service Unavailable", "Code=503") # the page body "Code=503" is from the application.
         end
       end
 
-      context "when the namespace is annotated with an error code from the cluster error list" do
-        let(:ing_annotations) { [
-          "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
-          %{nginx.ingress.kubernetes.io/custom-http-errors="503"},
-        ] }
-        let(:namespace_url) { "https://#{host}/err?code=503" }
+      context "when the ingress is annotated with an error code from the cluster error list" do
+        let(:error_to_annotate) { "503" }
+        let(:error_to_raise) { "503" }
 
         it "serves 503 error page from namespace default backend" do
           expect_backend_error_page(namespace_url, "503 Service Unavailable", "5xx html") # the page body "5xx html" is defined in the backend docker image
         end
       end
 
-      context "when the namespace is annotated with an error code not in the cluster error list" do
-        let(:ing_annotations) { [
-          "nginx.ingress.kubernetes.io/default-backend=nginx-errors",
-          %{nginx.ingress.kubernetes.io/custom-http-errors="429"},  
-        ] }
-        let(:namespace_url) { "https://#{host}/err?code=429" }
+      context "when the ingress is annotated with an error code not in the cluster error list" do
+        let(:error_to_annotate) { "429" }
+        let(:error_to_raise) { "429" }
 
         it "serves 429 error page from namespace default backend" do
           expect_backend_error_page(namespace_url, "429 Too Many Requests", "4xx html") # the page body "4xx html" is defined in the backend docker image
