71054f7cb2bd9450ec6a29a05ba8c37546908d50
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-14 22:17:03 +0000

    tf 0.12upgrade terraform/cloud-platform

--- terraform/cloud-platform/kops.tf
+++ terraform/cloud-platform/kops.tf
@@ -1,5 +1,5 @@
 resource "local_file" "kops" {
-  content  = "${data.template_file.kops.rendered}"
+  content  = data.template_file.kops.rendered
   filename = "../../kops/${terraform.workspace}.yaml"
 }
 
@@ -8,25 +8,26 @@ resource "aws_kms_key" "kms" {
 }
 
 data "template_file" "kops" {
-  template = "${file("./templates/kops.yaml.tpl")}"
+  template = file("./templates/kops.yaml.tpl")
 
-  vars {
-    cluster_domain_name                  = "${local.cluster_base_domain_name}"
-    cluster_node_count                   = "${local.is_live_cluster ? var.cluster_node_count : 3}"
-    kops_state_store                     = "${data.terraform_remote_state.global.cloud_platform_kops_state}"
-    oidc_issuer_url                      = "${local.oidc_issuer_url}"
-    oidc_client_id                       = "${auth0_client.kubernetes.client_id}"
-    network_cidr_block                   = "${module.cluster_vpc.vpc_cidr_block}"
-    network_id                           = "${module.cluster_vpc.vpc_id}"
-    internal_subnets_id_a                = "${module.cluster_vpc.private_subnets[0]}"
-    internal_subnets_id_b                = "${module.cluster_vpc.private_subnets[1]}"
-    internal_subnets_id_c                = "${module.cluster_vpc.private_subnets[2]}"
-    external_subnets_id_a                = "${module.cluster_vpc.public_subnets[0]}"
-    external_subnets_id_b                = "${module.cluster_vpc.public_subnets[1]}"
-    external_subnets_id_c                = "${module.cluster_vpc.public_subnets[2]}"
-    authorized_keys_manager_systemd_unit = "${indent(6, data.template_file.authorized_keys_manager.rendered)}"
-    kms_key                              = "${aws_kms_key.kms.arn}"
-    worker_node_machine_type             = "${var.worker_node_machine_type}"
-    master_node_machine_type             = "${var.master_node_machine_type}"
+  vars = {
+    cluster_domain_name                  = local.cluster_base_domain_name
+    cluster_node_count                   = local.is_live_cluster ? var.cluster_node_count : 3
+    kops_state_store                     = data.terraform_remote_state.global.outputs.cloud_platform_kops_state
+    oidc_issuer_url                      = local.oidc_issuer_url
+    oidc_client_id                       = auth0_client.kubernetes.client_id
+    network_cidr_block                   = module.cluster_vpc.vpc_cidr_block
+    network_id                           = module.cluster_vpc.vpc_id
+    internal_subnets_id_a                = module.cluster_vpc.private_subnets[0]
+    internal_subnets_id_b                = module.cluster_vpc.private_subnets[1]
+    internal_subnets_id_c                = module.cluster_vpc.private_subnets[2]
+    external_subnets_id_a                = module.cluster_vpc.public_subnets[0]
+    external_subnets_id_b                = module.cluster_vpc.public_subnets[1]
+    external_subnets_id_c                = module.cluster_vpc.public_subnets[2]
+    authorized_keys_manager_systemd_unit = indent(6, data.template_file.authorized_keys_manager.rendered)
+    kms_key                              = aws_kms_key.kms.arn
+    worker_node_machine_type             = var.worker_node_machine_type
+    master_node_machine_type             = var.master_node_machine_type
   }
 }
+
--- terraform/cloud-platform/main.tf
+++ terraform/cloud-platform/main.tf
@@ -24,13 +24,13 @@ provider "aws" {
 #
 provider "auth0" {
   version = ">= 0.1.18"
-  domain  = "${local.auth0_tenant_domain}"
+  domain  = local.auth0_tenant_domain
 }
 
 data "terraform_remote_state" "global" {
   backend = "s3"
 
-  config {
+  config = {
     bucket  = "cloud-platform-terraform-state"
     region  = "eu-west-1"
     key     = "global-resources/terraform.tfstate"
@@ -39,36 +39,36 @@ data "terraform_remote_state" "global" {
 }
 
 locals {
-  cluster_name             = "${terraform.workspace}"
+  cluster_name             = terraform.workspace
   cluster_base_domain_name = "${local.cluster_name}.cloud-platform.service.justice.gov.uk"
   auth0_tenant_domain      = "justice-cloud-platform.eu.auth0.com"
   oidc_issuer_url          = "https://${local.auth0_tenant_domain}/"
 
-  is_live_cluster      = "${terraform.workspace == "live-1"}"
-  services_base_domain = "${local.is_live_cluster ? "cloud-platform.service.justice.gov.uk" : "apps.${local.cluster_base_domain_name}"}"
+  is_live_cluster      = terraform.workspace == "live-1"
+  services_base_domain = local.is_live_cluster ? "cloud-platform.service.justice.gov.uk" : "apps.${local.cluster_base_domain_name}"
 }
 
 # Modules
 module "cluster_dns" {
   source                   = "../modules/cluster_dns"
-  cluster_base_domain_name = "${local.cluster_base_domain_name}"
-  parent_zone_id           = "${data.terraform_remote_state.global.cp_zone_id}"
+  cluster_base_domain_name = local.cluster_base_domain_name
+  parent_zone_id           = data.terraform_remote_state.global.outputs.cp_zone_id
 }
 
 module "cluster_ssl" {
   source                   = "../modules/cluster_ssl"
-  cluster_base_domain_name = "${local.cluster_base_domain_name}"
-  dns_zone_id              = "${module.cluster_dns.cluster_dns_zone_id}"
+  cluster_base_domain_name = local.cluster_base_domain_name
+  dns_zone_id              = module.cluster_dns.cluster_dns_zone_id
 }
 
 module "cluster_vpc" {
   version              = "1.66.0"
   source               = "terraform-aws-modules/vpc/aws"
-  name                 = "${local.cluster_name}"
-  cidr                 = "${var.vpc_cidr}"
-  azs                  = "${var.availability_zones}"
-  private_subnets      = "${var.internal_subnets}"
-  public_subnets       = "${var.external_subnets}"
+  name                 = local.cluster_name
+  cidr                 = var.vpc_cidr
+  azs                  = var.availability_zones
+  private_subnets      = var.internal_subnets
+  public_subnets       = var.external_subnets
   enable_nat_gateway   = true
   enable_vpn_gateway   = false
   enable_dns_hostnames = true
@@ -87,8 +87,8 @@ module "cluster_vpc" {
 
   tags = {
     Terraform = "true"
-    Cluster   = "${local.cluster_name}"
-    Domain    = "${local.cluster_base_domain_name}"
+    Cluster   = local.cluster_name
+    Domain    = local.cluster_base_domain_name
   }
 }
 
@@ -98,8 +98,8 @@ resource "tls_private_key" "cluster" {
 }
 
 resource "aws_key_pair" "cluster" {
-  key_name   = "${local.cluster_base_domain_name}"
-  public_key = "${tls_private_key.cluster.public_key_openssh}"
+  key_name   = local.cluster_base_domain_name
+  public_key = tls_private_key.cluster.public_key_openssh
 }
 
 resource "auth0_client" "kubernetes" {
@@ -107,14 +107,22 @@ resource "auth0_client" "kubernetes" {
   description = "Cloud Platform kubernetes"
   app_type    = "regular_web"
 
-  callbacks = ["${format("https://login.%s/ui", local.services_base_domain)}"]
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibility in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  callbacks = [format("https://login.%s/ui", local.services_base_domain)]
 
   custom_login_page_on = true
   is_first_party       = true
   oidc_conformant      = true
   sso                  = true
 
-  jwt_configuration = {
+  jwt_configuration {
     alg                 = "RS256"
     lifetime_in_seconds = "2592000"
   }
@@ -126,13 +134,34 @@ resource "auth0_client" "components" {
   app_type    = "regular_web"
 
   callbacks = [
-    "${format("https://prometheus.%s/oauth2/callback", local.services_base_domain)}",
-    "${format("https://alertmanager.%s/oauth2/callback", local.services_base_domain)}",
-    "${format("https://concourse.%s/sky/issuer/callback", local.services_base_domain)}",
-    "${format("https://kibana.%s/oauth2/callback", local.services_base_domain)}",
-    "${format("https://kibana-audit.%s/oauth2/callback", local.services_base_domain)}",
-    "${format("https://grafana.%s/login/generic_oauth", local.services_base_domain)}",
-    "${format("https://kube-ops.%s/login/authorized", local.services_base_domain)}",
+    format(
+      "https://prometheus.%s/oauth2/callback",
+      local.services_base_domain,
+    ),
+    format(
+      "https://alertmanager.%s/oauth2/callback",
+      local.services_base_domain,
+    ),
+    format(
+      "https://concourse.%s/sky/issuer/callback",
+      local.services_base_domain,
+    ),
+    format(
+      "https://kibana.%s/oauth2/callback",
+      local.services_base_domain,
+    ),
+    format(
+      "https://kibana-audit.%s/oauth2/callback",
+      local.services_base_domain,
+    ),
+    format(
+      "https://grafana.%s/login/generic_oauth",
+      local.services_base_domain,
+    ),
+    format(
+      "https://kube-ops.%s/login/authorized",
+      local.services_base_domain,
+    ),
   ]
 
   custom_login_page_on = true
@@ -140,8 +169,9 @@ resource "auth0_client" "components" {
   oidc_conformant      = true
   sso                  = true
 
-  jwt_configuration = {
+  jwt_configuration {
     alg                 = "RS256"
     lifetime_in_seconds = "36000"
   }
 }
+
