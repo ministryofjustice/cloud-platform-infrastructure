40de4c0a84ecefc909b544d3f2c38846ec0401cc
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-14 21:47:39 +0000

    tf 0.12upgrade module cluster_dns

--- terraform/modules/cluster_dns/outputs.tf
+++ terraform/modules/cluster_dns/outputs.tf
@@ -1,7 +1,8 @@
 output "cluster_dns_zone_name" {
-  value = "${aws_route53_zone.cluster.name}"
+  value = aws_route53_zone.cluster.name
 }
 
 output "cluster_dns_zone_id" {
-  value = "${aws_route53_zone.cluster.zone_id}"
+  value = aws_route53_zone.cluster.zone_id
 }
+
--- terraform/modules/cluster_dns/variables.tf
+++ terraform/modules/cluster_dns/variables.tf
@@ -1,2 +1,6 @@
-variable "cluster_base_domain_name" {}
-variable "parent_zone_id" {}
+variable "cluster_base_domain_name" {
+}
+
+variable "parent_zone_id" {
+}
+
