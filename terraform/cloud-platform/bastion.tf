09d59647ced7c311111e0ca3c2423486e9e652a1
Alejandro Garrido Mota <garridomota@gmail.com>
2019-12-11 15:54:06 +0000

    Added public subnets variable to avoid dependency problems

--- terraform/cloud-platform/bastion.tf
+++ terraform/cloud-platform/bastion.tf
@@ -6,7 +6,8 @@
 module "bastion" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-bastion"
 
-  vpc_id       = module.cluster_vpc.vpc_id
-  key_name     = aws_key_pair.cluster.key_name
-  route53_zone = module.cluster_dns.cluster_dns_zone_name
+  vpc_id         = module.cluster_vpc.vpc_id
+  public_subnets = module.cluster_vpc.public_subnets
+  key_name       = aws_key_pair.cluster.key_name
+  route53_zone   = module.cluster_dns.cluster_dns_zone_name
 }
