d5ce752f42b197191c5fc22a2ff51291b4502525
mtrblmoj <mourad.trabelsi@digital.justice.gov.uk>
2019-12-24 06:11:36 +0000

    add cert-manager cert to the list of expected one

--- smoke-tests/spec/certificates_spec.rb
+++ smoke-tests/spec/certificates_spec.rb
@@ -6,7 +6,9 @@ describe "certificates" do
     names = get_certificates.map { |set| set.dig("metadata", "name") }.sort
 
     expected = [
-        "default", #ingress-controller certificate
+      "default", #ingress-controller certificate
+      "cert-manager-webhook-ca",
+      "cert-manager-webhook-webhook-tls",
     ]
     expect(names).to include(*expected)
   end
