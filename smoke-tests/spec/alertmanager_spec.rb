3b8ab184c0dcab96271eb0414ec13ce5763f1b40
mtrblmoj <mourad.trabelsi@digital.justice.gov.uk>
2019-12-27 15:01:13 +0000

    add test to ensure all alertmanager resource (!=CRD) exist in the cluster

---
+++ smoke-tests/spec/alertmanager_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe "Alertmanager resources" do
+  specify "expected Alertmanager resources" do
+    names = get_alertmanagers.map { |set| set.dig("metadata", "name") }.sort
+
+    expected = [
+      "prometheus-operator-alertmanager",
+    ]
+    expect(names).to include(*expected)
+  end
+end
+
+
+
