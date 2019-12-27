10090ef4bc8977385b2ae54257453672ec688f90
Alejandro Garrido Mota <garridomota@gmail.com>
2019-12-27 16:13:49 +0000

    - Add new outputs required by the concourseCI - Removed some variables not in use anymore

--- terraform/cloud-platform-eks/outputs.tf
+++ terraform/cloud-platform-eks/outputs.tf
@@ -10,6 +10,18 @@ output "cluster_name" {
   value = local.cluster_name
 }
 
+output "vpc_id" {
+  value = data.aws_vpc.selected.id
+}
+
+output "internal_subnets" {
+  value = data.aws_subnet.private_cidrs.*.cidr_block
+}
+
+output "internal_subnets_ids" {
+  value = tolist(data.aws_subnet_ids.private.ids)
+}
+
 output "base_route53_hostzone" {
   value = local.base_route53_hostzone
 }
