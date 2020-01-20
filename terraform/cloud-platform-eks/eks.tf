24d83f80a3ab6f8e01fbf8d42c0de695e89cbf0a
Alejandro Garrido Mota <garridomota@gmail.com>
2020-01-20 13:20:50 +0000

    - Upgrade to the most recent version of the module. - Implement of node manage groups by AWS

--- terraform/cloud-platform-eks/eks.tf
+++ terraform/cloud-platform-eks/eks.tf
@@ -2,25 +2,49 @@
 # EKS Cluster #
 ###############
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "v7.0.0"
+  version = "v8.1.0"
 
   cluster_name       = local.cluster_name
   subnets            = concat(tolist(data.aws_subnet_ids.private.ids), tolist(data.aws_subnet_ids.public.ids))
   vpc_id             = data.aws_vpc.selected.id
   config_output_path = "./files/"
 
-  worker_groups = [
-    {
-      instance_type        = var.worker_node_machine_type
-      subnets              = data.aws_subnet_ids.private.ids
-      asg_max_size         = 30
-      asg_min_size         = 0
-      asg_desired_capacity = var.cluster_node_count
-      key_name             = local.key_name
+  node_groups = {
+    default_ng = {
+      desired_capacity = var.cluster_node_count
+      max_capacity     = 30
+      min_capacity     = 1
+      subnets          = data.aws_subnet_ids.private.ids
+
+      instance_type = var.worker_node_machine_type
+      k8s_labels = {
+        Terraform = "true"
+        Cluster   = local.cluster_name
+        Domain    = local.cluster_base_domain_name
+      }
+      additional_tags = {
+        default_ng = "true"
+      }
     }
-  ]
+  }
 
   # Out of the box you can't specify groups to map, just users. Some people did some workarounds
   # we can explore later: https://ygrene.tech/mapping-iam-groups-to-eks-user-access-66fd745a6b77
