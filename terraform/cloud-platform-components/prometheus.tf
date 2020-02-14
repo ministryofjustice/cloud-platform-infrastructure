e076478e08f0ca9d2430fe6966cefd471f47904a
Alejandro Garrido Mota <garridomota@gmail.com>
2020-02-14 09:47:53 +0000

    - Added IAM roles and policies to allow Thanos writting in S3 without credentials. - Also added the required input value for the `values.yaml` template

--- terraform/cloud-platform-components/prometheus.tf
+++ terraform/cloud-platform-components/prometheus.tf
@@ -149,6 +149,7 @@ resource "helm_release" "prometheus_operator" {
     prometheus_ingress     = local.prometheus_ingress
     random_username        = random_id.username.hex
     random_password        = random_id.password.hex
+    monitoring_aws_role    = aws_iam_role.monitoring.name
   })]
 
   # Depends on Helm being installed
@@ -258,3 +259,46 @@ resource "helm_release" "alertmanager_proxy" {
   }
 }
 
+# This is to create a policy which allows Prometheus (thanos to be precise) to have a role to write to S3 without credentials
+data "aws_iam_policy_document" "monitoring_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_iam_role.nodes.arn]
+    }
+  }
+}
+
+resource "aws_iam_role" "monitoring" {
+  name               = "monitoring.${data.terraform_remote_state.cluster.outputs.cluster_domain_name}"
+  assume_role_policy = data.aws_iam_policy_document.monitoring_assume.json
+}
+
+data "aws_iam_policy_document" "monitoring" {
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:PutObject"
+    ]
+
+    # Bucket name is hardcoded because it hasn't been created with terraform
+    # files inside this repository. Once we are happy with the test we must: 
+    # 1. Create S3 bucket from the cp-environments repo (or maybe from here?)
+    # 2. Use the output (S3 bucket name) in this policy
+    resources = [
+      "arn:aws:s3:::cloud-platform-2b5483a8020c73862c69b87f44c398b8/*",
+      "arn:aws:s3:::cloud-platform-2b5483a8020c73862c69b87f44c398b8"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "monitoring" {
+  name   = "route53"
+  role   = aws_iam_role.monitoring.id
+  policy = data.aws_iam_policy_document.monitoring.json
+}
