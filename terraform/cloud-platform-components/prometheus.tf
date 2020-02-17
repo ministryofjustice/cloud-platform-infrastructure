a00f8199baee0b995589ec584c26b86e1650298e
Alejandro Garrido Mota <garridomota@gmail.com>
2020-02-17 09:09:36 +0000

    Added k8s service and deployment for thanos-query and thanos-sidecar

--- terraform/cloud-platform-components/prometheus.tf
+++ terraform/cloud-platform-components/prometheus.tf
@@ -261,6 +261,10 @@ resource "helm_release" "alertmanager_proxy" {
   }
 }
 
+##########
+# THANOS #
+##########
+
 # This is to create a policy which allows Prometheus (thanos to be precise) to have a role to write to S3 without credentials
 data "aws_iam_policy_document" "monitoring_assume" {
   statement {
@@ -303,6 +307,105 @@ resource "aws_iam_role_policy" "monitoring" {
   name   = "route53"
   role   = aws_iam_role.monitoring.id
   policy = data.aws_iam_policy_document.monitoring.json
+}
+
+# Thanos side-car service (not deployment needed because prometheus-operator included it)
+resource "kubernetes_service" "thanos_sidecar" {
+  metadata {
+    name      = "thanos-sidecar"
+    namespace = kubernetes_namespace.monitoring.id
+    labels = {
+      app = "thanos-sidecar"
+    }
+  }
+  spec {
+    selector = {
+      app = "prometheus"
+    }
+    port {
+      name        = "grpc"
+      port        = 10901
+      target_port = "grpc"
+    }
+  }
+}
+
+# Thanos Query deployment
+resource "kubernetes_deployment" "thanos_query" {
+  metadata {
+    name = "thanos-query"
+    namespace = kubernetes_namespace.monitoring.id
+    labels = {
+      app = "thanos-query"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "thanos-query"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "thanos-query"
+        }
+      }
+
+      spec {
+        container {
+          image = "quay.io/thanos/thanos:v0.10.1"
+          name  = "thanos-query"
+
+          args = [
+            "query",
+            "--log.level=debug",
+            "--query.replica-label=prometheus_replica",
+            "--query.replica-label=thanos_ruler_replica",
+            "--store=dnssrv+_grpc._tcp.thanos-sidecar.${kubernetes_namespace.monitoring.id}.svc.cluster.local",
+            "--store=dnssrv+_grpc._tcp.thanos-ruler.${kubernetes_namespace.monitoring.id}.svc.cluster.local"
+          ]
+
+          port {
+            name = "http"
+            container_port = 10902
+          }
+
+          port {
+            name = "grpc"
+            container_port = 10901
+          }
+
+
+        }
+      }
+    }
+  }
+}
+
+# Thanos side-car service (not deployment needed because prometheus-operator included it)
+resource "kubernetes_service" "thanos_query" {
+  metadata {
+    name      = "thanos-query"
+    namespace = kubernetes_namespace.monitoring.id
+    labels = {
+      app = "thanos-query"
+    }
+  }
+  spec {
+    selector = {
+      app = "thanos-query"
+    }
+    port {
+      name        = "http"
+      port        = 9090
+      target_port = "http"
+    }
+  }
 }
 
 
