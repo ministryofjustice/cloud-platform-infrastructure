8e673b3e50b43d6db6d65d06250d54ce237d16cf
Alejandro Garrido Mota <garridomota@gmail.com>
2020-03-03 08:48:31 +0000

    Set components oidc output as sensitive

--- terraform/cloud-platform/outputs.tf
+++ terraform/cloud-platform/outputs.tf
@@ -69,6 +69,7 @@ output "oidc_components_client_id" {
 
 output "oidc_components_client_secret" {
   value = auth0_client.components.client_secret
+  sensitive = true
 }
 
 output "certificate_arn" {
