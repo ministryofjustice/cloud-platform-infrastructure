17f471abf3a3b756f7b911135fbb84730f418c65
Alejandro Garrido Mota <garridomota@gmail.com>
2020-03-17 13:26:57 +0000

    Testing new nodegroup

--- terraform/cloud-platform-components/prometheus.tf
+++ terraform/cloud-platform-components/prometheus.tf
@@ -1,12 +1,15 @@
 
 module "prometheus" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-prometheus?ref=0.0.7"
+  #source = "github.com/ministryofjustice/cloud-platform-terraform-prometheus?ref=0.0.7"
+  source = "/Users/mogaal/workspace/github/ministryofjustice/cloud-platform-terraform-prometheus"
 
   alertmanager_slack_receivers = var.alertmanager_slack_receivers
   iam_role_nodes               = data.aws_iam_role.nodes.arn
   pagerduty_config             = var.pagerduty_config
   enable_ecr_exporter          = terraform.workspace == local.live_workspace ? true : false
   enable_cloudwatch_exporter   = terraform.workspace == local.live_workspace ? true : false
+  #enable_prometheus_affinity   = terraform.workspace == local.live_workspace ? true : false
+  enable_prometheus_affinity   = true
 
   dependence_deploy = null_resource.deploy
   dependence_opa    = helm_release.open-policy-agent
