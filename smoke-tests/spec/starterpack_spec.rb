54e3dad4ef3309ac546db57b5e7825d964c64634
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-03-25 18:41:07 +0000

    Add tests for checking whether starter pack has deployed correctly by checking their ingress urls

---
+++ smoke-tests/spec/starterpack_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe "Starter pack Apps", speed: "fast" do
+  
+  let(:namespace) { "starter-pack"}
+  let(:ingresses) { get_ingresses(namespace) }
+  let(:ingress_first) { ingresses[0] }
+  let(:ingress_second) { ingresses[1] }
+
+    it "Hello world app response ok" do
+      response = get_url(ingress_first)
+      expect(response.code).to eq "200"
+    end    
+  
+    it "Multi container app response ok" do
+      response = get_url(ingress_second)
+      expect(response.code).to eq "200"
+    end   
+
+
+end
