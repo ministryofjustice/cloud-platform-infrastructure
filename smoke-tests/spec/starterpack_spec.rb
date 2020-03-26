a9b74fb08fbd4abde607eb98b87cc45c98b63306
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-03-26 15:22:01 +0000

    Change the way to parse ingress to handle any number of ingress in the starter pack namespace

--- smoke-tests/spec/starterpack_spec.rb
+++ smoke-tests/spec/starterpack_spec.rb
@@ -1,21 +1,16 @@
 require "spec_helper"
 
 describe "Starter pack Apps", speed: "fast" do
-  
+
   let(:namespace) { "starter-pack"}
   let(:ingresses) { get_ingresses(namespace) }
-  let(:ingress_first) { ingresses[0] }
-  let(:ingress_second) { ingresses[1] }
 
-    it "Hello world app response ok" do
-      response = get_url(ingress_first)
-      expect(response.code).to eq "200"
-    end    
-  
-    it "Multi container app response ok" do
-      response = get_url(ingress_second)
-      expect(response.code).to eq "200"
-    end   
-
-
+  context "Test starter pack apps" do
+    it "Starter pack app response ok" do
+      ingresses.each do |ingress|
+        response = get_url(ingress)
+        expect(response.code).to eq "200"
+      end  
+    end
+  end
 end
