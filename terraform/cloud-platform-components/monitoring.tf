61d45120a2966457b4a765157dca6288e497f9f0
Alejandro Garrido Mota <garridomota@gmail.com>
2020-04-07 14:11:52 +0100

    Updated to the latest helm chart oauth proxy

--- terraform/cloud-platform-components/monitoring.tf
+++ terraform/cloud-platform-components/monitoring.tf
@@ -1,6 +1,6 @@
 
 module "prometheus" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.1.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.1.5"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
