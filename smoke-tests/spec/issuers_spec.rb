a3e6e3960d619c7924be6058017e2e9d97ebf4f1
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-04-17 14:09:30 +0100

    Removed issuer integration tests
    After cert-manager upgrade to v0.14, we will not maintain issuer
    
    https://cert-manager.io/docs/installation/upgrading/upgrading-0.9-0.10/

--- smoke-tests/spec/issuers_spec.rb
+++ smoke-tests/spec/issuers_spec.rb
@@ -12,14 +12,3 @@ describe "Clusterissuer", speed: "fast" do
   end
 end
 
-describe "Issuer", speed: "fast" do
-  specify "expected Issuer" do
-    names = get_issuers.map { |set| set.dig("metadata", "name") }.sort
-
-    expected = [
-      "cert-manager-webhook-ca",
-      "cert-manager-webhook-selfsign",
-    ]
-    expect(names).to include(*expected)
-  end
-end
