faf937b00fcb00053e1716a60894a5f87a3d52e1
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-05-05 14:41:13 +0100

    Modularize external-dns component

--- terraform/cloud-platform-components/external-dns.tf
+++ terraform/cloud-platform-components/external-dns.tf
@@ -1,95 +1,13 @@
-data "aws_iam_policy_document" "external_dns_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
+module "external_dns" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-external-dns?ref=1.0.1"
 
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_iam_role.nodes.arn]
-    }
-  }
+  iam_role_nodes      = data.aws_iam_role.nodes.arn
+  cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+  hostzone            = lookup(var.cluster_r53_resource_maps, terraform.workspace, ["arn:aws:route53:::hostedzone/${data.terraform_remote_state.cluster.outputs.hosted_zone_id}"])
+
+  dependence_deploy = null_resource.deploy
+  dependence_kiam   = helm_release.kiam
+
+  # This section is for EKS
+  eks = false
 }
-
-resource "aws_iam_role" "external_dns" {
-  name               = "external-dns.${data.terraform_remote_state.cluster.outputs.cluster_domain_name}"
-  assume_role_policy = data.aws_iam_policy_document.external_dns_assume.json
-}
-
-data "aws_iam_policy_document" "external_dns" {
-  statement {
-    actions = ["route53:ChangeResourceRecordSets"]
-
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
-    resources = [format(
-      "arn:aws:route53:::hostedzone/%s",
-      terraform.workspace == local.live_workspace ? "*" : data.terraform_remote_state.cluster.outputs.hosted_zone_id,
-    )]
-  }
-
-  statement {
-    actions = [
-      "route53:ListHostedZones",
-      "route53:ListResourceRecordSets",
-    ]
-
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role_policy" "external_dns" {
-  name   = "route53"
-  role   = aws_iam_role.external_dns.id
-  policy = data.aws_iam_policy_document.external_dns.json
-}
-
-resource "helm_release" "external_dns" {
-  name      = "external-dns"
-  chart     = "stable/external-dns"
-  namespace = "kube-system"
-  version   = "2.6.4"
-
-  values = [
-    <<EOF
-image:
-  tag: 0.5.17-debian-9-r0
-sources:
-  - service
-  - ingress
-provider: aws
-aws:
-  region: eu-west-2
-  zoneType: public
-domainFilters:
-  ${terraform.workspace == local.live_workspace ? "" : format(
-    "- %s",
-    data.terraform_remote_state.cluster.outputs.cluster_domain_name,
-)}
-rbac:
-  create: true
-  apiVersion: v1
-  serviceAccountName: default
-txtPrefix: "_external_dns."
-logLevel: info
-podAnnotations:
-  iam.amazonaws.com/role: "${aws_iam_role.external_dns.name}"
-EOF
-,
-]
-
-depends_on = [
-  null_resource.deploy,
-  helm_release.kiam,
-  module.opa.helm_opa_status,
-]
-
-lifecycle {
-  ignore_changes = [keyring]
-}
-}
-
