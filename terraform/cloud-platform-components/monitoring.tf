cf1dbbf08dc507ec4b3d0476fcc50f47ed2e33f9
Alejandro Garrido Mota <garridomota@gmail.com>
2020-05-06 10:42:41 +0100

    Not deploy dependence anymore

--- terraform/cloud-platform-components/monitoring.tf
+++ terraform/cloud-platform-components/monitoring.tf
@@ -15,7 +15,7 @@ module "prometheus" {
   oidc_components_client_secret = data.terraform_remote_state.cluster.outputs.oidc_components_client_secret
   oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
 
-  dependence_deploy = null_resource.deploy
+  dependence_deploy = "null_resource.deploy"
   dependence_opa    = module.opa.helm_opa_status
 }
 
