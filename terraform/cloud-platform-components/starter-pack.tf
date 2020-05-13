b7cdcf6b93b4c8a4d2b590b3050af9ef4ecd63cd
Alejandro Garrido Mota <garridomota@gmail.com>
2020-05-13 14:55:05 +0100

    Enable condition only for cluster that are not live-1

--- terraform/cloud-platform-components/starter-pack.tf
+++ terraform/cloud-platform-components/starter-pack.tf
@@ -2,7 +2,6 @@
 module "starter_pack" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=improvements-and-helm3"
 
-  #enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
-  enable_starter_pack = false
+  enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
 }
