5c3564a6a17cac2dd210ba8b2b24c8650ddc232f
Alejandro Garrido Mota <garridomota@gmail.com>
2020-05-13 14:58:40 +0100

    Pointing to the latest version

--- terraform/cloud-platform-components/starter-pack.tf
+++ terraform/cloud-platform-components/starter-pack.tf
@@ -1,6 +1,6 @@
 
 module "starter_pack" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=improvements-and-helm3"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.0.3"
 
   enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
