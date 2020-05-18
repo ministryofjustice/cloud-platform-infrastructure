4fa42b8d571bf2854888fa3c923e4b0db4f1cc81
Alejandro Garrido Mota <garridomota@gmail.com>
2020-05-18 18:39:59 +0100

    - Migrated components from cloud-platform-terraform-eks-components to infrastructure. - Also pinned helm provider 1.0.0 which basically migrate to Helm3

--- terraform/cloud-platform-eks/components/variables.tf
+++ terraform/cloud-platform-eks/components/variables.tf
@@ -11,3 +11,27 @@ variable "cloud_platform_slack_webhook" {
   description = "Slack webhook to pass it to  script to send alerts"
 }
 
+variable "cluster_r53_resource_maps" {
+  default = {
+    manager = ["arn:aws:route53:::hostedzone/Z1OWR28V4Q2RTU", "arn:aws:route53:::hostedzone/Z5C82RHBFD2NI"]
+  }
+}
+
+variable "elasticsearch_hosts_maps" {
+  default = {
+    manager = "search-cloud-platform-live-dibidbfud3uww3lpxnhj2jdws4.eu-west-2.es.amazonaws.com"
+  }
+}
+
+variable "elasticsearch_audit_hosts_maps" {
+  default = {
+    manager = "search-cloud-platform-audit-dq5bdnjokj4yt7qozshmifug6e.eu-west-2.es.amazonaws.com"
+  }
+}
+
+variable "cluster_r53_domainfilters" {
+  default = {
+    live-1  = ["*"]
+    manager = ["manager.cloud-platform.service.justice.gov.uk.", "cloud-platform.service.justice.gov.uk."]
+  }
+}
