1a57d6bb2c0581a0efea0f98b7cdd81a1118434e
Alejandro Garrido Mota <garridomota@gmail.com>
2020-05-20 13:32:39 +0100

    Updated components to the latest module versions

--- terraform/cloud-platform-components/monitoring.tf
+++ terraform/cloud-platform-components/monitoring.tf
@@ -1,6 +1,6 @@
 
 module "prometheus" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.1"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
--- terraform/cloud-platform-components/velero.tf
+++ terraform/cloud-platform-components/velero.tf
@@ -1,5 +1,5 @@
 module "velero" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=0.0.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=0.0.4"
 
   iam_role_nodes        = data.aws_iam_role.nodes.arn
   dependence_prometheus = module.prometheus.helm_prometheus_operator_status
