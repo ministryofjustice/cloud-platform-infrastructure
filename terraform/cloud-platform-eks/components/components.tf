809ca02ba87a72a9214cdba30c588a926197b481
Alejandro Garrido Mota <garridomota@gmail.com>
2020-05-22 12:28:33 +0100

    Remove grafana datasource from EKS

--- terraform/cloud-platform-eks/components/components.tf
+++ terraform/cloud-platform-eks/components/components.tf
@@ -65,7 +65,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.2"
 
   alertmanager_slack_receivers = var.alertmanager_slack_receivers
   iam_role_nodes               = data.aws_iam_role.nodes.arn
