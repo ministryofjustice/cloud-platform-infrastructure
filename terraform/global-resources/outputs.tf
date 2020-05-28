34b8f1dd4e5c3c33ae7c48e7ecd6949db1ffcea2
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-05-28 15:41:10 +0100

    Update global-resources outputs.tf
    Removed live-0 tf resources

--- terraform/global-resources/outputs.tf
+++ terraform/global-resources/outputs.tf
@@ -1,10 +1,10 @@
-output "k8s_zone_id" {
-  value = aws_route53_zone.k8s_integration_dsd_io.zone_id
-}
+# output "k8s_zone_id" {
+#   value = aws_route53_zone.k8s_integration_dsd_io.zone_id
+# }
 
-output "k8s_domain_name" {
-  value = aws_route53_zone.k8s_integration_dsd_io.name
-}
+# output "k8s_domain_name" {
+#   value = aws_route53_zone.k8s_integration_dsd_io.name
+# }
 
 output "cp_zone_id" {
   value = aws_route53_zone.cloud-platform_justice_gov_uk.zone_id
@@ -14,9 +14,9 @@ output "cp_domain_name" {
   value = aws_route53_zone.cloud-platform_justice_gov_uk.name
 }
 
-output "kops_state_store" {
-  value = aws_s3_bucket.kops_state_store.bucket
-}
+# output "kops_state_store" {
+#   value = aws_s3_bucket.kops_state_store.bucket
+# }
 
 output "cloud_platform_kops_state" {
   value = aws_s3_bucket.cloud_platform_kops_state.bucket
