5c83c567181544f6646f68daba6000cf7aa4a79b
Alejandro Garrido Mota <garridomota@gmail.com>
2020-06-03 10:25:15 +0100

    Deleted dependence_deploy, it was needed when we were using Helm 2 (all Helm charts use to depend on tiller)

--- terraform/cloud-platform-components/starter-pack.tf
+++ terraform/cloud-platform-components/starter-pack.tf
@@ -1,8 +1,7 @@
 
 module "starter_pack" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.0.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.0.6"
 
   enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
-  dependence_deploy   = "null_resource.deploy"
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
 }
