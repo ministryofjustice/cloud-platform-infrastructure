02e1c6083b306cb8a14713622a8549eb56c366ea
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-06-09 13:04:00 +0100

    Upgrade grafana to 7.0.2
    This is due to Grafana 7.0.2 released with important security fix

--- terraform/cloud-platform-eks/components/components.tf
+++ terraform/cloud-platform-eks/components/components.tf
@@ -61,7 +61,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.4"
 
   alertmanager_slack_receivers = var.alertmanager_slack_receivers
   iam_role_nodes               = data.aws_iam_role.nodes.arn
