f36348aa3982e7960f0fd2d450e5e47f9eafd5fc
Imran Awan <imran.awan@gec.io>
2020-06-17 17:08:30 +0100

    amended tests as per comments

--- smoke-tests/spec/concourse_spec.rb
+++ smoke-tests/spec/concourse_spec.rb
@@ -1,41 +1,30 @@
 require "spec_helper"
 
 describe "concourse-test", "eks-manager": true do
-  context "concourse namespace should exists" do
-    it "Fail if namespace doesnt exist" do
-      expect(namespace_exists?("concourse")).to eq(true)
-    end
+
+  specify {
+    expect(namespace_exists?("concourse")).to eq(true)
+  }
+
+  specify {
+    expect(namespace_exists?("concourse-main")).to eq(true)
+  }
+
+  it "runs postgresql pods" do
+    pods = get_running_app_pods("concourse", "postgresql")
+    expect(all_containers_running?(pods)).to eq(true)
   end
 
-  context "concourse-main namespace should exists" do
-    it "Fail if namespace doesnt exist" do
-      expect(namespace_exists?("concourse-main")).to eq(true)
-    end
+  it "runs concourse-worker pods" do
+    pods = get_running_app_pods("concourse", "concourse-worker")
+    expect(all_containers_running?(pods)).to eq(true)
   end
 
-  context "expected all concourse-worker pods running inside concourse namespace" do
-    let(:pods) { get_running_app_pods("concourse", "concourse-worker") }
-
-    it "all concourse-worker pods inside concourse are running" do
-      expect(all_containers_running?(pods)).to eq(true)
-    end
+  it "runs concourse-web pods" do
+    pods = get_running_app_pods("concourse", "concourse-web")
+    expect(all_containers_running?(pods)).to eq(true)
   end
-
-  context "expected all postgresql pods running inside concourse namespace" do
-    let(:pods) { get_running_app_pods("concourse", "postgresql") }
-
-    it "all postgresql pods inside concourse are running" do
-      expect(all_containers_running?(pods)).to eq(true)
-    end
-  end
-
-  context "expected all concourse-web running inside concourse namespace" do
-    let(:pods) { get_running_app_pods("concourse", "concourse-web") }
-
-    it "all concourse-web pods inside concourse are running" do
-      expect(all_containers_running?(pods)).to eq(true)
-    end
-  end
+ 
 end
 
 
