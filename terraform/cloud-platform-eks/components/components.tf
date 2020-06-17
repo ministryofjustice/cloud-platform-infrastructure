4636cb3dc7aed3b7c60d18edba3339534809ffda
Alejandro Garrido Mota <garridomota@gmail.com>
2020-06-17 16:39:08 +0100

    Default certificate must also include the wildcard.cloud-platform.service......

--- terraform/cloud-platform-eks/components/components.tf
+++ terraform/cloud-platform-eks/components/components.tf
@@ -70,7 +70,7 @@ module "ingress_controllers" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=0.0.5"
 
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  is_live_cluster     = false
+  is_live_cluster     = terraform.workspace == local.live_workspace ? false : true
 
   # This module requires helm and OPA already deployed
   dependence_prometheus  = module.monitoring.helm_prometheus_operator_status
