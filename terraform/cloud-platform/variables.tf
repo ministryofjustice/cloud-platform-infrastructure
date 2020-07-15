c8c5b870685728834663d6cdf57ecc4831406928
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-15 15:05:16 +0100

    Updated with the right tenant

--- terraform/cloud-platform/variables.tf
+++ terraform/cloud-platform/variables.tf
@@ -62,6 +62,6 @@ variable "enable_large_nodesgroup" {
 variable "auth0_tenant_domain" {
   description = "The auth0 domain/tenant used, different for live/test clusters"
   default = {
-    default = "moj-cloud-platforms-dev.eu.auth0.com"
+    default = "justice-cloud-platform.eu.auth0.com"
   }
 }
