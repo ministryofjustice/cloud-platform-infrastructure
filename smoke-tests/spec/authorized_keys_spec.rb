493500d0d2291231abb63c87c007067f9967bd80
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-20 11:06:35 +0100

    Fixed integation tests with the latest changes

--- smoke-tests/spec/authorized_keys_spec.rb
+++ smoke-tests/spec/authorized_keys_spec.rb
@@ -7,14 +7,6 @@ describe "authorized_keys", "live-1": true, speed: "fast" do
     end
   end
 
-  context "expected all pods running inside authorized-keys-provider namespace" do
-    let(:pods) { get_running_app_pods("authorized-keys-provider", "authorized-keys-provider") }
-
-    it "all pods inside authorized-keys-provider are running" do
-      expect(all_containers_running?(pods)).to eq(true)
-    end
-  end
-
   context "expected authorized_keys file within the bastion repo" do
     it "authorized_keys file exists and it can be downloaded" do
       response = URI.open("https://github.com/ministryofjustice/cloud-platform-terraform-bastion/blob/main/files/authorized_keys.txt")
