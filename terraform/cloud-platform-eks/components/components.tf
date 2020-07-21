a95bf9803d690397bf2f3ad4ea5c2e95aa37b541
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-07-21 18:26:39 +0100

    Install sonarqube in EKS using terraform sonarqube module
    https://github.com/ministryofjustice/cloud-platform-terraform-sonarqube

--- terraform/cloud-platform-eks/components/components.tf
+++ terraform/cloud-platform-eks/components/components.tf
@@ -127,3 +127,17 @@ module "velero" {
   eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
 }
 
+module "sonarqube" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sonarqube?ref=0.0.1"
+
+  vpc_id                        = data.terraform_remote_state.cluster.outputs.vpc_id
+  internal_subnets              = data.terraform_remote_state.cluster.outputs.internal_subnets
+  internal_subnets_ids          = data.terraform_remote_state.cluster.outputs.internal_subnets_ids
+  cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+  oidc_components_client_id     = data.terraform_remote_state.cluster.outputs.oidc_components_client_id
+  oidc_components_client_secret = data.terraform_remote_state.cluster.outputs.oidc_components_client_secret
+  oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
+
+  # This is to enable sonarqube, by default it is false for test clusters
+  enable_sonarqube = terraform.workspace == local.live_workspace ? true : false
+}
