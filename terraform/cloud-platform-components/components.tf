4e0e490fe94b33209630c76fd95fc4ee5035b4d0
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2020-07-31 12:02:39 +0100

    Bump starter pack release for new multi-container-app chart (#823)

--- terraform/cloud-platform-components/components.tf
+++ terraform/cloud-platform-components/components.tf
@@ -101,7 +101,7 @@ module "opa" {
 }
 
 module "starter_pack" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.0.8"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.0.9"
 
   enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
