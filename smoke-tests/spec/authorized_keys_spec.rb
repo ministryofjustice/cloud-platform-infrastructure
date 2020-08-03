a57f08f5ebc271ab419fee6d4ad6893260924a11
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2020-08-03 11:26:45 +0100

    Remove test for checking the authorized_keys_provider namespace. This no longer exists and the functionality is moved to the pipeline (#825)

--- smoke-tests/spec/authorized_keys_spec.rb
+++ smoke-tests/spec/authorized_keys_spec.rb
@@ -1,12 +1,6 @@
 require "spec_helper"
 
 describe "authorized_keys", "live-1": true, speed: "fast" do
-  context "authorized-keys-provider namespace should exists" do
-    it "Fail if namespace doesnt exist" do
-      expect(namespace_exists?("authorized-keys-provider")).to eq(true)
-    end
-  end
-
   context "expected authorized_keys file within the bastion repo" do
     it "authorized_keys file exists and it can be downloaded" do
       response = URI.open("https://github.com/ministryofjustice/cloud-platform-terraform-bastion/blob/main/files/authorized_keys.txt")
