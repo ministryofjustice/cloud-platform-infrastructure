1717ad98b74826a005f0f118b35eff425e45365b
PaulWyborn <paul.wyborn@digital.justice.gov.uk>
2020-08-11 15:01:21 +0100

    new release 3.6  to cloud-platform-terraform-monitoring

--- terraform/cloud-platform-components/components.tf
+++ terraform/cloud-platform-components/components.tf
@@ -62,7 +62,7 @@ module "logging" {
 }
 
 module "prometheus" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.6"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
