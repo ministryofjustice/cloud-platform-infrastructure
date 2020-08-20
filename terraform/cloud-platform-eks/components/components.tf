9e57d5d6f3208c2e4803e48a404e507dbc5854b2
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2020-08-20 13:09:21 +0100

    Bump EKS monitoring module to 0.4.0 (#861)

--- terraform/cloud-platform-eks/components/components.tf
+++ terraform/cloud-platform-eks/components/components.tf
@@ -92,7 +92,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.3.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=0.4.0
 
   alertmanager_slack_receivers = var.alertmanager_slack_receivers
   iam_role_nodes               = data.aws_iam_role.nodes.arn
