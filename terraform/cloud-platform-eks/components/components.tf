094cf24f777e9060d900af9bb1260f2329a8542d
Alejandro Garrido Mota <garridomota@gmail.com>
2020-10-20 15:48:07 +0100

    Upgraded logging module in EKS (deleting fluentd)

--- terraform/cloud-platform-eks/components/components.tf
+++ terraform/cloud-platform-eks/components/components.tf
@@ -82,13 +82,12 @@ module "ingress_controllers" {
 }
 
 module "logging" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=0.0.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=1.0.0"
 
   elasticsearch_host       = lookup(var.elasticsearch_hosts_maps, terraform.workspace, "placeholder-elasticsearch")
   elasticsearch_audit_host = lookup(var.elasticsearch_audit_hosts_maps, terraform.workspace, "placeholder-elasticsearch")
 
   dependence_prometheus       = module.monitoring.helm_prometheus_operator_status
-  dependence_priority_classes = kubernetes_priority_class.cluster_critical
 }
 
 module "monitoring" {
