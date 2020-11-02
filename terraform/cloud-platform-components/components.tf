4d38a576841328464a248921f53f47f3b0705d4b
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2020-11-02 16:58:29 +0000

    Update components.tf

--- terraform/cloud-platform-components/components.tf
+++ terraform/cloud-platform-components/components.tf
@@ -2,6 +2,7 @@ module "cert_manager" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=0.0.9"
 
 
+  
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   hostzone            = lookup(var.cluster_r53_resource_maps, terraform.workspace, ["arn:aws:route53:::hostedzone/${data.terraform_remote_state.cluster.outputs.hosted_zone_id}"])
