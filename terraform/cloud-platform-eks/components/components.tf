f41b234a2087b95d41217386f0c0d90c946128a3
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2020-11-04 17:02:42 +0000

    Update components.tf

--- terraform/cloud-platform-eks/components/components.tf
+++ terraform/cloud-platform-eks/components/components.tf
@@ -31,7 +31,7 @@ module "concourse" {
 }
 
 module "cert_manager" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=0.0.9"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=0.0.8"
 
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
