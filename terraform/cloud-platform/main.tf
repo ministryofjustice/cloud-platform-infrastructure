63bc4fa5b6e1ff3c50d7956a4a71b30a9c2f6c12
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2020-11-04 16:28:44 +0000

    Update main.tf

--- terraform/cloud-platform/main.tf
+++ terraform/cloud-platform/main.tf
@@ -57,7 +57,7 @@ locals {
 ########
 
 module "kops" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-kops?ref=0.0.9"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-kops?ref=0.0.8"
 
   vpc_name            = local.vpc
   cluster_domain_name = trimsuffix(local.cluster_base_domain_name, ".")
