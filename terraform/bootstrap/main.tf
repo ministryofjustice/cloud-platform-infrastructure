492ac9fe3e55d0c7bf9b90b7c9ea7ebeada9ed88
Alejandro Garrido Mota <garridomota@gmail.com>
2020-11-05 17:24:09 +0000

    Update mod ref to latest

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -4,8 +4,7 @@ provider "aws" {
 }
 
 module "tf_states_backend" {
-  # source = "github.com/ministryofjustice/cloud-platform-terraform-tfstate-backend?ref=0.0.1"
-  source = "/home/mogaal/workspace/github/ministryofjustice/cloud-platform-terraform-tfstate-backend"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-tfstate-backend?ref=0.0.2"
 
   s3_bucket_name = "cloud-platform-terraform-state"
   region = "eu-west-1"
