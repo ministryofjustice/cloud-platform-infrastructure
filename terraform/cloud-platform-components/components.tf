51fa129bee4f275ed56dbdcf743b64d24a251061
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-11-10 13:09:00 +0000

    Bump Starter pack version. This has ability to create multiple starter pack and remove some redundant secrets when multi-container app is disabled

--- terraform/cloud-platform-components/components.tf
+++ terraform/cloud-platform-components/components.tf
@@ -134,7 +134,7 @@ module "opa" {
 }
 
 module "starter_pack" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.0.9"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.1"
 
   enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
