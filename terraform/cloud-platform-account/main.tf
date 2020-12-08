72d1e0b36fac6590921e88312ec6a00e19d1e44a
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2020-12-08 11:34:24 +0000

    Update main.tf

--- terraform/cloud-platform-account/main.tf
+++ terraform/cloud-platform-account/main.tf
@@ -17,14 +17,14 @@ provider "aws" {
 
 # IAM configuration for cloud-platform. Users, groups, etc
 module "iam" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-iam?ref=0.0.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-iam?ref=0.0.4"
 
   aws_account_name = "cloud-platform-aws"
 }
 
 # Baselines: cloudtrail, cloudwatch, lambda. Everything that our accounts should have
 module "baselines" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-baselines?ref=0.0.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-baselines?ref=0.0.3"
 
   enable_logging           = true
   enable_slack_integration = true
