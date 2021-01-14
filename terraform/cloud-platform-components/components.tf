0a209dc4d1ba444a4c9ceb06a8aabd49cffcd128
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-01-14 17:44:28 +0000

    Update starter pack module versionto 0.1.2 and enable multi container app.

--- terraform/cloud-platform-components/components.tf
+++ terraform/cloud-platform-components/components.tf
@@ -137,11 +137,10 @@ module "opa" {
 }
 
 module "starter_pack" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.2"
 
   enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  multi_container_app = false
 }
 
 module "velero" {
