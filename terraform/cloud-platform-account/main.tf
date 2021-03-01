9290a96154125458c1efd0edf2a84fa8c7d88088
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2021-03-01 10:45:56 +0000

    Add new bucket to the list of public access exceptions
    This was requested in this PR:
    https://github.com/ministryofjustice/cloud-platform-environments/pull/4388

--- terraform/cloud-platform-account/main.tf
+++ terraform/cloud-platform-account/main.tf
@@ -36,7 +36,8 @@ module "baselines" {
   s3_bucket_block_publicaccess_exceptions = [
     "cloud-platform-9025c5a1a81bca7eaefd78a38df7d7de",
     "cloud-platform-fdc5e4b70a599d8ea84b4ffd31a832b3",
-    "cloud-platform-6cf3132ef8fce52bb371b1d02f40c36d"
+    "cloud-platform-6cf3132ef8fce52bb371b1d02f40c36d",
+    "cloud-platform-dfc64bcd6ed89a72777fc7924f9da01e",
   ]
 }
 
