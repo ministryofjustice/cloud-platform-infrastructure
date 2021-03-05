e1e3e7d8a75c7552e5ecaccafbb6855630f0c471
Alejandro Garrido Mota <garridomota@gmail.com>
2021-03-05 16:11:26 +0000

    Updated tf remote state

--- terraform/cloud-platform-account/main.tf
+++ terraform/cloud-platform-account/main.tf
@@ -3,8 +3,7 @@ terraform {
   backend "s3" {
     bucket               = "cloud-platform-terraform-state"
     region               = "eu-west-1"
-    key                  = "terraform.tfstate"
-    workspace_key_prefix = "cloud-platform-account"
+    key                  = "cloud-platform-account/terraform.tfstate"
     profile              = "moj-cp"
     dynamodb_table       = "cloud-platform-terraform-state"
   }
