cc05dde438aa748ceab2fde961912943b3b2b336
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-03-11 14:14:52 +0000

    Bump all component versions in bootstrap to Terraform 0.13

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 }
 
 module "tf_states_backend" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-tfstate-backend?ref=0.0.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-tfstate-backend?ref=0.0.3"
 
   s3_bucket_name    = "cloud-platform-terraform-state"
   region            = "eu-west-1"
