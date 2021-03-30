71503ea4248810176ad4883eca86b74a213ac9c9
Alejandro Garrido Mota <garridomota@gmail.com>
2021-03-30 09:50:19 +0100

    Moved EKS and EKS components to its own folder structure (also in S3)

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/main.tf
@@ -9,7 +9,7 @@ terraform {
     bucket               = "cloud-platform-terraform-state"
     region               = "eu-west-1"
     key                  = "terraform.tfstate"
-    workspace_key_prefix = "cloud-platform-eks"
+    workspace_key_prefix = "aws-accounts/cloud-platform-aws/vpc/eks"
     profile              = "moj-cp"
     dynamodb_table       = "cloud-platform-terraform-state"
   }
