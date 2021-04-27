3914aadebde5144732cf5d0a861bc5f146f00817
Paul Wyborn <paul.wyborn@digital.justice.gov.uk>
2021-04-27 12:48:16 +0100

    Update components.tf
    Update CPU alerts - latest release

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -95,7 +95,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=1.6.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=1.6.6"
 
   alertmanager_slack_receivers = var.alertmanager_slack_receivers
   iam_role_nodes               = data.aws_iam_role.nodes.arn
