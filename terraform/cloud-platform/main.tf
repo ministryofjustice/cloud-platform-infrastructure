65bfb1fd28cfa2c22dbaa57867809645bc1d86c9
Alejandro Garrido Mota <garridomota@gmail.com>
2021-04-29 14:20:59 +0100

    Disable ingress nodes

--- terraform/cloud-platform/main.tf
+++ terraform/cloud-platform/main.tf
@@ -61,7 +61,7 @@ locals {
 ########
 
 module "kops" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-kops?ref=0.2.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-kops?ref=0.2.1"
 
   vpc_name            = local.vpc
   cluster_domain_name = trimsuffix(local.cluster_base_domain_name, ".")
