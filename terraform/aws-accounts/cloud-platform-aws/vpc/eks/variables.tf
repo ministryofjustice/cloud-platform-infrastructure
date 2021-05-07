352a40761a63b28c23c5fb0a9a3c043aabdbbfb6
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-05-07 11:42:24 +0100

    Change worker node type to match live in kops

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
@@ -7,11 +7,11 @@ variable "vpc_name" {
 
 variable "cluster_node_count" {
   description = "The number of worker node in the cluster"
-  default     = "4"
+  default     = "19"
 }
 
 variable "worker_node_machine_type" {
   description = "The AWS EC2 instance types to use for worker nodes"
-  default     = "m4.xlarge"
+  default     = "r5.xlarge"
 }
 
