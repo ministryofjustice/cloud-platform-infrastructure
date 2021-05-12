9696e5a99262921e38316a04534370e47599f4b2
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-05-12 10:38:06 +0100

    dot not add concourse user in manager cluster

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/k8s-resources.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/k8s-resources.tf
@@ -84,6 +84,7 @@ resource "kubernetes_cluster_role_binding" "webops" {
 }
 
 resource "kubernetes_service_account" "concourse_build_environments" {
+  count = terraform.workspace == "manager" ? 0 : 1
   metadata {
     name      = "concourse-build-environments"
     namespace = "kube-system"
@@ -91,6 +92,7 @@ resource "kubernetes_service_account" "concourse_build_environments" {
 }
 
 resource "kubernetes_cluster_role_binding" "concourse_build_environments" {
+  count = terraform.workspace == "manager" ? 0 : 1
   metadata {
     name = "concourse-build-environments"
   }
