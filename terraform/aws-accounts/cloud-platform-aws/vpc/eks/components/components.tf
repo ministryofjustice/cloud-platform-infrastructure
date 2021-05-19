7f06deb600217bd348780c3096230db231084a6a
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2021-05-19 14:06:01 +0100

    Update components.tf

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -130,7 +130,7 @@ module "opa" {
 }
 
 module "velero" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=0.0.8"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=tf-docs-cleanup"
 
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
