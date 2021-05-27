473990fff567a6263bd23a17d546ac184368e4f9
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2021-05-27 03:45:06 -0700

    Update components.tf

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -1,5 +1,6 @@
 
 module "concourse" {
+  count  = terraform.workspace == manager ? 1 : 0
   source = "github.com/ministryofjustice/cloud-platform-terraform-concourse?ref=1.6.3"
 
   vpc_id                                            = data.terraform_remote_state.cluster.outputs.vpc_id
@@ -136,6 +137,7 @@ module "velero" {
 }
 
 module "sonarqube" {
+  count  = terraform.workspace == manager ? 1 : 0
   source = "github.com/ministryofjustice/cloud-platform-terraform-sonarqube?ref=0.0.4"
 
   vpc_id                        = data.terraform_remote_state.cluster.outputs.vpc_id
