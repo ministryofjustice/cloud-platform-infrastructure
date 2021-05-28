023da118ee93e45d91105d26727484bdd0470673
Alejandro Garrido Mota <garridomota@gmail.com>
2021-05-28 15:45:49 +0100

    Moved node_count to maps

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
@@ -1,8 +1,3 @@
-variable "cluster_node_count" {
-  description = "The number of worker node in the cluster"
-  default     = "4"
-}
-
 variable "worker_node_machine_type" {
   description = "The AWS EC2 instance types to use for worker nodes"
   default     = "m4.xlarge"
