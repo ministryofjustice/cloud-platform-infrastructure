0d84561f63d96a2020a04e35e936b98d51d23dc7
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-05-28 16:30:03 +0100

    rebase

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
@@ -1,15 +1,3 @@
-variable "vpc_name" {
-  description = "The VPC name where the cluster(s) are going to be provisioned. VPCs are created in cloud-platform-network"
-  default = {
-    manager = "live-1"
-  }
-}
-
-variable "cluster_node_count" {
-  description = "The number of worker node in the cluster"
-  default     = "4"
-}
-
 variable "worker_node_machine_type" {
   description = "The AWS EC2 instance types to use for worker nodes"
   default     = "m4.xlarge"
@@ -22,4 +10,3 @@ variable "dockerhub_user" {
 variable "dockerhub_token" {
   description = "Token for the above"
 }
-
