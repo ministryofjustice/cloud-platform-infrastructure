1f786faf4cdcacb39c62f0d7f89351b18b2d6971
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-06-07 19:12:35 +0300

    use AWS Calico (#1214)

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/versions.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/versions.tf
@@ -13,6 +13,10 @@ terraform {
     null = {
       source = "hashicorp/null"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.11.1"
+    }
   }
   required_version = ">= 0.14"
 }
