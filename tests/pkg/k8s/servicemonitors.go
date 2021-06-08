20b2d65521231f1f0b92cda1904d3365073d6cf0
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-06-08 17:46:24 +0100

    Seperate fetching client functions in client.go. This can be reused for another crds

--- tests/pkg/k8s/servicemonitors.go
+++ tests/pkg/k8s/servicemonitors.go
@@ -4,15 +4,12 @@ import (
 	"context"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/clientcmd"
 
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/testing"
 	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	prom "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned/typed/monitoring/v1"
 )
 
 // GetServiceMonitorSet returns a serviceMonitor resource in the provided namespace with the given name. This will
@@ -25,22 +22,9 @@ func GetServiceMonitorSet(t testing.TestingT, options *k8s.KubectlOptions, servi
 
 // GetServiceMonitorSetE returns a serviceMonitor resource in the provided namespace with the given name.
 func GetServiceMonitorSetE(t testing.TestingT, options *k8s.KubectlOptions, serviceMonitorName string) (*v1.ServiceMonitor, error) {
-
-	var err error
-
-	kubeConfigPath, err := options.GetConfigPath(t)
+	client, err := GetPrometheusClientSetE(t, options)
 	if err != nil {
 		return nil, err
-	}
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	if err != nil {
-		return nil, errors.Wrap(err, "build config from flags failed")
-	}
-
-	client, err := prom.NewForConfig(config)
-	if err != nil {
-		return nil, errors.Wrap(err, "creating v1 monitoring client failed")
 	}
 	return client.ServiceMonitors(options.Namespace).Get(context.Background(), serviceMonitorName, metav1.GetOptions{})
 }
