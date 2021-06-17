fa1a1027e8c703fcc63d5e49bd5d4a76f68b9144
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-06-17 10:01:48 +0100

    add prom dep back

--- terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
@@ -5,7 +5,7 @@ module "cert_manager" {
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   hostzone            = lookup(var.cluster_r53_resource_maps, terraform.workspace, ["arn:aws:route53:::hostedzone/${data.terraform_remote_state.cluster.outputs.hosted_zone_id}"])
 
-  dependence_prometheus = "ignore" #module.prometheus.helm_prometheus_operator_status
+  dependence_prometheus = module.prometheus.helm_prometheus_operator_status
   dependence_opa        = "ignore"
 }
 
