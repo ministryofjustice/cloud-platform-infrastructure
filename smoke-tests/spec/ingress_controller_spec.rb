42f8b641d08a6cf1d869691899801af8c0dad9fe
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-06-17 10:57:11 +0100

    actually test the ingress

--- smoke-tests/spec/ingress_controller_spec.rb
+++ smoke-tests/spec/ingress_controller_spec.rb
@@ -1,14 +1,13 @@
 require "spec_helper"
 
-describe "ingress controllers", speed: "fast" do
+describe "test ingress controllers", speed: "fast", "live-1": false do
   context "default" do
-    # This needs to be any url of an ingress on the default controller,
-    let(:url) { "https://reports.cloud-platform.service.justice.gov.uk/dashboard" }
+    host = "prometheus.apps.#{current_cluster}"
+    let(:url) { "https://#{host}" }
 
-    it "returns 200 for http get" do
+    it "returns 302 for http get" do
       result = Net::HTTP.get_response(URI(url))
-      expect(result.code).to eq("200")
-      expect(result.message.strip).to eq("OK")
+      expect(result.code).to eq("302")
     end
 
     it "does not return a server header" do
@@ -18,16 +17,20 @@ describe "ingress controllers", speed: "fast" do
       expect(headers).to_not have_key("server")
     end
   end
+end
 
-  context "eks" do
-    let(:url) { "https://concourse.cloud-platform.service.justice.gov.uk/" }
+describe "live ingress controllers", speed: "fast", "live-1": true do
+  context "default" do
+    host = "prometheus.cloud-platform.service.justice.gov.uk"
+    let(:url) { "https://#{host}" }
 
     it "returns 302 for http get" do
       result = Net::HTTP.get_response(URI(url))
-      expect(result.code).to eq("200")
+      expect(result.code).to eq("302")
     end
 
     it "does not return a server header" do
+      # The `Server` response header should be suppressed by the ingress-controller configuration
       result = Net::HTTP.get_response(URI(url))
       headers = result.each_header.to_h
       expect(headers).to_not have_key("server")
