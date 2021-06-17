bc2e0db3d6d2d2ad6020cc3e731c01288c02d2d9
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-06-17 16:06:28 +0100

    Upgrade cert-maanger to v1.4.0
    This release fixes an issue where an ACME Certificate,
    with a long name (52 characters or more)
    jetstack/cert-manager#3866.
    
    Ticket related to it:
    ministryofjustice/cloud-platform#2976

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -36,7 +36,7 @@ module "concourse" {
 }
 
 module "cert_manager" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=1.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=1.2.0"
 
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
