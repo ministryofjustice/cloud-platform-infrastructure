58ec377eb8d233de49e66b910d368c403704525f
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-06-21 13:36:57 +0100

    Add kuberos module to eks. This is needed for users to authenticate to the cluster

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -96,6 +96,17 @@ module "modsec_ingress_controllers" {
   dependence_certmanager = module.cert_manager.helm_cert_manager_status
 }
 
+module "kuberos" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.3.0"
+
+  cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+  oidc_kubernetes_client_id     = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_id
+  oidc_kubernetes_client_secret = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_secret
+  oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
+  cluster_address               = data.terraform_remote_state.cluster.outputs.cluster_endpoint
+  create_aws_redirect           = false
+}
+
 module "logging" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=1.1.0"
 
