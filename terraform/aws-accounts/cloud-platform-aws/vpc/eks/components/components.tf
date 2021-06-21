52648cdc8fc64d9f68451e1663b7f2d782c9d40b
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-06-21 17:26:19 +0100

    Bump kuberos module release

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -97,7 +97,7 @@ module "modsec_ingress_controllers" {
 }
 
 module "kuberos" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.3.1"
 
   cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   oidc_kubernetes_client_id     = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_id
