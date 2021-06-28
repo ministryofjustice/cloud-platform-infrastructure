4ba747aa298f0d79301cf79bfcdb0a5f36fa92c8
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-06-28 08:10:35 +0100

    Update components.tf
    fixes https://concourse.cloud-platform.service.justice.gov.uk/teams/main/pipelines/create-test-destroy/jobs/destroy-cluster/builds/53

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -1,7 +1,7 @@
 
 module "concourse" {
   count  = terraform.workspace == "manager" ? 1 : 0
-  source = "github.com/ministryofjustice/cloud-platform-terraform-concourse?ref=1.6.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-concourse?ref=1.6.4"
 
   vpc_id                                            = data.terraform_remote_state.cluster.outputs.vpc_id
   internal_subnets                                  = data.terraform_remote_state.cluster.outputs.internal_subnets
