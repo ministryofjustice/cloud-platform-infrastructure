78621acc294228591d1f683aecf072e6fb2194ed
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-06-28 14:55:20 +0100

    Add psp as part of clsuter creation. This will prevent psp getting destroyed when destroying components and hence aws-node pods can be recreated with correct psp

---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/k8s-resources.tf
@@ -0,0 +1,20 @@
+
+
+#########################
+# Pod Security Policies #
+#########################
+
+resource "null_resource" "pod_security_policy" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${path.module}/resources/psp/pod-security-policy.yaml"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl delete --ignore-not-found -f ${path.module}/resources/psp/pod-security-policy.yaml"
+  }
+
+  triggers = {
+    content = filesha1("${path.module}/resources/psp/pod-security-policy.yaml")
+  }
+}
