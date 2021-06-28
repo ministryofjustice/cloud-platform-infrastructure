be95fe63f5f3986a6656db669237e1f6a4ededc8
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-06-28 18:05:25 +0100

    Add dependency of cluster creation to psp null_resource

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/k8s-resources.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/k8s-resources.tf
@@ -5,6 +5,7 @@
 #########################
 
 resource "null_resource" "pod_security_policy" {
+  depends_on = [module.eks.cluster_id]
   provisioner "local-exec" {
     command = "kubectl apply -f ${path.module}/resources/psp/pod-security-policy.yaml"
   }
