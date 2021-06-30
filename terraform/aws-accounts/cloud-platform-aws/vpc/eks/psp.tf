33333b4d20a7d09cb96fb432316d67fb66209530
Alejandro Garrido Mota <garridomota@gmail.com>
2021-06-30 14:58:41 +0100

    Added remaining PSP as terraform resources
    Following our plan of migrating PSP from null_resource to native terraform resources

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/psp.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/psp.tf
@@ -27,14 +27,124 @@ resource "kubernetes_pod_security_policy" "privileged" {
       "*",
     ]
 
+    host_ports {
+      min = 0
+      max = 65535
+    }
 
     host_network = true
-
     host_ipc = true
     host_pid = true
 
+    run_as_user {
+      rule = "RunAsAny"
+    }
 
+    se_linux {
+      rule = "RunAsAny"
+    }
 
+    supplemental_groups {
+      rule = "RunAsAny"
+    }
+
+    fs_group {
+      rule = "RunAsAny"
+    }
+  }
+}
+
+resource "kubernetes_pod_security_policy" "restricted" {
+  metadata {
+    name = "restricted"
+    annotations = {
+      "seccomp.security.alpha.kubernetes.io/allowedProfileNames" = "docker/default,runtime/default"
+      "seccomp.security.alpha.kubernetes.io/defaultProfileName" = "runtime/default"
+    }
+  }
+
+  spec {
+    privileged                 = false
+    allow_privilege_escalation = false
+
+    required_drop_capabilities = [
+      "ALL",
+    ]
+
+    volumes = [
+      "configMap",
+      "emptyDir",
+      "projected",
+      "secret",
+      "downwardAPI",
+      # Assume that persistentVolumes set up by the cluster admin are safe to use.
+      "persistentVolumeClaim",
+    ]
+
+    host_network = false
+    host_ipc = false
+    host_pid = false
+
+    run_as_user {
+      rule = "MustRunAsNonRoot"
+    }
+
+    se_linux {
+      rule = "RunAsAny"
+    }
+
+    supplemental_groups {
+      rule = "MustRunAs"
+      range {
+        min = 1
+        max = 65535
+      }
+    }
+
+    fs_group {
+      rule = "MustRunAs"
+      range {
+        min = 1
+        max = 65535
+      }
+    }
+
+    read_only_root_filesystem = true
+  }
+}
+
+resource "kubernetes_pod_security_policy" "aws_node" {
+  metadata {
+    name = "aws-node"
+    annotations = {
+      "seccomp.security.alpha.kubernetes.io/allowedProfileNames" = "*"
+    }
+    labels = {
+      "kubernetes.io/cluster-service" = "true"
+    }
+  }
+
+  spec {
+    privileged                 = true
+    allow_privilege_escalation = true
+
+    allowed_capabilities = [
+      "NET_BIND_SERVICE",
+      "NET_ADMIN"
+    ]
+
+    volumes = [
+      "*",
+    ]
+
+    host_ports {
+      min = 0
+      max = 65535
+    }
+
+    host_network = true
+    host_ipc = true
+    host_pid = true
 
     run_as_user {
       rule = "RunAsAny"
