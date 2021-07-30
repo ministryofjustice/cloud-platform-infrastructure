7799c68523671b15804f2631614dce42aea17507
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-07-30 10:56:11 +0100

    remove gp3 for now

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/k8s-resources.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/k8s-resources.tf
@@ -18,61 +18,10 @@ resource "kubernetes_storage_class" "storageclass" {
   }
 }
 
-data "aws_iam_policy_document" "ebs_doc" {
-  statement {
-    actions = [
-      "ec2:CreateSnapshot",
-      "ec2:AttachVolume",
-      "ec2:CreateVolume",
-      "ec2:DetachVolume",
-      "ec2:DeleteVolume",
-      "ec2:ModifyVolume",
-      "ec2:DescribeAvailabilityZones",
-      "ec2:DescribeInstances",
-      "ec2:DeleteSnapshot",
-      "ec2:DescribeSnapshots",
-      "ec2:CreateTags",
-      "ec2:DeleteTags",
-      "ec2:DescribeTags",
-      "ec2:DescribeVolumes",
-      "ec2:DescribeVolumesModifications",
-      "sts:AssumeRoleWithWebIdentity"
-    ]
-    resources = [
-      "*",
-    ]
-  }
-}
-
-resource "aws_iam_policy" "ebs_policy" {
-  name        = "eks-csi-policy-${terraform.workspace}"
-  path        = "/cloud-platform/"
-  policy      = data.aws_iam_policy_document.ebs_doc.json
-  description = "Policy for EKS CSI driver"
-}
-
-module "ebs_irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=role-name"
-
-  eks_cluster      = terraform.workspace
-  namespace        = "kube-system"
-  service_account  = "ebs-csi-controller-sa"
-  role_policy_arns = [aws_iam_policy.ebs_policy.arn]
-}
-
-resource "helm_release" "aws_ebs" {
-  name       = "aws-ebs-csi-driver"
-  chart      = "aws-ebs-csi-driver"
-  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-  namespace  = "kube-system"
-  version    = "2.0.3"
-
-  set {
-    name  = "controller.serviceAccount.create"
-    value = "false"
-  }
-
-  depends_on = [module.ebs_irsa]
+module "eks_csi" {
+  count       = 0
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-eks-csi?ref=gp3"
+  eks_cluster = terraform.workspace
 }
 
 resource "kubernetes_storage_class" "storageclass_gp3" {
@@ -93,7 +42,7 @@ resource "kubernetes_storage_class" "storageclass_gp3" {
     encrypted = "true"
   }
 
-  depends_on = [helm_release.aws_ebs]
+  depends_on = [module.eks_csi]
 }
 
 # mini-hack to remove the default from GP2 because otherwise terraform tries (and fails) to create the storageclass again
