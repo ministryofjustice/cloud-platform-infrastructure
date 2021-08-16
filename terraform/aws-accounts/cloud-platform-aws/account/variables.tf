10c01ffcfac6557c4266f0de4cb59bbd566a00e8
Alejandro Garrido Mota <garridomota@gmail.com>
2021-08-16 16:32:19 +0100

    Migrated and refactor kubeconfig generation file
    Fully migration of the kubeconfig file generation from concourse repo (https://github.com/ministryofjustice/cloud-platform-concourse/blob/main/pipelines/manager/main/build-environments/kubeconfig.tf) to infrastructure repo.
    
    Also refactor how to file is generated

--- terraform/aws-accounts/cloud-platform-aws/account/variables.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/variables.tf
@@ -6,3 +6,8 @@ variable "aws_region" {
   description = "Region where components and resources are going to be deployed"
   default     = "eu-west-2"
 }
+
+variable "kubeconfig_clusters" {
+  description = "Cluster(s) credentials used by concourse pipelines to run terraform"
+  type        = any
+}
