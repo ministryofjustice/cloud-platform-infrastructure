2d2e040d1de41d2a6a30f8f17fdae37b2c01374d
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2021-09-20 17:33:03 +0100

    Update prometheus to use the latest version (#1356)
    This version includes:
    - Increase memory limit for prometheus
    - User new node tag for affinity

--- terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
@@ -58,7 +58,7 @@ module "logging" {
 }
 
 module "prometheus" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=1.7.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=1.7.7"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
