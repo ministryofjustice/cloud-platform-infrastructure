b6ffbcda9fb0317062ca1314c0a4def3111b3815
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-09-21 19:13:15 +0100

    Update set-identifier for cert-manager tests

--- smoke-tests/spec/cert_manager_spec.rb
+++ smoke-tests/spec/cert_manager_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe "cert-manager" do
   let(:namespace) { "cert-manager-test-#{readable_timestamp}" }
   ingress_class = "nginx"
+  ingress_name = "integration-test-app-ing"
+  set_identifier = "#{ingress_name}-#{namespace}-#{external_dns_annotation_color}"
 
   before do
     create_namespace(namespace)
@@ -21,6 +23,7 @@ describe "cert-manager" do
         namespace: namespace,
         host: host,
         ingress_class: ingress_class,
+        set_identifier: set_identifier,
         file: "spec/fixtures/helloworld-deployment.yaml.erb",
         binding: binding
       )
