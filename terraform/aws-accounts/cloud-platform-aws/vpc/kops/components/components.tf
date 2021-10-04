e96f10204b6c2b079f6a3fe9f0e6ca11c0462fbf
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-10-04 12:43:50 +0100

    dashboard link to AlertManager slack template

--- terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
@@ -58,7 +58,7 @@ module "logging" {
 }
 
 module "prometheus" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=1.7.8"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=1.7.9"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
