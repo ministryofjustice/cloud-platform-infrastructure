9fe0a8c90b316072901ffd0169e1d5afab9c8365
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2021-10-20 15:00:22 +0100

    Update logging module to use latest version (#1397)
    Add helm dependency for prometheus_rule_alert,
    as prometheus_rule_alert have a dependency for Prometheus

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -104,7 +104,7 @@ module "kuberos" {
 }
 
 module "logging" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=1.1.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=1.1.6"
 
   elasticsearch_host       = lookup(var.elasticsearch_hosts_maps, terraform.workspace, "placeholder-elasticsearch")
   elasticsearch_audit_host = lookup(var.elasticsearch_audit_hosts_maps, terraform.workspace, "placeholder-elasticsearch")
