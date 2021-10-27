e87e01521f75e4f1b61c663489bb0fdeef1bac4a
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-10-27 14:38:54 +0100

    Add CreateHelloWorldApp and options datatype
    This was created off the back of needing a helloworld application with an ingress rule. This function lets you specify some arguments and create that app in a namespace of your choice.

--- tests/pkg/helpers/main.go
+++ tests/pkg/helpers/main.go
@@ -7,7 +7,51 @@ import (
 	"text/template"
 
 	"github.com/go-resty/resty/v2"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	. "github.com/onsi/ginkgo"
 )
+
+// HellowoldOpt type allows you to specify options for
+// deploying a helloworld app template in a cluster.
+type HelloworldOpt struct {
+	Class         string `default:"nginx"`
+	Identifier    string `default:"green-blue"`
+	EnableModSec  string `default:"false"`
+	Weight        string `default:"100"`
+	ModSecSnippet string
+	Hostname      string `example:"hostname.cloud-platform...."`
+}
+
+// CreateHelloWorldApp takes a HelloworldOpt type and KubectlOptions arguments
+// to create a HelloWorld app in the environment of your choice.
+func CreateHelloWorldApp(app *HelloworldOpt, opt *k8s.KubectlOptions) error {
+	if app.Hostname == "" {
+		return fmt.Errorf("helloworld app hostname must not be empty")
+	}
+
+	templateVars := map[string]interface{}{
+		"ingress_annotations": map[string]string{
+			"kubernetes.io/ingress.class":                     app.Class,
+			"external-dns.alpha.kubernetes.io/aws-weight":     app.Weight,
+			"external-dns.alpha.kubernetes.io/set-identifier": app.Identifier,
+			"nginx.ingress.kubernetes.io/enable-modsecurity":  app.EnableModSec,
+			"nginx.ingress.kubernetes.io/modsecurity-snippet": "|\n     SecRuleEngine On",
+		},
+		"host": app.Hostname,
+	}
+
+	tpl, err := TemplateFile("./fixtures/helloworld-deployment.yaml.tmpl", "outputTemplateDeployment.yaml.tmpl", templateVars)
+	if err != nil {
+		return fmt.Errorf("failed to create the helloworld template: %s", err)
+	}
+
+	err = k8s.KubectlApplyFromStringE(GinkgoT(), opt, tpl)
+	if err != nil {
+		return fmt.Errorf("failed to apply the helloworld template: %s", err)
+	}
+
+	return nil
+}
 
 // HttpStatusCode return the HTTP code for an endpoint
 func HttpStatusCode(u string) (int, error) {
