a40f08c795409ade55b0fe7fbaaeb65eccbbc6f9
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-10-27 19:24:28 +0100

    Add certificate creation to cer-manager test

--- tests/e2e/certmanager_test.go
+++ tests/e2e/certmanager_test.go
@@ -2,29 +2,45 @@ package integration_tests
 
 import (
 	"fmt"
+	"html/template"
 	"math/rand"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/ministryofjustice/cloud-platform-infrastructure/tests/pkg/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("cert-manager", func() {
 	var (
-		namespace    = fmt.Sprintf("cert-manager-test-%v", rand.Int())
-		ingressClass = "nginx"
-		ingressName  = "integration-test"
-		options      = k8s.NewKubectlOptions("", "", namespace)
+		namespace = fmt.Sprintf("cert-manager-test-%v", rand.Int())
+		options   = k8s.NewKubectlOptions("", "", namespace)
+		host      = fmt.Sprintf("https://%s.%v", namespace, c.ClusterName)
 	)
 
 	BeforeEach(func() {
-		// create namespace
 		k8s.CreateNamespace(GinkgoT(), options, namespace)
-		// create ingress resource
-		// create certificate
+
+		app := helpers.HelloworldOpt{
+			Hostname: host,
+		}
+
+		err := helpers.CreateHelloWorldApp(&app, options)
+		Expect(err).NotTo(HaveOccurred())
+
+		tpl, err := helpers.TemplateFile("./fixtures/certificate.yaml.tmpl", "cert.yaml.tmpl", template.FuncMap{
+			"cert-name":   namespace,
+			"namespace":   namespace,
+			"hostname":    host,
+			"environment": "staging",
+		})
+
+		err = k8s.KubectlApplyFromStringE(GinkgoT(), options, tpl)
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	AfterEach(func() {
+		defer k8s.DeleteNamespace(GinkgoT(), options, namespace)
 		// destroy ingress resource
 		// destroy namespace
 	})
