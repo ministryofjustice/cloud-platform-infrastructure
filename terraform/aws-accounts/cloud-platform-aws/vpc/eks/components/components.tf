5209d9d29e474f3460ab08b1ff3224bb09535fcf
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-11-01 15:01:52 +0000

    Update live wworkspace variable to get from workspace

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -147,7 +147,7 @@ module "opa" {
 module "starter_pack" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.4"
 
-  enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
+  enable_starter_pack = terraform.workspace == terraform.workspace == "live" ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
 }
 
