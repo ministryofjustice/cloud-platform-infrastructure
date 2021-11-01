03417d7f87df0d5b66ba82c092266896faba3f7d
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-11-01 14:44:30 +0000

    Add starter pack module to EKS test cluster

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -144,6 +144,13 @@ module "opa" {
   cluster_color                  = terraform.workspace == "live" ? "green" : "black"
 }
 
+module "starter_pack" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.4"
+
+  enable_starter_pack = terraform.workspace == local.live_workspace ? false : true
+  cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+}
+
 module "velero" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=0.0.8"
 
