5dddd7ecba838aedb262af2502a369ecca15ad7e
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-11-01 15:14:57 +0000

    Enable starter pack with lookup for prod_namespace

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -147,7 +147,7 @@ module "opa" {
 module "starter_pack" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.4"
 
-  enable_starter_pack = terraform.workspace == terraform.workspace == "live" ? false : true
+  enable_starter_pack = lookup(local.prod_workspace, terraform.workspace, false) ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
 }
 
