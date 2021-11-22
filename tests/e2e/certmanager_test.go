752dfd2e4967702150daa635eebe9f32f2fc40d6
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-11-22 12:11:41 +0000

    refactor(infrastructure testing): :construction: refactor certificate check to utilise before and after specs
    Ginkgo doesn't provide an option for before a context or describe, only a before "it" or before suite. I believe this feature is coming in GinkGo 2.0, but until then we have to structure namespace creation "before each" in a nested format. This will create a number of namespaces for each set of behaviours, such as "this is expected to pass" and "this is expected to fail"

--- tests/e2e/certmanager_test.go
+++ tests/e2e/certmanager_test.go
@@ -15,77 +15,91 @@ import (
 )
 
 var _ = Describe("cert-manager", func() {
-	var (
-		namespace = fmt.Sprintf("cert-test-%v", strings.ToLower(random.UniqueId()))
-		options   = k8s.NewKubectlOptions("", "", namespace)
-		domain    = "integrationtest.service.justice.gov.uk"
-		host      = fmt.Sprintf("%s.%s", namespace, domain)
+	const (
+		domain = "integrationtest.service.justice.gov.uk" // All clusters have access to the test domain name
 	)
 
-	BeforeEach(func() {
-		k8s.CreateNamespace(GinkgoT(), options, namespace)
+	Context("when the namespace has a certificate resource", func() {
+		var (
+			namespace = fmt.Sprintf("cert-smoketest-%v", strings.ToLower(random.UniqueId()))
+			options   = k8s.NewKubectlOptions("", "", namespace)
+			host      = fmt.Sprintf("%s.%s", namespace, domain)
 
-		// Create the application and ingress rule
-		app := helpers.HelloworldOpt{
-			Hostname:   host,
-			Class:      "nginx",
-			Identifier: namespace + "-integration-test-" + "green",
-			Namespace:  namespace,
-			Weight:     "\"100\"",
-		}
+			err  error
+			cert []string
+			// resp *http.Response
+			conn *tls.Conn
+		)
 
-		err := helpers.CreateHelloWorldApp(&app, options)
-		Expect(err).NotTo(HaveOccurred())
+		BeforeEach(func() {
+			k8s.CreateNamespace(GinkgoT(), options, namespace)
+			app := helpers.HelloworldOpt{
+				Hostname:   host,
+				Class:      "nginx",
+				Identifier: "integration-test-app-ing-" + namespace + "-green",
+				Namespace:  namespace,
+				Weight:     "\"100\"",
+			}
 
-		// Create the certificate resource
-		tpl, err := helpers.TemplateFile("./fixtures/certificate.yaml.tmpl", "certificate.yaml.tmpl", template.FuncMap{
-			"certname":    namespace,
-			"namespace":   namespace,
-			"hostname":    host,
-			"environment": "staging",
-		})
-		Expect(err).NotTo(HaveOccurred())
-
-		err = k8s.KubectlApplyFromStringE(GinkgoT(), options, tpl)
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	AfterEach(func() {
-		defer k8s.DeleteNamespace(GinkgoT(), options, namespace)
-	})
-
-	Context("when a certificate resource is created", func() {
-		FIt("should return the correct certificate name", func() {
-			// time.Sleep(160 * time.Second) // Wait for the certificate to be ready
-			waitForCertificate(options, namespace, 120)
-
-			conn, err := tls.Dial("tcp", host+":443", &tls.Config{InsecureSkipVerify: true})
+			err = helpers.CreateHelloWorldApp(&app, options)
 			Expect(err).NotTo(HaveOccurred())
 
-			defer conn.Close()
-
-			cert := conn.ConnectionState().PeerCertificates[0].Issuer.Organization
-
-			Expect(cert[0]).To(Equal("(STAGING) Let's Encrypt"))
+			err = createCertificate(namespace, host, options)
+			Expect(err).NotTo(HaveOccurred())
 		})
 
-		// gomega validation
+		AfterEach(func() {
+			k8s.DeleteNamespace(GinkgoT(), options, namespace)
+		})
+
+		FIt("should succeed and present a staging certificate", func() {
+			conn, err = tls.Dial("tcp", host+":443", &tls.Config{InsecureSkipVerify: true})
+			Expect(err).NotTo(HaveOccurred())
+			cert = conn.ConnectionState().PeerCertificates[0].Issuer.Organization
+			// resp, _ = http.Get("https://" + host)
+
+			// defer resp.Body.Close()
+			// defer conn.Close()
+
+			Expect(cert[0]).To(Equal("(STAGING) Let's Encrypt"))
+			// Expect(resp.StatusCode).To(Equal(200))
+		})
 	})
 })
 
-func waitForCertificate(options *k8s.KubectlOptions, namespace string, seconds int) string {
-	var status string
-	for i := 0; i < 12; i++ {
-		status, err := k8s.RunKubectlAndGetOutputE(GinkgoT(), options, "get", "certificate", namespace, "-o", "jsonpath='{.items[*].status.conditions[?(@.type==\"Ready\")].status}'")
-		Expect(err).NotTo(HaveOccurred())
-
-		fmt.Println(status)
-		if status == "True" {
-			break
-		}
-		time.Sleep(10 * time.Second)
-		fmt.Println("Waiting for certificate to be ready")
+func createCertificate(namespace, host string, options *k8s.KubectlOptions) error {
+	tpl, err := helpers.TemplateFile("./fixtures/certificate.yaml.tmpl", "certificate.yaml.tmpl", template.FuncMap{
+		"certname":    namespace,
+		"namespace":   namespace,
+		"hostname":    host,
+		"environment": "staging",
+	})
+	if err != nil {
+		return err
 	}
 
-	return status
+	err = k8s.KubectlApplyFromStringE(GinkgoT(), options, tpl)
+	if err != nil {
+		return err
+	}
+
+	// err = waitForCertificateToBeReady(namespace, options)
+	// if err != nil {
+	// 	return err
+	// }
+	time.Sleep(160 * time.Second) // Wait for the certificate to be ready
+
+	return nil
+}
+
+func waitForCertificateToBeReady(namespace string, options *k8s.KubectlOptions) error {
+	// Wait 160 seconds for the certificate to be ready
+	for i := 0; i < 160; i++ {
+		err := k8s.RunKubectlE(GinkgoT(), options, "get", "certificate", namespace, "-o", "jsonpath={.status.conditions[?(@.type=='Ready')].status}")
+		if err == nil {
+			return nil
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return nil
 }
