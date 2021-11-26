dccdcbcdf25e8d6046f905a3bc7b2791fd247506
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-11-26 15:01:06 +0000

    Update cluster_name var using terraform.workspace

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/main.tf
@@ -202,7 +202,7 @@ resource "null_resource" "set_prefix_delegation_target" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      aws eks --region eu-west-2 update-kubeconfig --name ${var.cluster_name}
+      aws eks --region eu-west-2 update-kubeconfig --name ${terraform.workspace}
       kubectl set env daemonset aws-node -n kube-system ENABLE_PREFIX_DELEGATION=true
       kubectl set env daemonset aws-node -n kube-system WARM_PREFIX_TARGET=1
     EOT
