cb377ea93d3fa91dcefc5e11761cdf834d94912d
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-12-01 19:56:46 +0000

    Fix the helm provider block in eks components

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/main.tf
@@ -33,10 +33,11 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    command     = "aws"
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
   }
 }
 
