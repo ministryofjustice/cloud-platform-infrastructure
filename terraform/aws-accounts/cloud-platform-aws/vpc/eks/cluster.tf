c8c7cc072a5028a48df97dcffe1a0094898a9e34
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-12-01 17:53:52 +0000

    Upgraded provider v2.0.2 for kubernetes

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -6,9 +6,6 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
