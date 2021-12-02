fff1af6cdd3893c43aff23e479682b93d4491033
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-12-02 13:14:06 +0000

    Update the version syntax for kubernetes provider

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/versions.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/versions.tf
@@ -9,7 +9,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.6.1"
+      version = "~> 2.6.1"
     }
     null = {
       source = "hashicorp/null"
