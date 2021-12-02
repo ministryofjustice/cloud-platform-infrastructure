bd3ab7867d70f5724e19d7e1b053b364e44d4fee
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2021-12-02 18:16:50 +0000

    Upgrade Kubernetes Provider (#1433)
    * Upgrade Kubernetes provider to 2.6.1
    
    This is to use the updated version during 1.20 upgrade

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -6,15 +6,15 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+    command     = "aws"
+  }
 }
 
 locals {
--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/main.tf
@@ -21,15 +21,23 @@ provider "aws" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+    command     = "aws"
+  }
 }
+
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
   }
 }
 
