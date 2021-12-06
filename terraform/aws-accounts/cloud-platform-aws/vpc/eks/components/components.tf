492f346addeedebf1fe05cae8c9717cf03737242
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-12-06 08:33:18 +0000

    upgrade ingress

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -66,7 +66,7 @@ module "external_dns" {
 }
 
 module "ingress_controllers" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=0.3.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=one"
 
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   is_live_cluster     = lookup(local.prod_workspace, terraform.workspace, false)
