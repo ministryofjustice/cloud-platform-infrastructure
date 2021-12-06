6b8f36415b9e0ab130d4d22364d40418d2a93b75
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-12-06 16:05:25 +0000

    Add cluster addons vpc-cni, coredns and kube-proxy to all EKS clusters

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -187,14 +187,10 @@ module "eks" {
 # EKS Cluster add-ons #
 #######################
 module "aws_eks_addons" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-eks-add-ons?ref=1.0.3"
-
+  source = "github.com/ministryofjustice/cloud-platform-terraform-eks-add-ons?ref=1.0.4"
   depends_on              = [module.eks]
   cluster_name            = terraform.workspace
   eks_cluster_id          = module.eks.cluster_id
-  addon_create_vpc_cni    = terraform.workspace == "manager" ? true : false
-  addon_create_kube_proxy = terraform.workspace == "manager" ? true : false
-  addon_create_coredns    = terraform.workspace == "manager" ? true : false
   cluster_oidc_issuer_url = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
   addon_tags              = local.tags
 }
