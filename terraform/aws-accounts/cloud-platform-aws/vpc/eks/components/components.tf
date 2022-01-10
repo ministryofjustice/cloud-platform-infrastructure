5409b290d9cbe8502fe44141c7bf9b98bbddd43a
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-01-10 15:47:32 +0000

    Cheaper Prometheus storage for test clusters.

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -110,7 +110,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.0.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.0.2"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
