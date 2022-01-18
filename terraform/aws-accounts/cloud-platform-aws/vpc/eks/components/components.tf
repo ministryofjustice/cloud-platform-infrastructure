a4290c4d41701b6eda5e401aa5eef39b496a26e4
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-01-18 14:43:42 +0000

    Upgrade CloudWatch Exporter (#1458)

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -110,7 +110,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.0.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.0.4"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
