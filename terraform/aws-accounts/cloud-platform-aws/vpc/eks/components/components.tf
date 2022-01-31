e002b032243312540bd40152761f5c7bc7915ecb
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-01-31 13:48:22 +0000

    Upgrade Velero (#1476)
    * Upgrade Velero

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -153,7 +153,7 @@ module "starter_pack" {
 }
 
 module "velero" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=0.0.8"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=1.7.1"
 
   iam_role_nodes        = data.aws_iam_role.nodes.arn
   dependence_prometheus = module.monitoring.helm_prometheus_operator_eks_status
