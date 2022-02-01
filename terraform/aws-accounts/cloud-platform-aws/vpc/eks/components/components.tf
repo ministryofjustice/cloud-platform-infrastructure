9812c815f3c453dd901b55feda2eacee1a135b7c
PaulWyborn <paul.wyborn@digital.justice.gov.uk>
2022-02-01 13:09:45 +0000

    Update metric server with bitnami chart

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -110,7 +110,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.0.6"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.0.7"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
   iam_role_nodes                             = data.aws_iam_role.nodes.arn
