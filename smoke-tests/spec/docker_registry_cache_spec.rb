7cd1948b4c17025904f112b68ae021baa2ce34ff
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2022-03-17 09:35:08 +0000

    Ignore docker registry rspec test in live-1 (#1505)
    Test is failing, as we are going to delete live-1,
    skipping the test, as we are not using this in "live"

--- smoke-tests/spec/docker_registry_cache_spec.rb
+++ smoke-tests/spec/docker_registry_cache_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "docker-registry-cache", kops: true do
+xdescribe "docker-registry-cache", kops: true do
   let(:namespace) { "docker-registry-cache" }
 
   context "pod" do
