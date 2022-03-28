bfca1edd649f050e1dee642ab942e3dff67bdd02
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-03-28 16:42:20 +0100

    Update kuberos module
    This is to remove aws_redirect  as we are using SSO

--- terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/kops/components/components.tf
@@ -33,14 +33,13 @@ module "kiam" {
 }
 
 module "kuberos" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.4.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.4.1"
 
   cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   oidc_kubernetes_client_id     = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_id
   oidc_kubernetes_client_secret = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_secret
   oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
   cluster_address               = "https://api.${data.terraform_remote_state.cluster.outputs.cluster_domain_name}"
-  create_aws_redirect           = false
 }
 
 
