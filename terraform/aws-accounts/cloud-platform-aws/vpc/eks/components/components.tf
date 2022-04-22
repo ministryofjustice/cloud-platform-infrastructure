14f86477cb473341cf4c82aefa7b773785b576e9
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-04-22 12:57:06 +0300

    upgrade OPA to fix cert-manager API change (#1517)
    * upgrade OPA

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -168,8 +168,8 @@ module "monitoring" {
 }
 
 module "opa" {
-  source     = "github.com/ministryofjustice/cloud-platform-terraform-opa?ref=0.3.0"
-  depends_on = [module.monitoring, module.ingress_controllers, module.velero, module.cert_manager]
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opa?ref=0.4.0"
+  depends_on = [module.monitoring, module.ingress_controllers, module.cert_manager]
 
   cluster_domain_name            = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   enable_invalid_hostname_policy = lookup(local.prod_workspace, terraform.workspace, false) ? false : true
