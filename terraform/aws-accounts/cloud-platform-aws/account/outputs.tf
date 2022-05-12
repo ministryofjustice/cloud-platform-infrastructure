f24b7b4dd39c58d0d39d171e92cd0e08164b4180
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2022-05-12 09:56:38 +0100

    Live-1 clean up delete kops state s3 (#1536)
    This is related to:
    https://github.com/ministryofjustice/cloud-platform/issues/3685
    
    Remove pinned version for aws provider, to fix the provider requirement error

--- terraform/aws-accounts/cloud-platform-aws/account/outputs.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/outputs.tf
@@ -7,7 +7,3 @@ output "cp_zone_id" {
   value       = aws_route53_zone.cloud_platform_justice_gov_uk.zone_id
   description = "This output is very important, it is widely used from kops/eks clusters"
 }
-
-output "cloud_platform_kops_state" {
-  value = aws_s3_bucket.cloud_platform_kops_state.bucket
-}
