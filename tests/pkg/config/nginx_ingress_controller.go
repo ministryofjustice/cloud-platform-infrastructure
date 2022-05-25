af2b6e5f8613e8eed9af20bee0fd35e85a370450
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-05-25 17:36:37 +0100

    refactor(tests): delete the `tests` directory which is managed elsewhere
    The tests are actually located in the root directory `test` now.

--- tests/pkg/config/nginx_ingress_controller.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// NginxIngressController holds the config for nginx ingress controller component
-type NginxIngressController struct {
-	NamespacePrefix string `yaml:"namespacePrefix"`
-}
-type ModsecIngressController struct {
-	NamespacePrefix string `yaml:"namespacePrefix"`
-}
