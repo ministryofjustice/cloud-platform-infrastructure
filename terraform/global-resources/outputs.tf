4a7f8cbbb0d7db02bddd3b230034c86822c115d3
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-05-31 16:00:00 +0100

    SAML moved to module

--- terraform/global-resources/outputs.tf
+++ terraform/global-resources/outputs.tf
@@ -2,7 +2,4 @@ output "k8s_oidc_group_claim_domain" {
   value = auth0_rule_config.k8s-oidc-group-claim-domain.value
 }
 
-output "saml_login_page" {
-  value = "https://${local.auth0_tenant_domain}/samlp/${auth0_client.saml.client_id}"
-}
 
