fecc32e9b5941f119eca47bbc31e0b56f9169bf8
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2022-06-07 16:46:53 +0100

    Update auth0 provider to auth0/auth0 (#1562)
    In global resources

--- terraform/global-resources/versions.tf
+++ terraform/global-resources/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     auth0 = {
-      source  = "alexkappa/auth0"
-      version = ">= 0.2.1"
+      source  = "auth0/auth0"
+      version = "~> 0.30.0"
     }
     aws = {
       source  = "hashicorp/aws"
