78fe8789ad1bd1b26c890418c1521a019d322cb2
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2022-06-16 12:18:28 +0100

    refactor(ingress): Updating Modsec test

--- test/nginx_modsec_ingress_ctl_test.go
+++ test/nginx_modsec_ingress_ctl_test.go
@@ -15,13 +15,15 @@ import (
 	"github.com/ministryofjustice/cloud-platform-infrastructure/test/helpers"
 )
 
-var _ = Describe("Nginx Ingress", func() {
+var _ = Describe("Modsec Ingress", func() {
 	var (
 		currentCluster = c.ClusterName
 		namespaceName  = fmt.Sprintf("smoketest-modsec-v1-%s", strings.ToLower(random.UniqueId()))
-		host           = fmt.Sprintf("%s-nginx.apps.%s.%s", namespaceName, currentCluster, domain)
+		host           = fmt.Sprintf("%s.apps.%s.%s", namespaceName, currentCluster, domain)
 		options        = k8s.NewKubectlOptions("", "", namespaceName)
 		url            = fmt.Sprintf("https://%s", host)
+		good_url       = fmt.Sprintf("https://%s", host)
+		bad_url        = fmt.Sprintf("%s?exec=/bin/bash", url)
 		class          = "modsec"
 		tpl            string
 	)
@@ -39,8 +41,8 @@ var _ = Describe("Nginx Ingress", func() {
 		defer k8s.DeleteNamespace(GinkgoT(), options, namespaceName)
 	})
 
-	Context("when ingress resource is deployed using 'nginx' ingress controller", func() {
-		It("should expose the service to the internet", func() {
+	Context("when ingress resource is deployed using 'modsec' ingress controller and modsec enabled", func() {
+		It("should block the request if the url is malicious", func() {
 			var err error
 
 			setIdentifier := "integration-test-app-ing-" + namespaceName + "-green"
@@ -49,6 +51,8 @@ var _ = Describe("Nginx Ingress", func() {
 				"ingress_annotations": map[string]string{
 					"external-dns.alpha.kubernetes.io/aws-weight":     "\"100\"",
 					"external-dns.alpha.kubernetes.io/set-identifier": setIdentifier,
+					"nginx.ingress.kubernetes.io/enable-modsecurity":  "\"true\"",
+					"nginx.ingress.kubernetes.io/modsecurity-snippet": "|\n     SecRuleEngine On",
 				},
 				"host":      host,
 				"class":     class,
@@ -63,7 +67,7 @@ var _ = Describe("Nginx Ingress", func() {
 			k8s.KubectlApplyFromString(GinkgoT(), options, tpl)
 			k8s.WaitUntilIngressAvailableV1Beta1(GinkgoT(), options, "integration-test-app-ing", 60, 5*time.Second)
 
-			retry.DoWithRetry(GinkgoT(), fmt.Sprintf("evaluating http code for %s", host), 20, 10*time.Second, func() (string, error) {
+			retry.DoWithRetry(GinkgoT(), fmt.Sprintf("evaluating http code for %s", host), 2, 120*time.Second, func() (string, error) {
 				s, err := helpers.HttpStatusCode(url)
 				if err != nil {
 					log.Fatalf("execution: %s", err)
@@ -75,10 +79,14 @@ var _ = Describe("Nginx Ingress", func() {
 				return "", nil
 			})
 
-			// Sleep for 60 seconds
-			time.Sleep(60 * time.Second)
+			// This is not ideal, we need to find the way how improve this.
+			time.Sleep(120 * time.Second)
 
-			Expect(helpers.HttpStatusCode(url)).To(Equal(200))
+			Expect(helpers.HttpStatusCode(bad_url)).To(Equal(403))
+
+			By("having an benign url, request succeeds")
+
+			Expect(helpers.HttpStatusCode(good_url)).To(Equal(200))
 		})
 	})
 })
