a67109603efd92730d3578a1abbbb0fb0f0b12c5
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2022-07-06 16:13:12 +0100

    Update flow-log module and remove sonarqube (#1601)
    - Create new resource aws_s3_bucket_acl for flow-log module
     - Remove sonarqube from manger cluster, as it is not used any more

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -189,22 +189,3 @@ module "velero" {
 module "kuberhealthy" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-kuberhealthy?ref=v1.0.0"
 }
-
-module "sonarqube" {
-  count  = terraform.workspace == "manager" ? 1 : 0
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sonarqube?ref=0.0.5"
-
-  vpc_id                        = data.terraform_remote_state.cluster.outputs.vpc_id
-  internal_subnets              = data.terraform_remote_state.cluster.outputs.internal_subnets
-  internal_subnets_ids          = data.terraform_remote_state.cluster.outputs.internal_subnets_ids
-  cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  oidc_components_client_id     = data.terraform_remote_state.cluster.outputs.oidc_components_client_id
-  oidc_components_client_secret = data.terraform_remote_state.cluster.outputs.oidc_components_client_secret
-  oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
-
-  # This is to enable sonarqube, by default it is false for test clusters
-  enable_sonarqube = lookup(local.prod_workspace, terraform.workspace, false)
-
-  depends_on = [module.ingress_controllers]
-}
-
