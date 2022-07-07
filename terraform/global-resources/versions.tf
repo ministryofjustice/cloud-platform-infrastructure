e5687c2f0afc708d303777611c0d767126465e2c
jakemulley <jakemulley@users.noreply.github.com>
2022-07-07 13:50:20 +0100

    Utilise exact operator (=) to ensure no upgrading without using -upgrade

--- terraform/global-resources/versions.tf
+++ terraform/global-resources/versions.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     auth0 = {
       source  = "auth0/auth0"
-      version = "~> 0.30.0"
+      version = "=0.30.3"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.20.1"
+      version = "=4.20.1"
     }
     external = {
       source  = "hashicorp/external"
-      version = "~> 2.2.2"
+      version = "=2.2.2"
     }
     http = {
       source  = "hashicorp/http"
-      version = "~> 2.2.0"
+      version = "=2.2.0"
     }
   }
   required_version = ">= 0.14"
