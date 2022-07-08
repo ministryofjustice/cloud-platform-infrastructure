3a75638b4f769d7562ad34e6c373c2429cf91f4e
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-07-08 18:04:32 +0100

    test(daemonset): use gomega assertion and add context desc

--- test/daemonsets_test.go
+++ test/daemonsets_test.go
@@ -7,41 +7,42 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Daemonsets checks", func() {
+var _ = Describe("Daemonsets", func() {
 	var notFoundDaemonSets []string
 
-	FIt("should exist the following daemonsets", func() {
-		c.ExpectedDaemonSets()
+	Context("expected daemonsets", func() {
+		It("should exist the in the cluster", func() {
+			c.ExpectedDaemonSets()
 
-		if len(c.DaemonSets) == 0 {
-			Skip("No daemonsets defined, skipping test")
-		}
+			if len(c.DaemonSets) == 0 {
+				Skip("No daemonsets defined, skipping test")
+			}
 
-		options := k8s.NewKubectlOptions("", "", "")
+			options := k8s.NewKubectlOptions("", "", "")
 
-		list, err := k8s.ListDaemonSetsE(GinkgoT(), options, metav1.ListOptions{})
-		if err != nil {
-			Fail(fmt.Sprintf("Failed to list daemonsets: %s", err))
-		}
+			list, err := k8s.ListDaemonSetsE(GinkgoT(), options, metav1.ListOptions{})
+			if err != nil {
+				Fail(fmt.Sprintf("Failed to list daemonsets: %s", err))
+			}
 
-		for _, daemonSet := range c.DaemonSets {
-			var found bool
-			for _, d := range list {
-				if d.Name == daemonSet {
-					found = true
-					continue
+			for _, daemonSet := range c.DaemonSets {
+				var found bool
+				for _, d := range list {
+					if d.Name == daemonSet {
+						found = true
+						continue
+					}
+
 				}
-
+				if !found {
+					notFoundDaemonSets = append(notFoundDaemonSets, daemonSet)
+				}
 			}
-			if !found {
-				notFoundDaemonSets = append(notFoundDaemonSets, daemonSet)
-			}
-		}
 
-		if notFoundDaemonSets != nil {
-			Fail(fmt.Sprintf("The following daemonsets DO NOT exist: %v", notFoundDaemonSets))
-		}
+			Expect(notFoundDaemonSets).To(BeEmpty())
+		})
 	})
 })
