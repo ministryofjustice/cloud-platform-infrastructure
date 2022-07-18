c0d71a6afbe1ec075eae6ac30d1dfc15e6378def
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-07-18 15:14:28 +0100

    fix(servicemonitor): remove non-existing servicemonitors and fix get request

--- test/servicemonitors_test.go
+++ test/servicemonitors_test.go
@@ -10,25 +10,21 @@ import (
 )
 
 var _ = Describe("ServiceMonitors", func() {
-	Context("when checking if the expected service monitors exist", func() {
-		It("should return true", func() {
-			c.ExpectedServiceMonitors()
+	It("should exist in the cluster", func() {
+		c.ExpectedServiceMonitors()
 
-			var notFound []string
+		var nonExistingServiceMonitors []string
 
-			if len(c.ServiceMonitors) == 0 {
-				Skip("No servicemonitors defined, skipping test")
-			}
-
-			for namespace, serviceMonitors := range c.ServiceMonitors {
-				for _, serviceMonitor := range serviceMonitors {
-					_, err := testHelpers.GetPrometheusClientSetE(GinkgoT(), k8s.NewKubectlOptions("", "", namespace))
-					if err != nil {
-						notFound = append(notFound, fmt.Sprintf("%s/%s", namespace, serviceMonitor))
-					}
+		By("Checking that the ServiceMonitors are running")
+		for namespace, serviceMonitors := range c.ServiceMonitors {
+			for _, serviceMonitor := range serviceMonitors {
+				_, err := testHelpers.GetServiceMonitorSetE(GinkgoT(), k8s.NewKubectlOptions("", "", namespace), serviceMonitor)
+				if err != nil {
+					nonExistingServiceMonitors = append(nonExistingServiceMonitors, fmt.Sprintf("%s/%s", namespace, serviceMonitor))
 				}
 			}
-			Expect(notFound).To(BeEmpty())
-		})
+		}
+
+		Expect(nonExistingServiceMonitors).To(BeEmpty())
 	})
 })
