1ae4ca978f5cdf9c3fbd67a671c3e7bf04248cb8
PaulWyborn <paul.wyborn@digital.justice.gov.uk>
2022-07-18 13:44:05 +0100

    pin-Helm-provider

--- terraform/cross-account-IAM/versions.tf
+++ terraform/cross-account-IAM/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "=2.6.0"
+      version = "~> 2.6.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
