f5e8bd7ca1f819cfe8b5883bedd9251c8606a486
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-07-18 16:10:22 +0100

    test(monitoring): add monitoring and crd expectations to "config"

---
+++ test/crds_test.go
@@ -0,0 +1,27 @@
+package integration_tests
+
+import (
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Custom resource definitions", func() {
+	options := k8s.NewKubectlOptions("", "", "")
+
+	// Get all expected CRDs from config
+	c.ExpectedCrds()
+
+	// Get all custom resource definitions
+	crds, err := k8s.RunKubectlAndGetOutputE(GinkgoT(), options, "get", "crd")
+	if err != nil {
+		Fail(err.Error())
+	}
+
+	FIt("should return all expected CRDs", func() {
+		for _, expectedCrd := range c.CustomResourceDefinitions {
+			Expect(crds).To(ContainSubstring(expectedCrd))
+		}
+	})
+
+})
---
+++ test/monitoring_test.go
@@ -0,0 +1,38 @@
+package integration_tests
+
+import (
+	"context"
+
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	testHelpers "github.com/ministryofjustice/cloud-platform-infrastructure/test/helpers"
+)
+
+var _ = Describe("Monitoring", func() {
+	namespace := "monitoring"
+	options := k8s.NewKubectlOptions("", "", namespace)
+	client, err := testHelpers.GetPrometheusClientSetE(GinkgoT(), options)
+	if err != nil {
+		Fail(err.Error())
+	}
+	c.ExpectedPromRules()
+
+	// Get all custome resource definitions
+	crds, err := client.PrometheusRules(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		Fail(err.Error())
+	}
+
+	// Spit list of prometheus rules into a slice containing the name only
+	var ruleNames []string
+	for _, crd := range crds.Items {
+		ruleNames = append(ruleNames, crd.GetName())
+	}
+
+	It("should have the expected custom resources", func() {
+		Expect(ruleNames).To(ConsistOf(c.PrometheusRules))
+	})
+})
