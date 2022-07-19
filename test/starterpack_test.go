e92c38e9d7dd464524c0275118c46b5d6818db8d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-07-19 15:39:12 +0100

    test(starterpack): replace rspec with ginkgo test

---
+++ test/starterpack_test.go
@@ -0,0 +1,21 @@
+package integration_tests
+
+import (
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Starterpack", func() {
+	host := "helloworld-app-starter-pack-0.apps." + c.ClusterName + ".cloud-platform.service.justice.gov.uk"
+	It("should respond OK to a GET request", func() {
+		resp, err := http.Get("https://" + host)
+		if err != nil {
+			Fail(err.Error())
+		}
+		defer resp.Body.Close()
+
+		Expect(resp.StatusCode).To(Equal(200))
+	})
+})
