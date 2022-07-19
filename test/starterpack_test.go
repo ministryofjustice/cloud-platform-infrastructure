ab711d25ce9af41fcfa4c9005f3c24b08d57cf51
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-07-19 16:21:26 +0100

    build(starterpack): add condition to skip test if on "manager" cluster

--- test/starterpack_test.go
+++ test/starterpack_test.go
@@ -2,6 +2,7 @@ package integration_tests
 
 import (
 	"net/http"
+	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -10,6 +11,10 @@ import (
 var _ = Describe("Starterpack", func() {
 	host := "helloworld-app-starter-pack-0.apps." + c.ClusterName + ".cloud-platform.service.justice.gov.uk"
 	It("should respond OK to a GET request", func() {
+		if strings.Contains(strings.ToLower(c.ClusterName), "manager") {
+			Skip("The Manager cluster does not have a Starterpack app")
+		}
+
 		resp, err := http.Get("https://" + host)
 		if err != nil {
 			Fail(err.Error())
