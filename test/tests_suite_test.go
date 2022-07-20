fda30214a3a913b152b9f44eabed5eeaaaf88773
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-07-20 14:56:26 +0100

    test(config): add kubernetes.Interface client generation
    This will allow us to perform calls on the API using the client-go package, rather than relying on kubectl and terratest. It makes things like calls for namespace names much easier.

--- test/tests_suite_test.go
+++ test/tests_suite_test.go
@@ -4,13 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/util/homedir"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"github.com/ministryofjustice/cloud-platform-go-library/client"
 	"github.com/ministryofjustice/cloud-platform-infrastructure/test/config"
 )
 
@@ -27,18 +30,29 @@ var c config.Config
 // Create a new instance of the logger. You can have any number of instances.
 var log = logrus.New()
 
-// cluster lets you select which cluster to run the tests on
-var cluster = flag.String("cluster", "", "Set the cluster name")
-
 // TestMain controls pre/post test logic
 func TestMain(m *testing.M) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	var cluster = flag.String("cluster", "", "(optional) set the cluster name")
 	flag.Parse()
+
+	client, err := client.NewKubeClientWithValues(*kubeconfig, "")
+	if err != nil {
+		Fail(fmt.Sprintf("Failed to create kube client: %v", err))
+	}
 
 	c = config.Config{
 		Prefix: "smoketest",
+		Client: *client,
 	}
 
-	err := c.SetClusterName(*cluster)
+	err = c.SetClusterName(*cluster)
 	if err != nil {
 		Fail(fmt.Sprintf("Failed to set cluster name: %s", err))
 	}
