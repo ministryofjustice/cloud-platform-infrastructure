e45c18e51c0239d92078f01210a6fba005b69558
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-07-21 21:06:23 +0100

    test(ingress): merge the two controller contexts together
    It was confusing having two files that both did the same thing but pointing at different controllers. In my opinion this makes much more sense.

--- test/nginx_ingress_test.go
+++ test/nginx_ingress_test.go
@@ -2,6 +2,7 @@ package integration_tests
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 	"time"
 
@@ -9,39 +10,31 @@ import (
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/retry"
 	. "github.com/onsi/gomega"
 
 	"github.com/ministryofjustice/cloud-platform-infrastructure/test/helpers"
 )
 
-var _ = Describe("Nginx Ingress", func() {
+var _ = Describe("ingress-controllers", func() {
 	var (
-		currentCluster = c.ClusterName
-		namespaceName  = fmt.Sprintf("%s-ingress-%s", c.Prefix, strings.ToLower(random.UniqueId()))
-		host           = fmt.Sprintf("%s-nginx.apps.%s.%s", namespaceName, currentCluster, domain)
-		options        = k8s.NewKubectlOptions("", "", namespaceName)
-		url            = fmt.Sprintf("https://%s", host)
-		tpl            string
+		namespaceName, host string
+		options             *k8s.KubectlOptions
 	)
 
 	BeforeEach(func() {
-		By("not having an ingress resource deployed")
-
-		Expect(helpers.HttpStatusCode(url)).To(Equal(404))
+		namespaceName = fmt.Sprintf("%s-ing-%s", c.Prefix, strings.ToLower(random.UniqueId()))
+		host = fmt.Sprintf("%s.apps.%s.%s", namespaceName, c.ClusterName, domain)
+		options = k8s.NewKubectlOptions("", "", namespaceName)
 
 		k8s.CreateNamespace(GinkgoT(), options, namespaceName)
 	})
 
 	AfterEach(func() {
-		defer k8s.KubectlDeleteFromString(GinkgoT(), options, tpl)
 		defer k8s.DeleteNamespace(GinkgoT(), options, namespaceName)
 	})
 
-	Context("when ingress resource is deployed using 'nginx' ingress controller", func() {
+	Context("when an ingress resource is deployed using the 'nginx' ingress controller", func() {
 		It("should expose the service to the internet", func() {
-			var err error
-
 			setIdentifier := "integration-test-app-ing-" + namespaceName + "-green"
 
 			TemplateVars := map[string]interface{}{
@@ -54,30 +47,59 @@ var _ = Describe("Nginx Ingress", func() {
 				"namespace": namespaceName,
 			}
 
-			tpl, err = helpers.TemplateFile("./fixtures/helloworld-deployment.yaml.tmpl", "helloworld-deployment.yaml.tmpl", TemplateVars)
+			tpl, err := helpers.TemplateFile("./fixtures/helloworld-deployment.yaml.tmpl", "helloworld-deployment.yaml.tmpl", TemplateVars)
 			if err != nil {
-				log.Fatalf("execution: %s", err)
+				Fail(err.Error())
 			}
 
 			k8s.KubectlApplyFromString(GinkgoT(), options, tpl)
-			k8s.WaitUntilIngressAvailableV1Beta1(GinkgoT(), options, "integration-test-app-ing", 60, 5*time.Second)
+			k8s.WaitUntilIngressAvailable(GinkgoT(), options, "integration-test-app-ing", 6, 20*time.Second)
 
-			retry.DoWithRetry(GinkgoT(), fmt.Sprintf("evaluating http code for %s", host), 20, 10*time.Second, func() (string, error) {
-				s, err := helpers.HttpStatusCode(url)
+			GinkgoWriter.Printf("Checking that the ingress is available at https://%s\n", host)
+			Eventually(func() int {
+				resp, err := http.Get("https://" + host)
 				if err != nil {
-					log.Fatalf("execution: %s", err)
+					Fail(err.Error())
 				}
+				defer resp.Body.Close()
+				return resp.StatusCode
+			}, "8m", "30s").Should(Equal(200))
 
-				if s != 200 {
-					return "", fmt.Errorf("Expected http return code 200. Got '%v'", s)
+		})
+	})
+
+	Context("when an ingress resource is deployed using 'default' ingress controller", func() {
+		FIt("should expose the service to the internet", func() {
+			setIdentifier := "integration-test-app-ing-" + namespaceName + "-green"
+			class := "default"
+
+			TemplateVars := map[string]interface{}{
+				"ingress_annotations": map[string]string{
+					"external-dns.alpha.kubernetes.io/aws-weight":     "\"100\"",
+					"external-dns.alpha.kubernetes.io/set-identifier": setIdentifier,
+				},
+				"host":      host,
+				"class":     class,
+				"namespace": namespaceName,
+			}
+
+			tpl, err := helpers.TemplateFile("./fixtures/helloworld-deployment-v1.yaml.tmpl", "helloworld-deployment-v1.yaml.tmpl", TemplateVars)
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			k8s.KubectlApplyFromString(GinkgoT(), options, tpl)
+			k8s.WaitUntilIngressAvailable(GinkgoT(), options, "integration-test-app-ing", 8, 20*time.Second)
+
+			GinkgoWriter.Printf("Checking that the ingress is available at https://%s\n", host)
+			Eventually(func() int {
+				resp, err := http.Get("https://" + host)
+				if err != nil {
+					Fail(err.Error())
 				}
-				return "", nil
-			})
-
-			// Sleep for 60 seconds
-			time.Sleep(60 * time.Second)
-
-			Expect(helpers.HttpStatusCode(url)).To(Equal(200))
+				defer resp.Body.Close()
+				return resp.StatusCode
+			}, "8m", "30s").Should(Equal(200))
 		})
 	})
 })
