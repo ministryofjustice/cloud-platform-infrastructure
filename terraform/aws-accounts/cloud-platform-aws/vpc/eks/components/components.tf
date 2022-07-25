e0afe9b9442c9c5257e3049f1867a8f9c7b0c97a
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-07-25 11:51:30 +0100

    Add modsec v1 dependency for starter_pack
    Starter pack module use both default and modsec

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -177,7 +177,10 @@ module "starter_pack" {
   enable_starter_pack = lookup(local.prod_workspace, terraform.workspace, false) ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
 
-  depends_on = [module.ingress_controllers_v1]
+  depends_on = [
+    module.ingress_controllers_v1,
+    module.modsec_ingress_controllers_v1
+  ]
 }
 
 module "velero" {
