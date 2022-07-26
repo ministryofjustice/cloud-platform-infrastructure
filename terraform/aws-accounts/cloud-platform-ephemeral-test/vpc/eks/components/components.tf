ba51a8fe9e2abe9cd3093329b5c996008869e628
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-07-26 15:58:45 +0100

    Update ephemeral to keep upto date
    This is to match with cloud-platform-aws

--- terraform/aws-accounts/cloud-platform-ephemeral-test/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-ephemeral-test/vpc/eks/components/components.tf
@@ -1,6 +1,6 @@
 
 module "cert_manager" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=1.4.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=1.5.0"
 
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
@@ -17,7 +17,7 @@ module "cert_manager" {
 }
 
 module "external_dns" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-external-dns?ref=1.7.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-external-dns?ref=1.9.1"
 
   iam_role_nodes      = data.aws_iam_role.nodes.arn
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
@@ -33,7 +33,7 @@ module "external_dns" {
 }
 
 module "ingress_controllers" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=0.3.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=0.3.5"
 
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   is_live_cluster     = lookup(local.prod_workspace, terraform.workspace, false)
@@ -56,6 +56,39 @@ module "modsec_ingress_controllers" {
   depends_on = [module.ingress_controllers]
 }
 
+module "ingress_controllers_v1" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.0.10"
+
+  replica_count       = "6"
+  controller_name     = "default"
+  enable_latest_tls   = true
+  cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+  is_live_cluster     = lookup(local.prod_workspace, terraform.workspace, false)
+  live1_cert_dns_name = lookup(local.live1_cert_dns_name, terraform.workspace, "")
+
+  # Enable this when we remove the module "ingress_controllers"
+  enable_external_dns_annotation = false
+  depends_on = [
+    module.cert_manager,
+    module.monitoring.prometheus_operator_crds_status
+  ]
+}
+
+module "modsec_ingress_controllers_v1" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.0.10"
+
+  replica_count       = "6"
+  controller_name     = "modsec"
+  cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+  is_live_cluster     = lookup(local.prod_workspace, terraform.workspace, false)
+  live1_cert_dns_name = lookup(local.live1_cert_dns_name, terraform.workspace, "")
+  enable_modsec       = true
+  enable_owasp        = true
+  enable_latest_tls   = true
+
+  depends_on = [module.ingress_controllers_v1]
+}
+
 module "kuberos" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.4.7"
 
@@ -67,13 +100,19 @@ module "kuberos" {
   create_aws_redirect           = false
 }
 
+module "logging" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=1.3.1"
 
+  elasticsearch_host       = lookup(var.elasticsearch_hosts_maps, terraform.workspace, "placeholder-elasticsearch")
+  elasticsearch_audit_host = lookup(var.elasticsearch_audit_hosts_maps, terraform.workspace, "placeholder-elasticsearch")
+  dependence_prometheus    = module.monitoring.prometheus_operator_crds_status
+  enable_curator_cronjob   = terraform.workspace == "live" ? true : false
+}
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.0.8"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.3.2"
 
   alertmanager_slack_receivers               = var.alertmanager_slack_receivers
-  iam_role_nodes                             = data.aws_iam_role.nodes.arn
   pagerduty_config                           = var.pagerduty_config
   cluster_domain_name                        = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   oidc_components_client_id                  = data.terraform_remote_state.cluster.outputs.oidc_components_client_id
@@ -82,20 +121,19 @@ module "monitoring" {
   enable_thanos_sidecar                      = lookup(local.prod_workspace, terraform.workspace, false)
   enable_large_nodesgroup                    = terraform.workspace == "live" ? true : false
   enable_prometheus_affinity_and_tolerations = true
+  enable_kibana_audit_proxy                  = terraform.workspace == "live" ? true : false
+  enable_kibana_proxy                        = terraform.workspace == "live" ? true : false
 
   enable_thanos_helm_chart = lookup(local.prod_workspace, terraform.workspace, false)
   enable_thanos_compact    = lookup(local.manager_workspace, terraform.workspace, false)
 
-  enable_ecr_exporter        = lookup(local.cloudwatch_workspace, terraform.workspace, false)
-  enable_cloudwatch_exporter = lookup(local.cloudwatch_workspace, terraform.workspace, false)
-
-  # This section is for EKS
-  eks                         = true
+  enable_ecr_exporter         = lookup(local.cloudwatch_workspace, terraform.workspace, false)
+  enable_cloudwatch_exporter  = lookup(local.cloudwatch_workspace, terraform.workspace, false)
   eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
 }
 
 module "opa" {
-  source     = "github.com/ministryofjustice/cloud-platform-terraform-opa?ref=0.2.3"
+  source     = "github.com/ministryofjustice/cloud-platform-terraform-opa?ref=0.4.2"
   depends_on = [module.monitoring, module.ingress_controllers, module.velero, module.cert_manager]
 
   cluster_domain_name            = data.terraform_remote_state.cluster.outputs.cluster_domain_name
@@ -106,21 +144,17 @@ module "opa" {
 }
 
 module "starter_pack" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.1.6"
 
   enable_starter_pack = lookup(local.prod_workspace, terraform.workspace, false) ? false : true
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
 }
 
 module "velero" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=1.7.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=1.8.0"
 
-  iam_role_nodes        = data.aws_iam_role.nodes.arn
-  dependence_prometheus = module.monitoring.helm_prometheus_operator_eks_status
+  dependence_prometheus = module.monitoring.prometheus_operator_crds_status
   cluster_domain_name   = data.terraform_remote_state.cluster.outputs.cluster_domain_name
 
-  # This section is for EKS
-  eks                         = true
   eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
 }
-
