e26bb654412c36521ee6f36cea3d7171be90e13f
jakemulley <jakemulley@users.noreply.github.com>
2022-07-29 17:54:47 +0100

    Remove unused root module providers; add missing var types and descriptions

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/variables.tf
@@ -1,19 +1,24 @@
 variable "dockerhub_user" {
-  description = "Cloud platform user (see lastpass). This is required to avoid hitting limits when pulling images."
+  description = "DockerHub user for the Cloud Platform (see LastPass). This is required to avoid hitting limits when pulling images."
+  type        = string
 }
 
 variable "dockerhub_token" {
-  description = "Token for the above"
+  description = "DockerHub token for the Cloud Platform user"
+  type        = string
 }
+
 variable "auth0_issuerUrl" {
-  description = "domain IssuerURL by which Auth0 can find the OpenID Provider Configuration Document"
+  description = "Domain issuer URL by which Auth0 can find the OpenID Provider Configuration Document"
   default     = "https://justice-cloud-platform.eu.auth0.com/"
+  type        = string
 }
 
 # Set when Auth0 account is setup in here: /terraform/global-resources/auth0.tf
 variable "auth0_groupsClaim" {
   description = "OIDC Group Claim domain for justice cloud-platform account"
   default     = "https://k8s.integration.dsd.io/groups"
+  type        = string
 }
 
 variable "cluster_enabled_log_types" {
