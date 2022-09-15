f69d896bd10190f64c7b93710bdc69ab078a0f3c
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-09-15 14:00:40 +0100

    release GP3

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/storage.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/storage.tf
@@ -3,7 +3,7 @@
 ########
 
 module "eks_csi" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-eks-csi?ref=gp3"
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-eks-csi?ref=1.0.0"
   eks_cluster = terraform.workspace
   depends_on = [helm_release.calico]
 }
