2bc095aca8d75e5b88da4c814506ec60f22ab4ab
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-10-20 16:14:36 +0100

    Add ssl-ciphers for v1 ingress controllers
    This is related to:
    https://github.com/ministryofjustice/cloud-platform/issues/4159

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -96,7 +96,7 @@ module "modsec_ingress_controllers" {
 }
 
 module "ingress_controllers_v1" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.0.14"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=add-cipher-tls"
 
   replica_count       = "6"
   controller_name     = "default"
@@ -114,7 +114,7 @@ module "ingress_controllers_v1" {
 }
 
 module "modsec_ingress_controllers_v1" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.0.14"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=add-cipher-tls"
 
   replica_count       = "6"
   controller_name     = "modsec"
