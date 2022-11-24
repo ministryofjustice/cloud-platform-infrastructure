0feb3d9c2f95f39540546cb208d705e6aedd0ba9
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2022-11-24 14:15:47 +0000

    This is to fix the dashboard not showing up (#1946)
    Set labelValue is set to "", as by default it is looking for "1"

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -137,7 +137,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.4.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.4.2"
 
   alertmanager_slack_receivers               = local.enable_alerts ? var.alertmanager_slack_receivers : [{ severity = "dummy", webhook = "https://dummy.slack.com", channel = "#dummy-alarms" }]
   pagerduty_config                           = local.enable_alerts ? var.pagerduty_config : "dummy"
