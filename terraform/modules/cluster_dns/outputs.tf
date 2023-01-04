52d3b9531c332c26a941333ce98b47d897109bab
jakemulley <jakemulley@users.noreply.github.com>
2023-01-04 13:48:06 +0000

    Fix terraform/modules/cluster_dns lint warnings

--- terraform/modules/cluster_dns/outputs.tf
+++ terraform/modules/cluster_dns/outputs.tf
@@ -5,4 +5,3 @@ output "cluster_dns_zone_name" {
 output "cluster_dns_zone_id" {
   value = aws_route53_zone.cluster.zone_id
 }
-
--- terraform/modules/cluster_dns/variables.tf
+++ terraform/modules/cluster_dns/variables.tf
@@ -1,6 +1,8 @@
 variable "cluster_base_domain_name" {
+  type = string
 }
 
 variable "parent_zone_id" {
+  type = string
 }
 
