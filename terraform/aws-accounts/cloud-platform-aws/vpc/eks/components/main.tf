be13c44cffbab447bf9fa4553b39bc4a3bdb1356
jakemulley <jakemulley@users.noreply.github.com>
2023-01-18 17:56:13 +0000

    Add default tags to cloud-platform-aws/vpc/eks/components

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/main.tf
@@ -15,6 +15,16 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      business-unit = "Platforms"
+      application   = "cloud-platform-aws/vpc/eks/components"
+      is-production = "true"
+      owner         = "Cloud Platform: platforms@digital.justice.gov.uk"
+      source-code   = "github.com/ministryofjustice/cloud-platform-infrastructure"
+    }
+  }
 }
 
 provider "kubernetes" {
