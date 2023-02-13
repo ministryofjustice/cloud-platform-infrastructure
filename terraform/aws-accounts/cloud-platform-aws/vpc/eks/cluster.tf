9ddb505bfdee92a08046f47da3045827e8ff2ef9
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2023-02-13 17:59:17 +0000

    Remove old info related to docker
    Also update max_capacity

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -57,7 +57,7 @@ locals {
   dockerhub_credentials = base64encode("${var.cp_dockerhub_user}:${var.cp_dockerhub_token}")
   default_ng_12_22 = {
     desired_capacity     = lookup(local.node_groups_count, terraform.workspace, local.node_groups_count["default"])
-    max_capacity         = 80
+    max_capacity         = 85
     min_capacity         = lookup(local.default_ng_min_count, terraform.workspace, local.default_ng_min_count["default"])
     subnets              = data.aws_subnets.private.ids
     bootstrap_extra_args = "--use-max-pods false"
