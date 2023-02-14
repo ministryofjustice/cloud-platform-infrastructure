23272305b22d71a87661cdc412a7c1e7d8f46130
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2023-02-14 17:00:06 +0000

    Run plan on main

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -6,7 +6,6 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
