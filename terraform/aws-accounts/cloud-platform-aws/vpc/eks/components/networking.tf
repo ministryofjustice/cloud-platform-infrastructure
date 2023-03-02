adbf642256aa9c2dae9ea427013628a175e27a04
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-03-02 16:31:38 +0000

    fix: 🐛 use module

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/networking.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/networking.tf
@@ -7,7 +7,7 @@ locals {
   calico_crds = {
     tigera_apiserver = "https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/charts/tigera-operator/crds/operator.tigera.io_apiservers_crd.yaml"
     tigera_imagesets = "https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/charts/tigera-operator/crds/operator.tigera.io_imagesets_crd.yaml"
-    tigera_installations = "https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/charts/tigera-operator/crds/operator.tigera.io_imagesets_crd.yaml"
+    tigera_installations = "https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/charts/tigera-operator/crds/operator.tigera.io_installations_crd.yaml"
     tigera_statues = "https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/charts/tigera-operator/crds/operator.tigera.io_tigerastatuses_crd.yaml"
     caliconodestatues = "https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/libcalico-go/config/crd/crd.projectcalico.org_caliconodestatuses.yaml"
     bgpconfigurations     = "https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/libcalico-go/config/crd/crd.projectcalico.org_bgpconfigurations.yaml"
@@ -40,124 +40,11 @@ resource "kubectl_manifest" "calico_crds" {
   yaml_body         = each.value["body"]
 }
 
-data "kubectl_file_documents" "calico_global_policies" {
-  content = file("${path.module}/resources/calico-global-policies.yaml")
-}
-
-resource "kubectl_manifest" "calico_global_policies" {
-  count     = length(data.kubectl_file_documents.calico_global_policies.documents)
-  yaml_body = element(data.kubectl_file_documents.calico_global_policies.documents, count.index)
-
-
-  depends_on = [helm_release.tigera_calico]
-}
-
-resource "kubernetes_namespace" "calico_system" {
-  metadata {
-    name = "calico-system"
-
-    labels = {
-      "component" = "calico"
-    }
-
-    annotations = {
-      "cloud-platform.justice.gov.uk/application"                = "tigera-operator-calico"
-      "cloud-platform.justice.gov.uk/business-unit"              = "Platforms"
-      "cloud-platform.justice.gov.uk/owner"                      = "Cloud Platform: platforms@digital.justice.gov.uk"
-      "cloud-platform.justice.gov.uk/source-code"                = "https://github.com/ministryofjustice/cloud-platform-infrastructure"
-      "cloud-platform.justice.gov.uk/can-tolerate-master-taints" = "true"
-      "cloud-platform-out-of-hours-alert"                        = "true"
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [metadata]
-  }
-}
-
-resource "kubernetes_namespace" "calico_apiserver" {
-  metadata {
-    name = "calico-apiserver"
-
-    labels = {
-      "component" = "calico"
-    }
-
-    annotations = {
-      "cloud-platform.justice.gov.uk/application"                = "tigera-operator-calico"
-      "cloud-platform.justice.gov.uk/business-unit"              = "Platforms"
-      "cloud-platform.justice.gov.uk/owner"                      = "Cloud Platform: platforms@digital.justice.gov.uk"
-      "cloud-platform.justice.gov.uk/source-code"                = "https://github.com/ministryofjustice/cloud-platform-infrastructure"
-      "cloud-platform.justice.gov.uk/can-tolerate-master-taints" = "true"
-      "cloud-platform-out-of-hours-alert"                        = "true"
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [metadata]
-  }
-}
-
-resource "kubernetes_namespace" "tigera_operator" {
-  metadata {
-    name = "tigera-operator"
-
-    labels = {
-      "component" = "calico"
-    }
-
-    annotations = {
-      "cloud-platform.justice.gov.uk/application"                = "tigera-operator-calico"
-      "cloud-platform.justice.gov.uk/business-unit"              = "Platforms"
-      "cloud-platform.justice.gov.uk/owner"                      = "Cloud Platform: platforms@digital.justice.gov.uk"
-      "cloud-platform.justice.gov.uk/source-code"                = "https://github.com/ministryofjustice/cloud-platform-infrastructure"
-      "cloud-platform.justice.gov.uk/can-tolerate-master-taints" = "true"
-      "cloud-platform-out-of-hours-alert"                        = "true"
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [metadata]
-  }
-}
-
-# Calico Helm release cannot be deleted because of finalizers and installation issues, this can be removed once the below issue is fixed.
-# https://github.com/projectcalico/calico/issues/6629
-resource "null_resource" "remove_installation" {
-  depends_on = [helm_release.tigera_calico]
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<-EOT
-      kubectl delete installations.operator.tigera.io default
-    EOT
-  }
-
-  triggers = {
-    helm_tigera = helm_release.tigera_calico.status
-  }
-}
-
-
-resource "helm_release" "tigera_calico" {
-  name       = "tigera-calico-release"
-  chart      = "tigera-operator"
-  repository = "https://projectcalico.docs.tigera.io/charts"
-  namespace  = "tigera-operator"
-  timeout    = 300
-  version    = "3.25.0"
-  skip_crds = true
-
+module "tigera_calico" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-tigera-calico?ref=wait-for-apiserver"
 
   depends_on = [
-    kubernetes_namespace.tigera_operator,
-    kubernetes_namespace.calico_system,
-    kubernetes_namespace.calico_apiserver,
     kubectl_manifest.calico_crds
-    ]
-
-  set {
-    name  = "installation.kubernetesProvider"
-    value = "EKS"
-  }
+  ]
 }
+
