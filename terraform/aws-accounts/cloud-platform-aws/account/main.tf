b86e3b09e8b76ef3dd21179f34231da20f2d612a
Jake Mulley <jakemulley@users.noreply.github.com>
2023-03-29 12:19:22 +0100

    Update main.tf

--- terraform/aws-accounts/cloud-platform-aws/account/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/main.tf
@@ -49,7 +49,7 @@ module "iam" {
 
 # Github SSO
 module "sso" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-aws-sso?ref=1.2.6"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-aws-sso?ref=1.2.7"
 
   auth0_tenant_domain = "justice-cloud-platform.eu.auth0.com"
 }
