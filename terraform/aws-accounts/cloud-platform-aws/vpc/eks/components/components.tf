0daa2baed86601308a34f05069986e4cff2ca091
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-05-16 16:04:09 +0100

    Use terraform workspace as the prefix

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -84,7 +84,7 @@ module "external_secrets_operator" {
   dependence_prometheus       = module.monitoring.prometheus_operator_crds_status
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
-  secrets_prefix = lookup(local.live_workspace, terraform.workspace, false) ? "live" : "nonlive"
+  secrets_prefix = terraform.workspace
 }
 module "ingress_controllers_v1" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.2.4"
