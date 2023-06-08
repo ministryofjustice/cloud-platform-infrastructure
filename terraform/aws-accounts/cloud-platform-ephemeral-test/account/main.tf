ba673a0567bfb3344fe9621b1737fcd9a5dc8ee8
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-06-08 10:57:30 +0100

    Bump account-baselines module to 0.1.0

--- terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
+++ terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
@@ -32,7 +32,7 @@ provider "aws" {
 ###########################
 
 module "baselines" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-baselines?ref=0.0.12"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-awsaccounts-baselines?ref=0.1.0"
 
   enable_logging           = true
   enable_slack_integration = true
