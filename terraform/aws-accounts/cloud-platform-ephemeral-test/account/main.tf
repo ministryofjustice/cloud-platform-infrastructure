fdbe6267d02e4154dafec369d72080b4db8e1b37
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2023-06-22 16:27:17 +0100

    feat(TF): add ecr for testing to the ephrmeral account

--- terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
+++ terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
@@ -8,6 +8,11 @@ terraform {
   }
 }
 
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
 provider "aws" {
   region  = "eu-west-2"
   profile = "moj-et"
@@ -65,4 +70,70 @@ module "sso" {
 # New parent DNS zone for clusters
 resource "aws_route53_zone" "aws_account_hostzone_id" {
   name = "et.cloud-platform.service.justice.gov.uk."
+}
+
+# ECR creation for Kuberhealthy checks image
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.2.0"
+
+  # REQUIRED: Repository configuration
+  team_name = var.team_name
+  repo_name = "cloud-platform-kuberhealthy-checks"
+  namespace = var.namespace
+
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["github"]
+
+  # REQUIRED: GitHub repositories that push to this container repository
+  github_repositories = ["cloud-platform-kuberhealthy-checks"]
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  # github_environments = ["production"]
+
+  # Lifecycle policies
+  # Uncomment the below to automatically tidy up old Docker images
+  lifecycle_policy = <<EOF
+    {
+      "rules": [
+        {
+          "rulePriority": 1,
+          "description": "Expire untagged images older than 14 days",
+          "selection": {
+            "tagStatus": "untagged",
+            "countType": "sinceImagePushed",
+            "countUnit": "days",
+            "countNumber": 14
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 2,
+          "description": "Keep last 30 dev and staging images",
+          "selection": {
+            "tagStatus": "tagged",
+            "tagPrefixList": ["dev", "staging"],
+            "countType": "imageCountMoreThan",
+            "countNumber": 30
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 3,
+          "description": "Keep the newest 100 images and mark the rest for expiration",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 100
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+    EOF
 }
