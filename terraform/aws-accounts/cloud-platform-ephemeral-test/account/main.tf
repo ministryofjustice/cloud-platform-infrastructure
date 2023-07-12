2ca6cfe2a6ae5e480122a4cabbdf54b96ac3a047
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-07-12 14:08:58 +0100

    Comment unused data declarations

--- terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
+++ terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
@@ -24,7 +24,7 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
-data "aws_iam_account_alias" "current" {}
+# data "aws_iam_account_alias" "current" {}
 data "aws_region" "current" {}
 
 ###########################
