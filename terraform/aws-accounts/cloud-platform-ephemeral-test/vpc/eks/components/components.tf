82d639a5e828d34877a10e07f5db0e35df60a4e0
PaulWyborn <paul.wyborn@digital.justice.gov.uk>
2023-07-17 11:07:23 +0100

    reference latest external-dns module release

--- terraform/aws-accounts/cloud-platform-ephemeral-test/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-ephemeral-test/vpc/eks/components/components.tf
@@ -70,7 +70,7 @@ module "cert_manager" {
 }
 
 module "external_dns" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-external-dns?ref=1.10.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-external-dns?ref=1.11.0"
 
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   hostzones           = lookup(local.hostzones, terraform.workspace, local.hostzones["default"])
