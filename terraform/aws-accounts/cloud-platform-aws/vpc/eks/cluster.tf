b5b040492b975a59c884a2f7f1c70a97bfe3c291
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-07-21 11:53:55 +0100

    Bump eks addon to bump vpc-cni 1.13.2

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -28,7 +28,7 @@ locals {
   }
   # Default node group minimum capacity 
   default_ng_min_count = {
-    live    = "46"
+    live    = "56"
     live-2  = "2"
     manager = "4"
     default = "2"
@@ -210,7 +210,7 @@ module "eks" {
 # EKS Cluster add-ons #
 #######################
 module "aws_eks_addons" {
-  source                  = "github.com/ministryofjustice/cloud-platform-terraform-eks-add-ons?ref=1.8.0"
+  source                  = "github.com/ministryofjustice/cloud-platform-terraform-eks-add-ons?ref=1.9.0"
   depends_on              = [module.eks.cluster]
   cluster_name            = terraform.workspace
   eks_cluster_id          = module.eks.cluster_id
