cf846554b0f93095262f2ded79500119c19612a2
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-07-25 16:27:00 +0100

    Bump monitoring module to increase prometheus cpu and memory limits (#2380)

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -146,7 +146,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.9.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.9.3"
 
   alertmanager_slack_receivers               = local.enable_alerts ? var.alertmanager_slack_receivers : [{ severity = "dummy", webhook = "https://dummy.slack.com", channel = "#dummy-alarms" }]
   pagerduty_config                           = local.enable_alerts ? var.pagerduty_config : "dummy"
