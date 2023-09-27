c4880476583f4072d52ca461c177e372d5222db9
jakemulley <jakemulley@users.noreply.github.com>
2023-09-27 15:29:57 +0100

    proof-of-concept: add a IPSet allowlist for use with CloudFront

---
+++ terraform/aws-accounts/cloud-platform-aws/account/cloudfront-waf.tf
@@ -0,0 +1,55 @@
+# This is a very specific AWS WAF implemention for use with CloudFront only
+# Users with an ingress must only utilise ModSecurity or the WAF-enabled centralised ingress
+# This WAF IPSet, rule, and corresponding WAF ACL is very specific to Prisoner Content Hub
+# who stream videos from an S3 bucket which is limited to access within a prison estate
+# Using AWS WAF is the only way to IP allowlist with CloudFront
+data "kubernetes_secret_v1" "prisoner_content_hub" {
+  metadata {
+    name      = "ip-allowlist"
+    namespace = "prisoner-content-hub-production"
+  }
+}
+
+resource "aws_waf_ipset" "prisoner_content_hub" {
+  name = "prisoner-content-hub"
+
+  dynamic "ip_set_descriptors" {
+    for_each = {
+      for k, v in data.kubernetes_secret_v1.prisoner_content_hub.data:
+      k => strcontains(v, "/") ? v : "${v}/32"
+    }
+
+    type  = "IPV4"
+    value = each.value
+  }
+}
+
+resource "aws_waf_rule" "prisoner_content_hub" {
+  name        = "prisoner_content_hub"
+  metric_name = "prisoner_content_hub"
+
+  predicates {
+    data_id = aws_waf_ipset.prisoner_content_hub.id
+    negated = false
+    type    = "IPMatch"
+  }
+}
+
+resource "aws_waf_web_acl" "prisoner_content_hub" {
+  name = "prisoner_content_hub"
+  metric_name = "prisoner_content_hub"
+
+  default_action {
+    type = "BLOCK"
+  }
+
+  rules {
+    action {
+      type = "ALLOW"
+    }
+
+    priority = 1
+    rule_id  = aws_waf_rule.prisoner_content_hub.id
+    type     = "REGULAR"
+  }
+}
