d72b510d6b18ac831f593d24e7de20bea669f428
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-10-17 16:35:27 +0100

    Upgrade ingress-controller module to 1.5.0

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -90,7 +90,7 @@ module "external_secrets_operator" {
   secrets_prefix              = terraform.workspace
 }
 module "ingress_controllers_v1" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.4.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.5.0"
 
   replica_count       = "12"
   controller_name     = "default"
@@ -108,7 +108,7 @@ module "ingress_controllers_v1" {
 }
 
 module "modsec_ingress_controllers_v1" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.4.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.5.0"
 
   replica_count                = "12"
   controller_name              = "modsec"
