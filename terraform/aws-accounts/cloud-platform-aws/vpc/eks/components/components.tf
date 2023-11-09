1292438b909440a0270b8e63abdf7743d3d638a7
sj-williams <steve.williams@digital.justice.gov.uk>
2023-11-09 11:13:12 +0000

    bump trivy helm release

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -240,7 +240,7 @@ module "kuberhealthy" {
 }
 
 module "trivy-operator" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-trivy-operator?ref=0.7.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-trivy-operator?ref=0.8.0"
 
   depends_on = [
     module.monitoring.prometheus_operator_crds_status
