b3b7579238d10b0afda2e69e3ce9ae55e8a2e555
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2023-11-17 13:41:43 +0000

    create PodSecurity Violations monitor

--- terraform/global-resources/kibana-alerts.tf
+++ terraform/global-resources/kibana-alerts.tf
@@ -104,3 +104,110 @@ resource "elasticsearch_opensearch_destination" "cloud_platform_alerts" {
 }
 EOF
 }
+
+resource "elasticsearch_opensearch_monitor" "psa_violations" {
+  provider = elasticsearch
+  body     = <<EOF
+{
+   "name": "PodSecurity Violations",
+   "type": "monitor",
+   "enabled": true,
+   "schedule": {
+      "period": {
+         "interval": 1,
+         "unit": "MINUTES"
+      }
+   },
+   "inputs": [
+      {
+         "search": {
+            "indices": [
+               "live-kubernetes-*"
+            ],
+            "query": {
+               "size": 0,
+               "aggregations": {},
+               "query": {
+                  "bool": {
+                     "adjust_pure_negative": true,
+                     "boost": 1,
+                     "filter": [
+                        {
+                            "multi_match": {
+                              "type": "phrase",
+                              "query": "violates PodSecurity",
+                              "lenient": true
+                            }
+                          },
+                          {
+                            "bool": {
+                              "filter": [
+                                {
+                                  "bool": {
+                                    "must_not": {
+                                      "multi_match": {
+                                        "type": "phrase",
+                                        "query": "smoketest-restricted",
+                                        "lenient": true
+                                      }
+                                    }
+                                  }
+                                },
+                                {
+                                  "bool": {
+                                    "must_not": {
+                                      "multi_match": {
+                                        "type": "phrase",
+                                        "query": "smoketest-privileged",
+                                        "lenient": true
+                                      }
+                                    }
+                                  }
+                                }
+                              ]
+                            }
+                          }
+                     ]
+                  }
+               }
+            }
+         }
+      }
+   ],
+   "triggers": [
+      {
+         "name": "PodSecurity Violations",
+         "severity": "5",
+         "condition": {
+            "script": {
+               "source": "ctx.results[0].hits.total.value > 1",
+               "lang": "painless"
+            }
+         },
+         "actions": [
+            {
+               "name": "Notify Cloud Platform lower-priority-alarms Slack Channel",
+               "destination_id": "${elasticsearch_opensearch_destination.cloud_platform_alerts.id}",
+               "throttle_enabled": true,
+               "throttle": {
+                  "value": 60,
+                  "unit": "MINUTES"
+               },
+               "message_template": {
+                  "source": "Monitor {{ctx.monitor.name}} just entered alert status. Please investigate the issue.\n- Trigger: {{ctx.trigger.name}}\n- Severity: {{ctx.trigger.severity}}\n- Period start: {{ctx.periodStart}}\n- Period end: {{ctx.periodEnd}}\n- Contact the user to rectify.",
+                  "lang": "mustache"
+               },
+               "subject_template": {
+                  "source": "PodSecurity Violations found",
+                  "lang": "mustache"
+               }
+            }
+         ]
+      }
+   ]
+}
+EOF
+
+  depends_on = [elasticsearch_opensearch_destination.cloud_platform_alerts]
+
+}
