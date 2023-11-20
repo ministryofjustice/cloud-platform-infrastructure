58e5ba304c84ace07a8ba552ac56f4b20ea8f6e3
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-11-20 11:46:03 +0000

    Bump gatekeeper fot 1.9.2

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -179,7 +179,7 @@ module "monitoring" {
 }
 
 module "gatekeeper" {
-  source     = "github.com/ministryofjustice/cloud-platform-terraform-gatekeeper?ref=1.9.1"
+  source     = "github.com/ministryofjustice/cloud-platform-terraform-gatekeeper?ref=1.9.2"
   depends_on = [module.monitoring, module.modsec_ingress_controllers_v1, module.cert_manager]
 
   dryrun_map = {
