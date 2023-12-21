243df9f839caf5a48db401f80556bb1039b93860
sj-williams <steve.williams@digital.justice.gov.uk>
2023-12-21 13:12:38 +0000

    bump ingress controller module release

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -95,7 +95,7 @@ module "external_secrets_operator" {
   ]
 }
 module "ingress_controllers_v1" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.6.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.6.1"
 
   replica_count       = "12"
   controller_name     = "default"
@@ -113,7 +113,7 @@ module "ingress_controllers_v1" {
 }
 
 module "modsec_ingress_controllers_v1" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.6.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-controller?ref=1.6.1"
 
   replica_count                = "12"
   controller_name              = "modsec"
