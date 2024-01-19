7d8cd9075d534d8518d8cbc3970427cd33836658
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-01-19 13:43:31 +0000

    Bump trivy-operator to 0.8.2 (#2734)

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -247,7 +247,7 @@ module "kuberhealthy" {
 }
 
 module "trivy-operator" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-trivy-operator?ref=0.8.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-trivy-operator?ref=0.8.2"
 
   depends_on = [
     module.monitoring.prometheus_operator_crds_status
