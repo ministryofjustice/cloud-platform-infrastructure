207b89f6575aec44370fedf7a5b95ca66936efb7
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-02-15 14:14:52 +0000

    bump sso module

--- terraform/aws-accounts/cloud-platform-aws/account/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/main.tf
@@ -75,7 +75,7 @@ module "iam" {
 
 # Github SSO
 module "sso" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-aws-sso?ref=1.5.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-aws-sso?ref=1.5.4"
 
   auth0_tenant_domain = "justice-cloud-platform.eu.auth0.com"
 }
