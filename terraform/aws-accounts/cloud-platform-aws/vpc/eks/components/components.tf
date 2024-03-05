d739f69efa4422da728eca5001627d1695c26c2a
Ky <ky.truong@digital.justice.gov.uk>
2024-03-05 09:40:42 +0000

    :arrow_up:bumping monitoring module for prometheus upgrade

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -156,7 +156,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=2.11.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.0.0"
 
   alertmanager_slack_receivers               = local.enable_alerts ? var.alertmanager_slack_receivers : [{ severity = "dummy", webhook = "https://dummy.slack.com", channel = "#dummy-alarms" }]
   pagerduty_config                           = local.enable_alerts ? var.pagerduty_config : "dummy"
