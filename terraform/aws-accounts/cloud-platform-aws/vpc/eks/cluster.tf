1d2f3ee10f098ed7fa1ed495eca45e0d3dfd9c9e
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-03-06 15:30:11 +0000

    bump kube-proxy to v1.26.13-eksbuild.2
    related to ministryofjustice/cloud-platform#5343

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -274,7 +274,7 @@ module "eks" {
 # EKS Cluster add-ons #
 #######################
 module "aws_eks_addons" {
-  source                  = "github.com/ministryofjustice/cloud-platform-terraform-eks-add-ons?ref=1.17.5"
+  source                  = "github.com/ministryofjustice/cloud-platform-terraform-eks-add-ons?ref=1.17.6"
   depends_on              = [module.eks.cluster]
   cluster_name            = terraform.workspace
   eks_cluster_id          = module.eks.cluster_id
