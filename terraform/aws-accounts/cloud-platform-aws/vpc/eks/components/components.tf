3feaf27264e31de5db8b55fa3ec390c394905a64
sj-williams <steve.williams@digital.justice.gov.uk>
2024-03-20 13:46:19 +0000

    bump external-dns module

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/components/components.tf
@@ -73,15 +73,15 @@ module "cert_manager" {
 }
 
 module "external_dns" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-external-dns?ref=1.14.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-external-dns?ref=1.15.0"
 
   cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   hostzones           = lookup(local.hostzones, terraform.workspace, local.hostzones["default"])
   domain_filters      = lookup(local.domain_filters, terraform.workspace, local.domain_filters["default"])
 
 
-  # For live* and manager clusters, filter out test cluster domains
-  enable_test_cluster_filters = lookup(local.prod_workspace, terraform.workspace, false) || terraform.workspace == "live-2"
+  # For tuning external_dns config for production vs test clusters
+  is_live_cluster = lookup(local.prod_workspace, terraform.workspace, false) || terraform.workspace == "live-2"
 
   eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
 
