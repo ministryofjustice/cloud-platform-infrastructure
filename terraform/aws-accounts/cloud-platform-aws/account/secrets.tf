eae81a053e27ab22c504cb3414f0a995fc92ed93
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-04-17 14:08:42 +0100

    update opensearch alert
    update opensearch alert
    
    update opensearch secret
    
    bump iam module
    
    Empty Commit

--- terraform/aws-accounts/cloud-platform-aws/account/secrets.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/secrets.tf
@@ -23,3 +23,26 @@ resource "aws_secretsmanager_secret_version" "circleci" {
 data "aws_secretsmanager_secret_version" "circleci" {
   secret_id = aws_secretsmanager_secret.circleci.id
 }
+
+
+# Slack Webhook URL
+resource "aws_secretsmanager_secret" "slack_webhook_url" {
+  name        = "cloud-platform/slack-webhook-url-for-app-opensearch"
+  description = "url used for Opensearch to post alerts to a channel"
+}
+
+resource "aws_secretsmanager_secret_version" "slack_webhook_url" {
+  secret_id = aws_secretsmanager_secret.slack_webhook_url.id
+  secret_string = jsonencode({
+    url = "CHANGE_ME_IN_THE_CONSOLE" # change this value manually in the console once the secret is created
+  })
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
+
+# Data call to fetch changed value
+data "aws_secretsmanager_secret_version" "slack_webhook_url" {
+  secret_id = aws_secretsmanager_secret.slack_webhook_url.id
+}
