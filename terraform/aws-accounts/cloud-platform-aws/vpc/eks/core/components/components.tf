c158551980905311aad35886c0455429745832b7
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-04-18 14:40:46 +0100

    Bump monitoring to 3.7.4 (#2999)
    * Bump monitoring to 3.7.4
    
    * Commit changes made by code formatters
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -192,16 +192,19 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.7.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.7.4"
 
-  alertmanager_slack_receivers               = local.enable_alerts ? var.alertmanager_slack_receivers : [{ severity = "dummy", webhook = "https://dummy.slack.com", channel = "#dummy-alarms" }]
-  pagerduty_config                           = local.enable_alerts ? var.pagerduty_config : "dummy"
-  cluster_domain_name                        = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  oidc_components_client_id                  = data.terraform_remote_state.cluster.outputs.oidc_components_client_id
-  oidc_components_client_secret              = data.terraform_remote_state.cluster.outputs.oidc_components_client_secret
-  oidc_issuer_url                            = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
-  enable_thanos_sidecar                      = lookup(local.prod_2_workspace, terraform.workspace, false)
-  enable_large_nodesgroup                    = lookup(local.live_workspace, terraform.workspace, false)
+  alertmanager_slack_receivers  = local.enable_alerts ? var.alertmanager_slack_receivers : [{ severity = "dummy", webhook = "https://dummy.slack.com", channel = "#dummy-alarms" }]
+  pagerduty_config              = local.enable_alerts ? var.pagerduty_config : "dummy"
+  cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+  oidc_components_client_id     = data.terraform_remote_state.cluster.outputs.oidc_components_client_id
+  oidc_components_client_secret = data.terraform_remote_state.cluster.outputs.oidc_components_client_secret
+  oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
+  enable_thanos_sidecar         = lookup(local.prod_2_workspace, terraform.workspace, false)
+  enable_large_nodesgroup       = lookup(local.live_workspace, terraform.workspace, false)
+  # The largegroup cpu and memory requests are valid only if the large_nodegroup is enabled.
+  large_nodesgroup_cpu_requests              = terraform.workspace == "live" ? "180000Mi" : "14000Mi"
+  large_nodesgroup_memory_requests           = terraform.workspace == "live" ? "4000m" : "1300m"
   enable_prometheus_affinity_and_tolerations = true
   enable_kibana_proxy                        = lookup(local.live_workspace, terraform.workspace, false)
   kibana_upstream                            = format("%s://%s", "https", lookup(var.elasticsearch_hosts_maps, terraform.workspace, "placeholder-elasticsearch"))
