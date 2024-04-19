4fcfa3db3e558b45c15fe1195895ff1933148c94
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-04-19 16:22:53 +0100

    Remove data related to aws account to fix linting

--- terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
+++ terraform/aws-accounts/cloud-platform-ephemeral-test/account/main.tf
@@ -23,10 +23,10 @@ provider "aws" {
   }
 }
 
-
-data "aws_caller_identity" "current" {}
-data "aws_iam_account_alias" "current" {}
-data "aws_region" "current" {}
+# These are used for opensearch/elasticsearch. Enable them when needed
+# data "aws_caller_identity" "current" {}
+# data "aws_iam_account_alias" "current" {}
+# data "aws_region" "current" {}
 
 ###########################
 # Security Baseguidelines #
