20e03d2eb2501dea70fc5f58bcee9ab8f38ee6cc
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-05-30 13:07:43 +0100

    perf: ⚡️ configure shard sizes and set n data nodes (#3104)
    * perf: ⚡️ configure shard sizes and set n data nodes

---
+++ terraform/aws-accounts/cloud-platform-aws/account/app-opensearch-index-templates.tf
@@ -0,0 +1,109 @@
+resource "opensearch_index_template" "live_kubernetes_cluster" {
+  provider = opensearch.app_logs
+  name     = "live_kubernetes_cluster"
+  body     = <<EOF
+{
+  "index_patterns": [
+    "live_kuberenetes_cluster-*"
+  ],
+    "settings": {
+      "index": {
+        "number_of_shards": "15",
+        "number_of_replicas": "1"
+      }
+    }
+  }
+EOF
+}
+
+resource "opensearch_index_template" "live_kubernetes_ingress" {
+  provider = opensearch.app_logs
+  name     = "live_kubernetes_ingress"
+  body     = <<EOF
+{
+  "index_patterns": [
+    "live_kuberenetes_ingress-*"
+  ],
+    "settings": {
+      "index": {
+        "number_of_shards": "1",
+        "number_of_replicas": "1"
+      }
+    }
+}
+EOF
+}
+
+resource "opensearch_index_template" "live_eventrouter" {
+  provider = opensearch.app_logs
+
+  name = "live_eventrouter"
+  body = <<EOF
+{
+  "index_patterns": [
+    "live_eventrouter-*"
+  ],
+    "settings": {
+      "index": {
+        "number_of_shards": "1",
+        "number_of_replicas": "1"
+      }
+    }
+}
+EOF
+}
+
+resource "opensearch_index_template" "manager_kubernetes_cluster" {
+  provider = opensearch.app_logs
+
+  name = "manager_kubernetes_cluster"
+  body = <<EOF
+{
+  "index_patterns": [
+    "manager_kuberenetes_cluster-*"
+  ],
+    "settings": {
+      "index": {
+        "number_of_shards": "1",
+        "number_of_replicas": "1"
+      }
+    }
+}
+EOF
+}
+
+resource "opensearch_index_template" "manager_kubernetes_ingress" {
+  provider = opensearch.app_logs
+  name     = "manager_kubernetes_ingress"
+  body     = <<EOF
+{
+  "index_patterns": [
+    "manager_kuberenetes_ingress-*"
+  ],
+    "settings": {
+      "index": {
+        "number_of_shards": "1",
+        "number_of_replicas": "1"
+      }
+    }
+}
+EOF
+}
+
+resource "opensearch_index_template" "manager_eventrouter" {
+  provider = opensearch.app_logs
+  name     = "manager_eventrouter"
+  body     = <<EOF
+{
+  "index_patterns": [
+    "manager_eventrouter-*"
+  ],
+    "settings": {
+      "index": {
+        "number_of_shards": "1",
+        "number_of_replicas": "1"
+      }
+    }
+  }
+EOF
+}
