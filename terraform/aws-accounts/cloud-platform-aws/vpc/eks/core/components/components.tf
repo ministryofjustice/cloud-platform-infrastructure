9c43fe8954c6ca44d28ed7e62765cf3c23fa1b9f
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-06-07 11:45:46 +0100

    Bump monitoring to 3.7.10 - Update nginx ingress alerts (#3124)

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -206,7 +206,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.7.9"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.7.10"
 
   alertmanager_slack_receivers  = local.enable_alerts ? var.alertmanager_slack_receivers : [{ severity = "dummy", webhook = "https://dummy.slack.com", channel = "#dummy-alarms" }]
   pagerduty_config              = local.enable_alerts ? var.pagerduty_config : "dummy"
