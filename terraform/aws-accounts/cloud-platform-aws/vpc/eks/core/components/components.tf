5630e77086416b1336f8419ab08a8a8f81b347ff
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-06-19 17:01:10 +0100

    Bump kuberos module to 0.6.0

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -180,14 +180,15 @@ module "modsec_ingress_controllers_v1" {
 }
 
 module "kuberos" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.5.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.6.0"
 
   cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   oidc_kubernetes_client_id     = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_id
   oidc_kubernetes_client_secret = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_secret
   oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
   cluster_address               = data.terraform_remote_state.cluster.outputs.cluster_endpoint
-
+  image_tag = "2.7.0"
+  
   depends_on = [
     module.ingress_controllers_v1,
     module.modsec_ingress_controllers_v1,
