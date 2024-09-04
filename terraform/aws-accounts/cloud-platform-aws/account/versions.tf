6fb591a7b91e148982801ee5a38c01f4604eb6dc
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2024-09-04 15:12:22 +0100

    update terraform-provider-aws version

--- terraform/aws-accounts/cloud-platform-aws/account/versions.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31.0"
+      version = "~> 5.40.0"
     }
     auth0 = {
       source  = "auth0/auth0"
