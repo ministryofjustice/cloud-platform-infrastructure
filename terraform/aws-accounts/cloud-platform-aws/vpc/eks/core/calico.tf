f4c9470a2df42cb128570c05339351e3acf9d33f
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2024-09-25 16:30:11 +0100

    bump tigera module

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/calico.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/calico.tf
@@ -42,7 +42,7 @@ resource "kubectl_manifest" "calico_crds" {
 
 
 module "tigera_calico" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-tigera-calico?ref=0.1.7"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-tigera-calico?ref=0.1.8"
 
   depends_on = [
     kubectl_manifest.calico_crds
