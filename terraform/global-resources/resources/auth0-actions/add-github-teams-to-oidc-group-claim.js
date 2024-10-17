0730ed60bade2dde1e01cf5c2c82e79a8b8cc33c
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-10-17 15:44:55 +0100

    perf: ⚡️ pull more teams (default: 30) max 100

--- terraform/global-resources/resources/auth0-actions/add-github-teams-to-oidc-group-claim.js
+++ terraform/global-resources/resources/auth0-actions/add-github-teams-to-oidc-group-claim.js
@@ -1,26 +1,28 @@
 exports.onExecutePostLogin = async (event, api) => {
-  const fetch = require("node-fetch")
+  const fetch = require("node-fetch");
   // Apply to 'github' connections only
-  if (event.connection.name === 'github') {
+  if (event.connection.name === "github") {
     // Get user's Github profile info (an Auth0 user can have multiple
     // connected accounts - Google, Facebook etc)
     //
     // Github user profile will also contain a Github API access token
     // which we can use to look up teams etc.
-    const github_identity = event.user.identities.find(id => id.connection === 'github');
+    const github_identity = event.user.identities.find(
+      (id) => id.connection === "github",
+    );
     // Get list of Github orgs for the user
     const orgs_req = {
-      url: 'https://api.github.com/user/teams',
+      url: "https://api.github.com/user/teams?per_page=100",
       headers: {
-        'Authorization': 'Bearer ' + github_identity.accessToken
-      }
+        Authorization: "Bearer " + github_identity.accessToken,
+      },
     };
 
     const response = await fetch(orgs_req.url, { headers: orgs_req.headers });
     const body = await response.json();
 
     if (response.status !== 200) {
-      return api.access.deny('Error retrieving teams from github: ' + body)
+      return api.access.deny("Error retrieving teams from github: " + body);
     }
 
     // Construct list of user's Github teams
@@ -36,8 +38,10 @@ exports.onExecutePostLogin = async (event, api) => {
     // Custom OIDC claims should be prefixed with a unique value
     // to prevent clashes with claims from other sources.
     // Common practice is to use a URL
-    api.idToken.setCustomClaim(event.secrets.K8S_OIDC_GROUP_CLAIM_DOMAIN, git_teams);
+    api.idToken.setCustomClaim(
+      event.secrets.K8S_OIDC_GROUP_CLAIM_DOMAIN,
+      git_teams,
+    );
   }
-  return
-}
-
+  return;
+};
