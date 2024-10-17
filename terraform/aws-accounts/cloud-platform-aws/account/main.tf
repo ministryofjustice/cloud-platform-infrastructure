5fe154283bbeb24c31a8a9822153ac641f6a365e
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2024-10-17 15:35:16 +0100

    Fixing ecr document typo from DescribeRepository to DescribeRepositories

--- terraform/aws-accounts/cloud-platform-aws/account/main.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/main.tf
@@ -87,7 +87,7 @@ module "iam" {
 
 # Github SSO
 module "sso" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-aws-sso?ref=1.5.9"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-aws-sso?ref=1.5.10"
 
   auth0_tenant_domain = "justice-cloud-platform.eu.auth0.com"
 }
