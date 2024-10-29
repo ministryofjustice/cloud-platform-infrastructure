ad270e0dd576885fb6ac604682ae5a5986959d54
jackstockley89 <jstock8904@gmail.com>
2024-10-29 17:15:41 +0000

    feat(bash): update help parameter with the latest config parameter usage

--- scripts/addons-upgrade.bash
+++ scripts/addons-upgrade.bash
@@ -21,48 +21,66 @@ check)
 	cdn=`eksctl utils describe-addon-versions --kubernetes-version ${p} --name coredns | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
 	ccdn=`eksctl utils describe-addon-versions --kubernetes-version ${p2} --name coredns | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
 
-	# place kp and ckp in a table
+# Kube-Proxy
+	command=$(kubectl -n kube-system get pods | grep kube-proxy | awk '{print $1}' | head -1)
+	kversion=$(kubectl -n kube-system get pods ${command} -o jsonpath='{.spec.containers[0].image}' | cut -d ':' -f2)
+	
 	echo -e "\n${bold}${purple}"
 	echo -e "---------------------------------------------------------------------------------------------------------"
-	printf " | %-10s | %-40s | %-40s\n" "Kube-Proxy" "Kubernetes Version ${p}" "Kubernetes Version ${p2}"
+	printf " | %-90s\n" "Addon Image Compatibility Check"
+	echo -e "---------------------------------------------------------------------------------------------------------${normal}"
+
+	echo -e "\n${bold}${purple}"
 	echo -e "---------------------------------------------------------------------------------------------------------"
+	printf "| %-90s\n" "cluster version: ${kversion}"
+	printf "| Please check the compatibility of the kube-proxy cluster version with the kubernetes version\n"
+	echo -e "---------------------------------------------------------------------------------------------------------"
+	printf "| %-10s | %-40s | %-40s\n" "Kube-Proxy" "Kubernetes Version ${p}" "Kubernetes Version ${p2}"
 	echo -e "---------------------------------------------------------------------------------------------------------${normal}\n"
-	# for each line in kp and ckp, print the line side by side in a table
 	kp_array=(${kp})
 	ckp_array=(${ckp})
 	for ((i=0; i<${#kp_array[@]}; i++)); do
 		printf "| %-10s | %-40s | %-40s\n" "" "${kp_array[$i]}" "${ckp_array[$i]}"
 	done
-	command=$(kubectl -n kube-system get pods | grep kube-proxy | awk '{print $1}' | head -1)
-	printf "| %-90s\n" "cluster version: `kubectl -n kube-system get pods ${command} -o jsonpath='{.spec.containers[0].image}' | cut -d ':' -f2`"
+	echo -e "---------------------------------------------------------------------------------------------------------"
+
+# VPC-CNI
+	command=$(kubectl -n kube-system get pods | grep aws-node | awk '{print $1}' | head -1)
+	kversion=$(kubectl -n kube-system get pods ${command} -o jsonpath='{.spec.containers[0].image}' | cut -d ':' -f2)
+	
 
 	echo -e "\n${bold}${purple}"
 	echo -e "---------------------------------------------------------------------------------------------------------"
-	printf " | %-10s | %-40s | %-40s\n" "VPC-CNI" "Kubernetes Version ${p}" "Kubernetes Version ${p2}"
+	printf "| %-90s\n" "cluster version: ${kversion}"
+	printf "| Please check the compatibility of the VPC-CNI cluster version with the kubernetes version\n"
 	echo -e "---------------------------------------------------------------------------------------------------------"
+	printf "| %-10s | %-40s | %-40s\n" "VPC-CNI" "Kubernetes Version ${p}" "Kubernetes Version ${p2}"
 	echo -e "---------------------------------------------------------------------------------------------------------${normal}\n"
 	vci_array=(${vci})
 	cvci_array=(${cvci})
 	for ((i=0; i<${#vci_array[@]}; i++)); do
 		printf "| %-10s | %-40s | %-40s\n" "" "${vci_array[$i]}" "${cvci_array[$i]}"
 	done
-	command=$(kubectl -n kube-system get pods | grep aws-node | awk '{print $1}' | head -1)
-	printf "| %-90s\n" "cluster version: `kubectl -n kube-system get pods ${command} -o jsonpath='{.spec.containers[0].image}' | cut -d ':' -f2`"
+	echo -e "---------------------------------------------------------------------------------------------------------"
+	
+# CoreDNS
+	command=$(kubectl -n kube-system get pods | grep coredns | awk '{print $1}' | head -1)
+	kversion=$(kubectl -n kube-system get pods ${command} -o jsonpath='{.spec.containers[0].image}' | cut -d ':' -f2)
+	
 
 	echo -e "\n${bold}${purple}"
 	echo -e "---------------------------------------------------------------------------------------------------------"
-	printf " | %-10s | %-40s | %-40s\n" "CoreDNS" "Kubernetes Version ${p}" "Kubernetes Version ${p2}"
+	printf "| %-90s\n" "cluster version: ${kversion}"
+	printf "| Please check the compatibility of the CoreDNS cluster version with the kubernetes version\n"
 	echo -e "---------------------------------------------------------------------------------------------------------"
+	printf "| %-10s | %-40s | %-40s\n" "CoreDNS" "Kubernetes Version ${p}" "Kubernetes Version ${p2}"
 	echo -e "---------------------------------------------------------------------------------------------------------${normal}\n"
 	cdn_array=(${cdn})
 	ccdn_array=(${ccdn})
 	for ((i=0; i<${#cdn_array[@]}; i++)); do
 		printf "| %-10s | %-40s | %-40s\n" "" "${cdn_array[$i]}" "${ccdn_array[$i]}"
 	done
-	command=$(kubectl -n kube-system get pods | grep coredns | awk '{print $1}' | head -1)
-	printf "| %-90s\n" "cluster version: `kubectl -n kube-system get pods ${command} -o jsonpath='{.spec.containers[0].image}' | cut -d ':' -f2`"
 	echo -e "---------------------------------------------------------------------------------------------------------"
-
 	;;
 before)
 	kubectl -n kube-system get pod ${awsnode} -oyaml >${awsnode}-before.yaml
@@ -79,14 +97,23 @@ after)
 	echo -e "---------------------------------------------------------------------------------------------------------"
 	;;
 help)
-	echo -e "\n${red}Error: Please specify a pod and state.${normal}"
-	echo "- check - check the version all the addons for the kubernetes version inputted"
-	echo "- before - get the yaml of the pod before the upgrade"
-	echo "- after - get the yaml of the pod after the upgrade and do a diff on the before and after yaml for changes"
-	echo "Usage: ./addons-upgrade.bash {check} {kubernetes-version} {current-kubernetes-version}"
-	echo "Example: ./addons-upgrade.bash check 1.29 1.28"
-	echo "Usage: ./addons-upgrade.bash {before|after} {pod}"
-	echo "Example: ./addons-upgrade.bash before aws-node"
+	echo -e "\n${bold}${purple}Usage:${normal}"
+	echo "check: runs a check on the compatibility versions for addons for the kubernetes versions inputted"
+	echo "	- parameters: {upgrade-kubernetes-version} {current-kubernetes-version}"
+	echo "	- Usage: ./addons-upgrade.bash {check} {upgrade-kubernetes-version} {current-kubernetes-version}"
+	echo "	- Example: ./addons-upgrade.bash check 1.29 1.28"
+
+	echo "before: get the yaml of the pod before the upgrade to be used in diff after the upgrade"
+	echo "	- parameters: {aws-node|kube-proxy|coredns}"
+	echo " 	- Usage: ./addons-upgrade.bash {before} {pod}"
+	echo " 	- Example: ./addons-upgrade.bash before aws-node"
+
+	echo "after: get the yaml of the pod after the upgrade and do a diff on the before and after yaml for changes"
+	echo "	- parameters: {aws-node|kube-proxy|coredns}"
+	echo " 	- Usage: ./addons-upgrade.bash {after} {pod}"
+	echo " 	- Example: ./addons-upgrade.bash after aws-node"
+
+	echo "help - display the usage information"
 	exit 1
 	;;
 *)
