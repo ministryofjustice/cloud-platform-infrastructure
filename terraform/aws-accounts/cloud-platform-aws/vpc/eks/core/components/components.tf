a31d713c79bec58b0e09c422eaf407a356570c17
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-10-29 13:08:08 +0000

    fix: 🐛 incorrect chart version

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -70,7 +70,7 @@ module "descheduler" {
 module "label_pods_controller" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-label-pods?ref=1.1.3"
 
-  chart_version = "1.0.2"
+  chart_version = "1.0.1"
   # https://github.com/ministryofjustice/cloud-platform-infrastructure/blob/main/terraform/aws-accounts/cloud-platform-aws/account/ecr.tf
   ecr_url   = "754256621582.dkr.ecr.eu-west-2.amazonaws.com/webops/cloud-platform-terraform-label-pods"
   image_tag = "1.1.3"
