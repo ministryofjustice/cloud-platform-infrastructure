0e256df21222d7406bbef5861e0b2f45cec08dd1
jackstockley89 <jstock8904@gmail.com>
2024-10-30 16:12:56 +0000

    feat(bash): add highlight of version compatibility when running a check on addon versions with k8 version

--- scripts/addons-upgrade.bash
+++ scripts/addons-upgrade.bash
@@ -9,17 +9,18 @@ awsnode=(${command})
 bold=$(tput bold)
 normal=$(tput sgr0)
 purple=$(tput setaf 5)
+green=$(tput setaf 2)
 red=$(tput setaf 1)
 
 case $ba in
 check)
-## get the version of the addons for the kubernetes version and remove [] and AddonVersion from the output
-	kp=`eksctl utils describe-addon-versions --kubernetes-version ${p} --name kube-proxy | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
-	ckp=`eksctl utils describe-addon-versions --kubernetes-version ${p2} --name kube-proxy | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
-	vci=`eksctl utils describe-addon-versions --kubernetes-version ${p} --name vpc-cni | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
-	cvci=`eksctl utils describe-addon-versions --kubernetes-version ${p2} --name vpc-cni | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
-	cdn=`eksctl utils describe-addon-versions --kubernetes-version ${p} --name coredns | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
-	ccdn=`eksctl utils describe-addon-versions --kubernetes-version ${p2} --name coredns | grep AddonVersion | awk '{print $2}' | tr -d '[],'`
+## get the version of the addons for the kubernetes version and remove [], AddonVersion and "" from the output
+	kp=`eksctl utils describe-addon-versions --kubernetes-version ${p} --name kube-proxy | grep AddonVersion | awk '{print $2}' | tr -d '[],' | tr -d '"'`
+	ckp=`eksctl utils describe-addon-versions --kubernetes-version ${p2} --name kube-proxy | grep AddonVersion | awk '{print $2}' | tr -d '[],' | tr -d '"'`
+	vci=`eksctl utils describe-addon-versions --kubernetes-version ${p} --name vpc-cni | grep AddonVersion | awk '{print $2}' | tr -d '[],' | tr -d '"'`
+	cvci=`eksctl utils describe-addon-versions --kubernetes-version ${p2} --name vpc-cni | grep AddonVersion | awk '{print $2}' | tr -d '[],' | tr -d '"'`
+	cdn=`eksctl utils describe-addon-versions --kubernetes-version ${p} --name coredns | grep AddonVersion | awk '{print $2}' | tr -d '[],' | tr -d '"'`
+	ccdn=`eksctl utils describe-addon-versions --kubernetes-version ${p2} --name coredns | grep AddonVersion | awk '{print $2}' | tr -d '[],' | tr -d '"'`
 
 # Kube-Proxy
 	command=$(kubectl -n kube-system get pods | grep kube-proxy | awk '{print $1}' | head -1)
@@ -40,7 +41,15 @@ check)
 	kp_array=(${kp})
 	ckp_array=(${ckp})
 	for ((i=0; i<${#kp_array[@]}; i++)); do
-		printf "| %-10s | %-40s | %-40s\n" "" "${kp_array[$i]}" "${ckp_array[$i]}"
+		if [[ ${kp_array[$i]} =~ ${kversion} ]] && [[ ${ckp_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "${green}${bold}${kp_array[$i]}${normal}" "${green}${bold}${ckp_array[$i]}${normal}"
+		elif [[ ${kp_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "${green}${bold}${kp_array[$i]}${normal}" ""
+		elif [[ ${ckp_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "" "${green}${bold}${ckp_array[$i]}${normal}"
+		else
+			printf "| %-10s | %-40s | %-40s\n" "" "${kp_array[$i]}" "${ckp_array[$i]}"
+		fi
 	done
 	echo -e "---------------------------------------------------------------------------------------------------------"
 
@@ -59,7 +68,15 @@ check)
 	vci_array=(${vci})
 	cvci_array=(${cvci})
 	for ((i=0; i<${#vci_array[@]}; i++)); do
-		printf "| %-10s | %-40s | %-40s\n" "" "${vci_array[$i]}" "${cvci_array[$i]}"
+		if [[ ${vci_array[$i]} =~ ${kversion} ]] && [[ ${cvci_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "${green}${bold}${vci_array[$i]}${normal}" "${green}${bold}${cvci_array[$i]}${normal}"
+		elif [[ ${vci_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "${green}${bold}${vci_array[$i]}${normal}" ""
+		elif [[ ${cvci_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "" "${green}${bold}${cvci_array[$i]}${normal}"
+		else
+			printf "| %-10s | %-40s | %-40s\n" "" "${vci_array[$i]}" "${cvci_array[$i]}"
+		fi
 	done
 	echo -e "---------------------------------------------------------------------------------------------------------"
 	
@@ -78,7 +95,15 @@ check)
 	cdn_array=(${cdn})
 	ccdn_array=(${ccdn})
 	for ((i=0; i<${#cdn_array[@]}; i++)); do
-		printf "| %-10s | %-40s | %-40s\n" "" "${cdn_array[$i]}" "${ccdn_array[$i]}"
+		if [[ ${cdn_array[$i]} =~ ${kversion} ]] && [[ ${ccdn_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "${green}${bold}${cdn_array[$i]}${normal}" "${green}${bold}${ccdn_array[$i]}${normal}"
+		elif [[ ${cdn_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "${green}${bold}${cdn_array[$i]}${normal}" ""
+		elif [[ ${ccdn_array[$i]} =~ ${kversion} ]]; then
+			printf "| %-10s | %-40s | %-40s\n" "" "" "${green}${bold}${ccdn_array[$i]}${normal}"
+		else
+			printf "| %-10s | %-40s | %-40s\n" "" "${cdn_array[$i]}" "${ccdn_array[$i]}"
+		fi
 	done
 	echo -e "---------------------------------------------------------------------------------------------------------"
 	;;
