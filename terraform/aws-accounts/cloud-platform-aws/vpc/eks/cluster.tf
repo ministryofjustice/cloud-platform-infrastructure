db97b87e969d12d6794aa713f558ef1f049748f2
jackstockley89 <jstock8904@gmail.com>
2024-11-13 16:27:53 +0000

    feat(tf): update live cluster version

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -35,7 +35,7 @@ locals {
   }
   # To manage different cluster versions
   cluster_version = {
-    live    = "1.28"
+    live    = "1.29"
     live-2  = "1.29"
     manager = "1.29"
     default = "1.29"
