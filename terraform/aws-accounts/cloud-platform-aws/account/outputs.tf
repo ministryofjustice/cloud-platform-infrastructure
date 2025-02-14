852fe2261253d4a75c6c9af847376d3aefdce0c4
sj-williams <steve.williams@digital.justice.gov.uk>
2025-02-14 15:17:51 +0000

    fix: 🐛 set ssm param output to sensitive

--- terraform/aws-accounts/cloud-platform-aws/account/outputs.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/outputs.tf
@@ -16,4 +16,5 @@ output "click_here_to_login" {
 output "github_teams_filter_api_key" {
   value       = module.sso.github_teams_filter_api_key
   description = "API key for the GitHub teams filter API"
+  sensitive   = true
 }
