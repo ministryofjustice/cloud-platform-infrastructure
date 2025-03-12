5d29011433882cff2bc720b8503406cdc4214410
Emterry <emma.terry@digital.justice.gov.uk>
2025-03-12 11:56:17 +0000

    bump monitoring module

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -207,7 +207,7 @@ module "logging" {
 }
 
 module "monitoring" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.19.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.19.1"
 
   alertmanager_slack_receivers  = local.enable_alerts ? var.alertmanager_slack_receivers : [{ severity = "dummy", webhook = "https://dummy.slack.com", channel = "#dummy-alarms" }]
   pagerduty_config              = local.enable_alerts ? var.pagerduty_config : "dummy"
