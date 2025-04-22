a1048da019f4436a4fb5ccc591cbb3e9134589de
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2025-04-22 17:08:04 +0100

    Updating eks csi driver to 1.41.0 post 1.30 kube upgrade

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/eks-csi.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/eks-csi.tf
@@ -1,4 +1,4 @@
 module "eks_csi" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-eks-csi?ref=1.2.1"
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-eks-csi?ref=1.2.2"
   eks_cluster = terraform.workspace
 }
