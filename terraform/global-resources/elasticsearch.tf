e03bd8bd5cab61aa7d9d75fbee6a81279b42db6a
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-04-22 12:18:38 +0100

    Increase warm_count to 2 to meet minimum threshold and adjust formatting

--- terraform/global-resources/elasticsearch.tf
+++ terraform/global-resources/elasticsearch.tf
@@ -308,14 +308,16 @@ resource "aws_elasticsearch_domain" "live-2" {
     instance_type            = "t3.small.search"
     instance_count           = "1"
     dedicated_master_enabled = true
-    dedicated_master_count = 1
+    dedicated_master_count   = 1
     zone_awareness_enabled   = true
     zone_awareness_config {
       availability_zone_count = 3
     }
-    warm_count   = 2
+
+    warm_count = 2
     warm_enabled = true
-    warm_type    = "ultrawarm1.medium.elasticsearch"
+    warm_type = "ultrawarm1.medium.elasticsearch"
+
     cold_storage_options {
       enabled = true
     }
