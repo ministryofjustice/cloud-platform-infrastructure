93dbfd50042531fca72b6b9313c282bf8423bf91
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-04-23 11:17:19 +0100

    feat: downscale live-2 OpenSearch cluster

--- terraform/global-resources/elasticsearch.tf
+++ terraform/global-resources/elasticsearch.tf
@@ -305,18 +305,18 @@ resource "aws_elasticsearch_domain" "live-2" {
   elasticsearch_version = "OpenSearch_1.3"
 
   cluster_config {
-    instance_type            = "t3.small.search"
-    instance_count           = "1"
+    instance_type            = "r5.large.search"
+    instance_count           = "2"
     dedicated_master_enabled = true
-    dedicated_master_count   = 1
+    dedicated_master_count   = 3
     zone_awareness_enabled   = true
     zone_awareness_config {
-      availability_zone_count = 3
+      availability_zone_count = 2
     }
 
     warm_count = 2
     warm_enabled = true
-    warm_type = "ultrawarm1.medium.elasticsearch"
+    warm_type = "ultrawarm1.medium.search"
 
     cold_storage_options {
       enabled = true
@@ -326,7 +326,7 @@ resource "aws_elasticsearch_domain" "live-2" {
   ebs_options {
     ebs_enabled = "true"
     volume_type = "gp3"
-    volume_size = "10"
+    volume_size = "50"
     iops        = "3000"
   }
 
