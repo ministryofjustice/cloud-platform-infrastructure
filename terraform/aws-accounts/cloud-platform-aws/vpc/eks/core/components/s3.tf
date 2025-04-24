c94c74c23f61c65c1f6b2e08a1852eef2b3c8378
sj-williams <steve.williams@digital.justice.gov.uk>
2025-04-24 15:22:34 +0100

    feat: 🎸 add s3 bucket for storing alertmanager webhook configs

---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/s3.tf
@@ -0,0 +1,42 @@
+#################################################
+# S3 resources for Alertmanager Slack Receivers #
+#################################################
+
+module "s3_bucket_alertmanager_slack_receivers" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.3.0"
+
+  count = terraform.workspace == "live" ? 1 : 0
+
+  bucket = "cloud-platform-alertmanager-slack-receivers"
+  acl    = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_object" "alertmanager_slack_receivers" {
+  count  = terraform.workspace == "live" ? 1 : 0
+
+  bucket = module.s3_bucket_alertmanager_slack_receivers[0].s3_bucket_id
+  key    = "alertmanager_slack_receivers.json"
+  acl    = "private"
+
+  content = jsonencode(var.alertmanager_slack_receivers)
+
+  server_side_encryption = "AES256"
+}
