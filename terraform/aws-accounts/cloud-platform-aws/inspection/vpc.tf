089ceec133ed4e762a120752dd4174d31ca7c2b2
David Sibley <david.sibley@digital.justice.gov.uk>
2025-05-22 17:54:08 +0100

    This PR creates an inspection vpc, an AWS network firewall with endpoints in private subnets, and uses a data call to compute the correct route from an intra subnet to a firewall endpoint
    placeholders to get started on creating firewall, policy, and rule-group through published modules
    
    finished first pass on firewall implementation
    
    minor linting and structure adjustments, pin module version:
    
    refactored to a separate inspection VPC
    
    bugfixes and final routing information
    
    use correct backend key
    
    moved to new position in folder structure
    
    Commit changes made by code formatters
    
    resolved tflint issue

---
+++ terraform/aws-accounts/cloud-platform-aws/inspection/vpc.tf
@@ -0,0 +1,88 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+/* Because the route table details aren't fully exposed this allows us to map route tables to subnets and from there
+we can get the correct availability zone */
+data "aws_route_table" "intra" {
+  for_each = {
+    for subnet in module.vpc.intra_subnet_objects : subnet.id => subnet
+  }
+
+  subnet_id = each.key
+}
+
+locals {
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, local.num_azs)
+  num_azs  = 3
+
+  subnet_cidrs    = cidrsubnets("10.0.0.0/16", 12, 12, 12, 12, 12, 12)
+  private_subnets = slice(local.subnet_cidrs, 0, 3)
+  intra_subnets   = slice(local.subnet_cidrs, 3, 6)
+
+  /* Create a map of Availability Zones with corresponding VPC Endpoint IDs */
+  az_to_endpoint_id = {
+    for sync in module.cloud-platform-firewall.status[0].sync_states :
+    sync.availability_zone => sync.attachment[0].endpoint_id
+  }
+
+  az_to_route_table_id = {
+    for az, subnet_id in local.az_to_subnet_id :
+    az => data.aws_route_table.intra[subnet_id].id
+  }
+
+  az_to_subnet_id = {
+    for subnet in module.vpc.intra_subnet_objects :
+    subnet.availability_zone => subnet.id
+  }
+
+  az_to_route_table_and_endpoint = {
+    for az in local.azs : az => {
+      route_table_id  = local.az_to_route_table_id[az]
+      vpc_endpoint_id = local.az_to_endpoint_id[az]
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.21.0"
+
+  name = "inspection-vpc"
+
+  azs  = local.azs
+  cidr = local.vpc_cidr
+
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  create_igw                           = false
+  create_multiple_intra_route_tables   = true
+
+  enable_flow_log = true
+
+  intra_subnets      = local.intra_subnets
+  intra_subnet_names = [for key in local.azs : "transit-${key}"]
+
+  private_subnets      = local.private_subnets
+  private_subnet_names = [for key in local.azs : "firewall-${key}"]
+}
+
+resource "aws_route" "transit-to-firewall" {
+  for_each               = local.az_to_route_table_and_endpoint
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = each.value["route_table_id"]
+  vpc_endpoint_id        = each.value["vpc_endpoint_id"]
+}
+
+/* Add a route back to the transit gateway once that's been constructed and attached
+resource "aws_route" "firewall-to-transit" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = module.vpc.private_route_table_ids
+  transit_gateway_id     = ""
+}
+*/
