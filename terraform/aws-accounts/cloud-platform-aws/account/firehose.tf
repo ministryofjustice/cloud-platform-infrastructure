39849d646b45713b8fa974754312cf7dcc0f92dc
Ky <ky.truong@digital.justice.gov.uk>
2025-06-03 11:12:50 +0100

    AWS Firehose to stream CloudWatch log groups to Cortex - live cluster

--- terraform/aws-accounts/cloud-platform-aws/account/firehose.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/firehose.tf
@@ -1,8 +1,8 @@
 #### AWS Firehose to stream CloudWatch log groups to Cortex XSIAM
 
-# test cluster EKS logs to Cortex XSIAM preproduction environment
-module "firehose_eks_logs_to_xsiam" {
+# live EKS logs to Cortex XSIAM
+module "firehose_eks_live_logs_to_xsiam" {
   source                     = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=1.0.0"
-  cloudwatch_log_group_names = ["/aws/eks/cp-0206-0839/cluster"]
+  cloudwatch_log_group_names = [data.terraform_remote_state.eks_live.outputs.cloudwatch_log_group_name]
   destination_http_endpoint  = data.aws_ssm_parameter.account["cortex_xsiam_endpoint"].value
 }
