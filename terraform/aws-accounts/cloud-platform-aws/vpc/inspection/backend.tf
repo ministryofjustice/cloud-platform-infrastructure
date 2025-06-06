9934723a7a61cef339d3b487f96ffe172f0531d0
David Sibley <david.sibley@digital.justice.gov.uk>
2025-06-06 16:34:29 +0100

    use correct backend key

--- terraform/aws-accounts/cloud-platform-aws/vpc/inspection/backend.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/inspection/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "cloud-platform-terraform-state"
-    key     = "transit-gateway/terraform.tfstate"
+    key     = "inspection/terraform.tfstate"
     region  = "eu-west-1"
     profile = "moj-cp"
   }
