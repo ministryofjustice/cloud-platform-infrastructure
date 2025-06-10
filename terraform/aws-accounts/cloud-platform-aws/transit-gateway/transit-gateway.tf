8ed4a0b0c345ce8e65ac4495e4276da84d724795
David Sibley <david.sibley@digital.justice.gov.uk>
2025-06-10 15:10:48 +0100

    create attachment and routing skeleton
    amend referencing to correctly point at root level outputs
    
    really fix the referencing this time
    
    fixed route table ID referencing
    
    caught inspection outputs
    
    added diagnostic output to help parse remote state outputs
    
    swapped to using a data source approach to retrieve VPC and subnet information
    
    caught a missing attribute
    
    Commit changes made by code formatters
    
    removed validation output
    
    ensure inspection VPC attached with appliance mode to ensure stateful flows
    
    get the argument name right for appliance mode support
    
    setting appliance_mode_support as enabled as per the resource documentation doesn't appear to work so setting as true to check the condition

--- terraform/aws-accounts/cloud-platform-aws/transit-gateway/transit-gateway.tf
+++ terraform/aws-accounts/cloud-platform-aws/transit-gateway/transit-gateway.tf
@@ -1,5 +1,13 @@
 locals {
   tgw_route_table_names = toset(["external", "inspection", "internal"])
+  vpc_attachments = {
+    inspection = {
+      appliance_mode_support = true
+      vpc_id                 = data.aws_vpc.inspection_vpc.id
+      route_table            = "inspection"
+      subnet_ids             = toset([for subnet in data.aws_subnet.inspection_vpc_intra : subnet.id])
+    }
+  }
 }
 
 module "cloud-platform-transit-gateway" {
@@ -13,10 +21,22 @@ module "cloud-platform-transit-gateway" {
   enable_default_route_table_association = false
   enable_default_route_table_propagation = false
   share_tgw                              = false
-  vpc_attachments                        = {}
+  vpc_attachments                        = local.vpc_attachments
 }
 
 resource "aws_ec2_transit_gateway_route_table" "this" {
   for_each           = local.tgw_route_table_names
   transit_gateway_id = module.cloud-platform-transit-gateway.ec2_transit_gateway_id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "this" {
+  for_each                       = local.vpc_attachments
+  transit_gateway_attachment_id  = module.cloud-platform-transit-gateway.ec2_transit_gateway_vpc_attachment[each.key].id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this[each.value.route_table].id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
+  for_each                       = local.vpc_attachments
+  transit_gateway_attachment_id  = module.cloud-platform-transit-gateway.ec2_transit_gateway_vpc_attachment[each.key].id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this[each.value.route_table].id
 }
