4ac70819a8063f6e445b0acd73d0a9d431b11d87
David Sibley <david.sibley@digital.justice.gov.uk>
2025-07-02 10:38:40 +0100

    updates to firewall policy to allow web traffic from internal platform to cloud platform using ip and port sets

--- terraform/aws-accounts/cloud-platform-aws/inspection/firewall.tf
+++ terraform/aws-accounts/cloud-platform-aws/inspection/firewall.tf
@@ -1,5 +1,6 @@
 locals {
-  raw_rules        = fileexists("./firewall_rules.json") ? jsondecode(file("./firewall_rules.json")) : {}
+  raw_rules        = fileexists("./firewall_rules.json") ? jsondecode(templatefile("./firewall_rules.json")) : {}
+  rulegroup_sets   = fileexists("./firewall_sets.json") ? jsondecode(templatefile("./firewall_sets.json")) : {}
   sorted_rule_keys = sort(keys(local.raw_rules))
   stateful_rules = [for idx, key in local.sorted_rule_keys : {
     action = local.raw_rules[key].action
@@ -72,11 +73,19 @@ module "cloud-platform-firewall-rule-group" {
   type        = "STATEFUL"
 
   rule_group = {
-    stateful_rule_options = {
-      rule_order = "DEFAULT_ACTION_ORDER"
+    rule_variables = {
+      ip_sets   = local.rulegroup_sets["ip_sets"]
+      port_sets = local.rulegroup_sets["port_sets"]
     }
     rules_source = {
       stateful_rule = local.stateful_rules
+    }
+    rule_variables = {
+      ip_sets   = local.ip_sets
+      port_sets = local.port_sets
+    }
+    stateful_rule_options = {
+      rule_order = "DEFAULT_ACTION_ORDER"
     }
   }
 }
