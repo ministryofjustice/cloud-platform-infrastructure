0f76636dd086d93412582984dd01b18fa35afb38
Ky <ky.truong@digital.justice.gov.uk>
2025-07-02 11:38:57 +0100

    output s3 bucket arn from logging module

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/outputs.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/outputs.tf
@@ -2,3 +2,8 @@ output "fluent_bit_irsa_arn" {
   description = "IAM Role ARN for Fluent Bit IRSA"
   value       = module.logging.fluent_bit_irsa_arn
 }
+
+output "s3_bucket_application_logs_arn" {
+  description = "S3 bucket ARN for application logs"
+  value       = module.logging.s3_bucket_application_logs_arn
+}
