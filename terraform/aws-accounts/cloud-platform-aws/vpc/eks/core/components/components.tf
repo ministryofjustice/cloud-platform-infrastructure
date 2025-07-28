02d0894e666498725800d757b8d4e31fbe176d13
Mike Bell <mike.bell1@justice.gov.uk>
2025-07-28 10:25:40 +0100

    chore: split github teams filter into own file

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -1,12 +1,1 @@
-module "github-teams-filter" {
-  source = "github.com/ministryofjustice/cloud-platform-github-teams-filter?ref=1.2.0"
 
-  count          = terraform.workspace == "live" ? 1 : 0
-  chart_version  = "1.0.1"
-  ecr_url        = "754256621582.dkr.ecr.eu-west-2.amazonaws.com/webops/cloud-platform-github-teams-filter"
-  image_tag      = "7d8e836a0685bd50fcc23f3b824a0aed892cf9b4"
-  replica_count  = 2
-  hostname       = "github-teams-filter.apps.${data.aws_route53_zone.selected.name}"
-  filter_api_key = data.terraform_remote_state.account.outputs.github_teams_filter_api_key
-
-}
