f4fae26354dfa6eb3259a790ee3fc010789d819f
Mike Bell <mike.bell1@justice.gov.uk>
2025-07-28 10:23:54 +0100

    chore: split start pack into own file

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -1,15 +1,3 @@
-module "starter_pack" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.3.0"
-
-  enable_starter_pack = lookup(local.prod_2_workspace, terraform.workspace, false) ? false : true
-  cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-
-  depends_on = [
-    module.ingress_controllers_v1,
-    module.modsec_ingress_controllers_v1
-  ]
-}
-
 module "velero" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=2.5.1"
 
---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/starter_pack.tf
@@ -0,0 +1,11 @@
+module "starter_pack" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-starter-pack?ref=0.3.0"
+
+  enable_starter_pack = lookup(local.prod_2_workspace, terraform.workspace, false) ? false : true
+  cluster_domain_name = data.terraform_remote_state.cluster.outputs.cluster_domain_name
+
+  depends_on = [
+    module.ingress_controllers_v1,
+    module.modsec_ingress_controllers_v1
+  ]
+}
