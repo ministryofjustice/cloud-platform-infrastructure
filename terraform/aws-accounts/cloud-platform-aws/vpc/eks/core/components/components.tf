9edf4fcbc130210bbcf5878f2f1595f20d5ccbcd
Mike Bell <mike.bell1@justice.gov.uk>
2025-07-28 10:15:43 +0100

    chore: split descheduler into own file

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -1,13 +1,3 @@
-module "descheduler" {
-  count  = lookup(local.manager_workspace, terraform.workspace, false) ? 0 : 1
-  source = "github.com/ministryofjustice/cloud-platform-terraform-descheduler?ref=0.9.2"
-
-  depends_on = [
-    module.monitoring,
-    module.label_pods_controller
-  ]
-}
-
 module "label_pods_controller" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-label-pods?ref=1.2.0"
 
