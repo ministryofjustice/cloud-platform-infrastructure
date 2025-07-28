b0862d922b1783d130b47dd998f4c767ba05ce5a
Mike Bell <mike.bell1@justice.gov.uk>
2025-07-28 10:24:22 +0100

    chore: split velero into own file

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -1,16 +1,3 @@
-module "velero" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-velero?ref=2.5.1"
-
-  enable_velero               = lookup(local.prod_2_workspace, terraform.workspace, false)
-  cluster_domain_name         = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
-  node_agent_cpu_requests     = "2m"
-
-  depends_on = [
-    module.label_pods_controller
-  ]
-}
-
 module "trivy-operator" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-trivy-operator?ref=0.13.0"
 
