137ed0e0973a786008afb3f6fdbf0234a95866e8
Mike Bell <mike.bell1@justice.gov.uk>
2025-07-28 10:22:04 +0100

    chore: split kuberos into own file

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -1,21 +1,3 @@
-module "kuberos" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-kuberos?ref=0.7.0"
-
-  cluster_domain_name           = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  oidc_kubernetes_client_id     = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_id
-  oidc_kubernetes_client_secret = data.terraform_remote_state.cluster.outputs.oidc_kubernetes_client_secret
-  oidc_issuer_url               = data.terraform_remote_state.cluster.outputs.oidc_issuer_url
-  cluster_address               = data.terraform_remote_state.cluster.outputs.cluster_endpoint
-  image_tag                     = "2.7.0"
-  replica_count                 = 8
-
-  depends_on = [
-    module.ingress_controllers_v1,
-    module.modsec_ingress_controllers_v1,
-    module.non_prod_ingress_controllers_v1
-  ]
-}
-
 module "logging" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=1.26.7"
 
