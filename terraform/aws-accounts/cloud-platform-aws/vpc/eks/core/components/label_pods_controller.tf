0ebc45c74031d94248b133b294f6412976901695
Mike Bell <mike.bell1@justice.gov.uk>
2025-07-28 10:16:36 +0100

    chore: split label_pods_controller into own file

---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/label_pods_controller.tf
@@ -0,0 +1,8 @@
+module "label_pods_controller" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-label-pods?ref=1.2.0"
+
+  chart_version = "1.0.2"
+  # https://github.com/ministryofjustice/cloud-platform-infrastructure/blob/main/terraform/aws-accounts/cloud-platform-aws/account/ecr.tf
+  ecr_url   = "754256621582.dkr.ecr.eu-west-2.amazonaws.com/webops/cloud-platform-terraform-label-pods"
+  image_tag = "1.2.0"
+}
