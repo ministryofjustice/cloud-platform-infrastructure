4c5b8187ceecc1845a93657219229a2ce094291c
Mike Bell <mike.bell1@justice.gov.uk>
2025-07-28 10:22:45 +0100

    chore: split logging into own file

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/components.tf
@@ -1,21 +1,3 @@
-module "logging" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-logging?ref=1.26.7"
-
-  opensearch_app_host = lookup(var.opensearch_app_host_map, terraform.workspace, "placeholder-opensearch")
-  elasticsearch_host  = lookup(var.elasticsearch_hosts_maps, terraform.workspace, "placeholder-elasticsearch")
-
-  depends_on = [
-    module.label_pods_controller
-  ]
-
-  # Required variables for tags in S3-Bucket submodule
-  business_unit = local.default_tags["business-unit"]
-  application   = local.default_tags["application"]
-  is_production = local.default_tags["is-production"]
-  team_name     = local.default_tags["owner"]
-
-}
-
 module "monitoring" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-monitoring?ref=3.26.0"
 
