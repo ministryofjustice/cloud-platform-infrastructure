9a9c126316e537d305dd7cc9bdcd57bd0ee5a52f
Wajid Farid <wajid.farid@justice.gov.uk>
2025-08-14 16:14:58 +0100

    point to gk module source 1.16.11

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/gatekeeper.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/gatekeeper.tf
@@ -1,5 +1,5 @@
 module "gatekeeper" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-gatekeeper?ref=1.16.9"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-gatekeeper?ref=1.16.11"
 
   dryrun_map = {
     service_type                       = false,
