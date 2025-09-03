8a96c5bf53e68a7c8a20177a89bfb0bc3e5551cc
Mike Bell <mike.bell1@justice.gov.uk>
2025-09-03 11:48:58 +0100

    refactor: add dep on cert manager

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/label_pods_controller.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/label_pods_controller.tf
@@ -5,4 +5,8 @@ module "label_pods_controller" {
   # https://github.com/ministryofjustice/cloud-platform-infrastructure/blob/main/terraform/aws-accounts/cloud-platform-aws/account/ecr.tf
   ecr_url   = "754256621582.dkr.ecr.eu-west-2.amazonaws.com/webops/cloud-platform-terraform-label-pods"
   image_tag = "1.2.0"
+
+  depends_on = [
+    "cert_manager"
+  ]
 }
