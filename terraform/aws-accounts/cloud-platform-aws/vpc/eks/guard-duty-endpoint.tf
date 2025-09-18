973b943188b0c6a8a62d04c1c79c9deefa75f0de
zuriguardiola <zuri.guardiola@justice.gov.uk>
2025-09-18 10:52:01 +0100

    update subnets

---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/guard-duty-endpoint.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "guardduty_vpce" {
+  name        = "guardduty-vpce-sg"
+  description = "Security group for GuardDuty VPC endpoint"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/16"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "guardduty-vpce-sg"
+  }
+}
+
+# GuardDuty interface endpoint
+resource "aws_vpc_endpoint" "guardduty_runtime" {
+  vpc_id            = module.vpc.vpc_id
+  vpc_endpoint_type = "Interface"
+
+  service_name = "com.amazonaws.eu-west-2.guardduty-data"
+
+  subnet_ids         = data.aws_subnets.eks_private.ids
+  security_group_ids = [aws_security_group.guardduty_vpce.id]
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = "guardduty-runtime-endpoint"
+  }
+}
+
+data "aws_subnets" "eks_private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+
+  tags = {
+    SubnetType = "EKS-Private"
+  }
+}
