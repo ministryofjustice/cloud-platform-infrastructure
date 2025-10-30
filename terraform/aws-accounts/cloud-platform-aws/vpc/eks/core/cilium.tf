68814c0050441d0cff781aa07450dcb2c8a3d719
Mike Bell <mike.bell1@justice.gov.uk>
2025-10-30 15:19:14 +0000

    feat: add cilium module

---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/cilium.tf
@@ -0,0 +1,5 @@
+module "cilium" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cilium?ref=cilium-node-group" # use the latest release
+
+  enable_clusterwide_policies = true
+}
