70a4d0e838f9ac2ca80dd0b6c3601868fdd4fc13
zuriguardiola <zuri.guardiola@justice.gov.uk>
2025-11-05 13:00:34 +0000

    Separate endpoint files to avoid resource recreation

---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/guardduty-data-vpce.tf
@@ -0,0 +1,50 @@
+module "aws_vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "6.0.0"
+
+  vpc_id = module.vpc.vpc_id
+
+  endpoints = {
+    guardduty = {
+      service             = "guardduty-data"
+      private_dns_enabled = true
+      security_group_ids  = [aws_security_group.guardduty_vpce.id]
+      subnet_ids          = aws_subnet.eks_private[*].id
+      tags = {
+        Name = "${terraform.workspace}-guardduty-monitoring-vpce"
+      }
+    }
+  }
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+resource "aws_security_group" "guardduty_vpce" {
+  name        = "${terraform.workspace}-guardduty-vpce-sg"
+  description = "Security group for GuardDuty VPC endpoint"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-guardduty-vpce-sg"
+  }
+
+  depends_on = [
+    module.vpc
+  ]
+}
---
+++ terraform/aws-accounts/cloud-platform-aws/vpc/s3-vpce.tf
@@ -0,0 +1,21 @@
+module "aws_s3_vpce" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "6.0.0"
+
+  vpc_id = module.vpc.vpc_id
+
+  endpoints = {
+    s3 = {
+      service         = "s3"
+      service_type    = "Gateway"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags = {
+        Name = "${terraform.workspace}-s3-vpce"
+      }
+    }
+  }
+
+  depends_on = [
+    module.vpc
+  ]
+}
