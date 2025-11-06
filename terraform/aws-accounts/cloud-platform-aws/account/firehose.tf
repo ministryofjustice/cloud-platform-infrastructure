bc024a6dc4fb8215e893dac656082b7c5892e4d2
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-11-06 09:10:38 +0000

    Add S3 bucket for RDS logs to Cortex XSIAM
    This is Step 1 of implementing the S3-based log delivery approach
    for RDS logs to Cortex XSIAM. The S3 bucket will store RDS logs
    from Firehose, preserving metadata (including RDS instance names)
    that allows Cortex to identify log ownership.
    
    Related to #7667

--- terraform/aws-accounts/cloud-platform-aws/account/firehose.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/firehose.tf
@@ -7,6 +7,23 @@ module "firehose_eks_live_logs_to_xsiam" {
   destination_http_endpoint  = data.aws_ssm_parameter.account["cortex_xsiam_endpoint"].value
 }
 
+# S3 bucket for RDS logs to Cortex XSIAM
+module "rds_logs_to_cortex_s3" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
+
+  # S3 configuration
+  versioning = true
+
+  # Tags
+  namespace              = "cloud-platform"
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
 # live RDS DB logs to Cortex XSIAM
 module "firehose_rds_test_logs_to_xsiam" {
   source                    = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=1.1.0"
