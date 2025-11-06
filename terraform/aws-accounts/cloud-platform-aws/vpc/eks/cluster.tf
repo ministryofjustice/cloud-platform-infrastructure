b4e062b7a81bcc11d4ccd12d77c23d79f74c7156
Emterry <emma.terry@digital.justice.gov.uk>
2025-11-06 16:28:58 +0000

    update coredns post eks upgrade

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/cluster.tf
@@ -380,7 +380,7 @@ module "aws_eks_addons" {
   addon_tags              = local.tags
 
   addon_vpc_cni_version         = "v1.20.2-eksbuild.1"
-  addon_coredns_version         = "v1.11.4-eksbuild.22"
+  addon_coredns_version         = "v1.11.4-eksbuild.24"
   addon_kube_proxy_version      = "v1.32.6-eksbuild.12"
   addon_guardduty_agent_version = "v1.11.0-eksbuild.4"
 }
