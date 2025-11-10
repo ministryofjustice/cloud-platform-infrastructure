5fb345f7a6cab19cdd381b752ea5cb34cf5809b7
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-11-10 12:47:03 +0000

    Switch RDS Firehose from HTTP to S3 delivery
    Step 2 of approach-1 for issue #7667.
    
    Changes:
    - Switch Firehose destination from HTTP endpoint to S3 bucket
    - Logs will now be delivered to module.rds_logs_to_cortex_s3
    - Preserves metadata (log group name with RDS instance identifier)
    
    Impact:
    - Brief interruption in log delivery to Cortex HTTP endpoint
    - Logs will begin accumulating in S3 with metadata
    - Next step: Validate logs appear in S3 with RDS instance names
    
    Related: #7667

--- terraform/aws-accounts/cloud-platform-aws/account/firehose.tf
+++ terraform/aws-accounts/cloud-platform-aws/account/firehose.tf
@@ -9,6 +9,6 @@ module "firehose_eks_live_logs_to_xsiam" {
 
 # live RDS DB logs to Cortex XSIAM
 module "firehose_rds_test_logs_to_xsiam" {
-  source                    = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=1.1.0"
-  destination_http_endpoint = data.aws_ssm_parameter.account["cortex_xsiam_endpoint"].value
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=1.1.0"
+  destination_bucket_arn = module.rds_logs_to_cortex_s3.bucket_arn
 }
