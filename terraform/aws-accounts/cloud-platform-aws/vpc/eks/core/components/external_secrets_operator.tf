bc362ba79d44952e8d0766ffd2f4c423929a6d78
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-11-13 15:02:13 +0000

    chore: bump eso

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/external_secrets_operator.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/external_secrets_operator.tf
@@ -1,5 +1,5 @@
 module "external_secrets_operator" {
-  source                      = "github.com/ministryofjustice/cloud-platform-terraform-external-secrets-operator?ref=0.2.0"
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-external-secrets-operator?ref=0.3.0"
   cluster_domain_name         = data.terraform_remote_state.cluster.outputs.cluster_domain_name
   eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
   secrets_prefix              = terraform.workspace
