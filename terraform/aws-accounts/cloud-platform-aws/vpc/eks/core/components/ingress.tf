5479ad134afbeaf65c1cce3a423ec4946b319617
sj-williams <steve.williams@digital.justice.gov.uk>
2026-03-23 07:59:57 +0000

    feat: 🎸 rollout modsec-non-prod validation controller

--- terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/ingress.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/eks/core/components/ingress.tf
@@ -207,3 +207,30 @@ module "ingress_controllers_internal_non_prod" {
     module.label_pods_controller
   ]
 }
+
+###########################
+# validation controllers  #
+###########################
+
+# non-prod-modsec
+module "modsec_non_prod_ingress_controllers_validator" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ingress-validation-controller?ref=0.1.0"
+  count  = terraform.workspace == "live" ? 1 : 0
+
+  replica_count      = "3"
+  controller_name    = "modsec-non-prod"
+  memory_requests    = "2Gi"
+  memory_limits      = "4Gi"
+  cluster            = terraform.workspace
+  enable_modsec      = true
+  enable_owasp       = true
+  validator_registry = "754256621582.dkr.ecr.eu-west-2.amazonaws.com"
+  validator_image    = "webops/cloud-platform-terraform-ingress-validation-controller"
+  validator_tag      = "ff3f53388052256d48606739aaa65092c234f1c8"
+  validator_digest   = "sha256:86586f2105b2d5c57e0c0c45e2216f6ad666992402122455138402c6e1d6caeb"
+
+  default_tags = local.default_tags
+
+  depends_on = [module.ingress_controllers_v1]
+
+}
