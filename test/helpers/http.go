b623598a567421514872033565d9a4f6a2d33b1e
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2026-08-12 16:21:24 +0100

    test: add test for kube-state-metrics prometheus job status

--- test/helpers/http.go
+++ test/helpers/http.go
@@ -2,6 +2,16 @@ package helpers
 
 import "github.com/go-resty/resty/v2"
 
+type PrometheusResponse struct {
+	Status string `json:"status"`
+	Data   struct {
+		Result []struct {
+			Metric map[string]string `json:"metric"`
+			Value  []interface{}     `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
 // HttpStatusCode return the HTTP code for an endpoint
 func HttpStatusCode(u string) (int, error) {
 	client := resty.New()
