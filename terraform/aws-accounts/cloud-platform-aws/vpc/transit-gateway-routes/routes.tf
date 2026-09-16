4d9b8b5c07b0683686ea8b003b36e49a0b7a8785
folarin oyenuga <folarin.oyenuga@digital.justice.gov.uk>
2026-09-16 12:10:52 +0100

    docs: simplify the Container Platform range comment

--- terraform/aws-accounts/cloud-platform-aws/vpc/transit-gateway-routes/routes.tf
+++ terraform/aws-accounts/cloud-platform-aws/vpc/transit-gateway-routes/routes.tf
@@ -20,7 +20,7 @@ locals {
     "10.162.96.0/20",  # Delius Core Training VPC
     "10.26.0.0/16",    # Mod Platform Dev and Test
     "10.27.0.0/16",    # Mod Platform Pre-Prod and Prod
-    "10.195.0.0/16",   # Container Platform nonlive and live hub
+    "10.195.0.0/16",   # Container Platform
     "10.202.0.0/20",   # LAA Platform Dev
     "10.203.0.0/20",   # LAA Platform Test
     "10.206.0.0/20",   # LAA Platform UAT
